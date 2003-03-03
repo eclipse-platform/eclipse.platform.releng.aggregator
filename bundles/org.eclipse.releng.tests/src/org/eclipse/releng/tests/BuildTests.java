cf497c6161be8dc362060f7b29604c16f38bbd3b
Dean Roberts <droberts>
2003-03-03 15:38:05 +0000

    Add CVS K tag tests

--- bundles/org.eclipse.releng.tests/src/org/eclipse/releng/tests/BuildTests.java
+++ bundles/org.eclipse.releng.tests/src/org/eclipse/releng/tests/BuildTests.java
@@ -1,16 +1,34 @@
+/******************************************************************************
+ * Copyright (c) 2003 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0 
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ ******************************************************************************/
+
 package org.eclipse.releng.tests;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+//import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
 import java.util.zip.ZipFile;
 
 import org.eclipse.core.boot.BootLoader;
@@ -19,6 +37,17 @@ import org.eclipse.core.runtime.Platform;
 import junit.framework.TestCase;
 
 public class BuildTests extends TestCase {
+	
+	private static final String CVS_KO = "-ko";
+	private static final String CVS_KKV = "-kkv";
+	private static final String CVS_KB = "-kb";
+	private static final String CVS_BINARY = "CVS_BINARY";
+	private static final int ENTRY_TYPE_INDEX = 3;
+	private static final int ENTRY_NAME_INDEX = 0;
+	private static final int ENTRY_FIELDS_SIZE = 5;
+	private static final String DEFAULT_CVS_TYPE = "-kkv";
+	
+	private Map cvsTypes;
 	private static final int HTML = 0;
 	private static final int PROPERTIES = 1;
 	private static final int XML = 2;
@@ -70,7 +99,177 @@ public class BuildTests extends TestCase {
 		
 		return result;
 	}
-		public void testChkpii() {
+	
+	public void testCVSKTag() {
+		boolean result = false;
+		
+		initializeCVSTypes();
+		
+		// String to use when running as an automated test.
+		String logFileName = BootLoader.getInstallURL().getPath() + ".." + File.separator + ".." + File.separator + "results" + File.separator + "cvsTags";
+
+		// String t use when running in an Eclipse runtime
+		// String logFileName = BootLoader.getInstallURL().getPath() + "plugins" + File.separator + "org.eclipse.releng.tests_2.1.0" + File.separator + "results" + File.separator + "cvsTags";
+
+		new File(logFileName).mkdirs();
+		logFileName = logFileName + File.separator + "cvsTypesLog.txt";
+
+		try {
+
+	//		StringWriter aWriter = new StringWriter();
+			BufferedWriter aLog = new BufferedWriter(new FileWriter(logFileName));
+			String rootDirectoryName = "d:\\sourceFetch";
+			File rootDirectory = new File(rootDirectoryName);
+			result = scanCVSKTag(rootDirectory, aLog);
+	//		System.out.println(aWriter.getBuffer().toString());
+			aLog.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		assertFalse("CVS KTag type errors.  See the releng test logs linked from the test results page for details.", result);
+	}
+	
+	private void initializeCVSTypes() {
+		cvsTypes = new HashMap();
+		cvsTypes.put("gif", CVS_BINARY);
+		cvsTypes.put("jpg", CVS_BINARY);
+		cvsTypes.put("zip", CVS_BINARY);
+		cvsTypes.put("jar", CVS_BINARY);
+		cvsTypes.put("bmp", CVS_BINARY);
+		cvsTypes.put("class", CVS_BINARY);
+		cvsTypes.put("dll", CVS_BINARY);
+		cvsTypes.put("doc", CVS_BINARY);
+		cvsTypes.put("exe", CVS_BINARY);
+		cvsTypes.put("ico", CVS_BINARY);
+		cvsTypes.put("jpeg", CVS_BINARY);
+		cvsTypes.put("pdf", CVS_BINARY);
+		cvsTypes.put("png", CVS_BINARY);
+		cvsTypes.put("ppt", CVS_BINARY);
+		cvsTypes.put("so", CVS_BINARY);
+		cvsTypes.put("tiff", CVS_BINARY);
+		cvsTypes.put("tif", CVS_BINARY);
+		cvsTypes.put("xls", CVS_BINARY);
+	}
+	
+	private boolean scanCVSKTag(File aDirectory, BufferedWriter aLog) {
+		
+		boolean result = false;
+		
+		File[] files = aDirectory.listFiles();
+		for (int i = 0; i < files.length; i++) {
+			File aFile = files[i];
+			if (aFile.isDirectory()) {
+				if (aFile.getName().equals("CVS")) {
+					result = result | scanCVSDirectory(aFile, aLog);
+				} else {
+					result = result | scanCVSKTag(aFile, aLog);
+				} 
+			}
+		}
+		return result;
+	}
+	
+	private boolean scanCVSDirectory(File aDirectory, BufferedWriter aLog) {
+		
+		boolean result = false;
+		
+		File entries = new File(aDirectory, "Entries");
+		try {
+			BufferedReader aReader = new BufferedReader(new FileReader(entries));
+			String aLine = aReader.readLine();
+			while (aLine != null) {
+				result = result | validateEntry(aDirectory.getParentFile(), aLine, aLog);
+				aLine = aReader.readLine();
+			}
+			
+			aReader.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("File Not Found reading Entries file");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("IOException reading Entries file");
+			e.printStackTrace();
+		}
+		
+		return result;
+		
+	}
+	
+	private boolean validateEntry(File aDirectory, String aLine, BufferedWriter aLog) {
+
+		boolean result = false;
+		
+		String[] fields = split(aLine, "/");
+		if (fields.length < ENTRY_FIELDS_SIZE) {
+			return result;
+		}
+		
+		String entryName = aDirectory + File.separator + fields[ENTRY_NAME_INDEX];
+		String expectedType  = (String) cvsTypes.get(entryName);
+
+		// No type registered for exact file name.  Check for extension
+		if (expectedType == null) {			
+			
+			String[] dotParts = split(fields[ENTRY_NAME_INDEX], ".");
+			String entryExtension;
+			if (dotParts.length == 0) {
+				// File name has no extension.
+				expectedType = DEFAULT_CVS_TYPE;
+			} else {
+				entryExtension = dotParts[dotParts.length - 1];
+				expectedType = (String) cvsTypes.get(entryExtension);
+				if (expectedType == null) {
+					// Extension has no type registered
+					expectedType = DEFAULT_CVS_TYPE;
+				}
+			}
+		}
+		
+		// We know what type to expect for this file.  Are we the right one?
+				
+		String entryType = fields[ENTRY_TYPE_INDEX];
+		
+		try {
+			if (expectedType.equals(CVS_BINARY)) {
+				if (!entryType.equals(CVS_KB)) {
+					// Fail Binary Test
+					aLog.write("File: " + entryName + " was expected to be be binary");
+//					System.out.println("File: " + entryName + " was expected to be be binary");
+					aLog.newLine();
+					result = true;
+				}
+			} else {
+				if (!(entryType.equals(CVS_KKV) || entryType.equals(CVS_KO))) {
+					// Fail
+					aLog.write("File: " + entryName + " was expected to be be text");
+//					System.out.println("File: " + entryName + " was expected to be be text");
+					aLog.newLine();
+					result = true;
+				}
+			}
+		} catch (IOException e) {	
+			System.out.println("IOException writing log");
+			e.printStackTrace();	
+		}
+		return result;
+	}
+	
+	/**
+	 * @param aLine
+	 * @param delimeter
+	 * @return String[]
+	 */
+	private String[] split(String aLine, String delimeter) {
+		StringTokenizer tokenizer = new StringTokenizer(aLine, delimeter);
+		List list = new ArrayList();
+		while (tokenizer.hasMoreTokens()) {
+			list.add(tokenizer.nextToken());
+		}
+		return (String[]) list.toArray(new String[0]);
+	}
+	
+	public void testChkpii() {
 			
 			String zipFile = locateEclipseZip();
 //			String sniffFolder = BootLoader.getInstallURL().getPath() + "releng_sniff_folder";
