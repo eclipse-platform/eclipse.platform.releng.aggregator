9a5aa157e0a9c7c634d6764fd122de57d59de5c6
Dean Roberts <droberts>
2003-03-10 22:09:45 +0000

    Copyright fix - automated changes

--- bundles/org.eclipse.releng.tests/src/org/eclipse/releng/tests/FileTool.java
+++ bundles/org.eclipse.releng.tests/src/org/eclipse/releng/tests/FileTool.java
@@ -1,13 +1,13 @@
-/******************************************************************************
- * Copyright (c) 2003 IBM Corporation and others.
+/*******************************************************************************
+ * Copyright (c) 2000, 2003 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials 
- * are made available under the terms of the Common Public License v1.0 
+ * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/cpl-v10.html
  * 
  * Contributors:
  *     IBM Corporation - initial API and implementation
- ******************************************************************************/
+ *******************************************************************************/
 
 package org.eclipse.releng.tests;
 import java.io.BufferedInputStream;
@@ -26,7 +26,8 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
 
-/**
+
+/**
  * A tool for performing operations on files.
  */
 public class FileTool {
