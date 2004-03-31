4bb30c9e8b749e8a95c807bbc155761c2a6ef92f
Sonia Dimitrov <sdimitro>
2004-03-31 20:04:20 +0000

    added performance test

---
+++ bundles/org.eclipse.releng.tests/src/org/eclipse/releng/tests/perfms/UITest.java
@@ -0,0 +1,32 @@
+package org.eclipse.releng.tests.perfms;
+import junit.framework.TestCase;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.WorkbenchException;
+import org.eclipse.perfmsr.core.PerfMsrCorePlugin;
+import org.eclipse.perfmsr.core.Upload;
+
+public class UITest extends TestCase {
+	public void runTest() {
+		testPerfms();
+	}
+	protected void tearDown() {
+		Upload.Status status = PerfMsrCorePlugin.getPerformanceMonitor(true)
+				.upload(null);
+		System.out.println(status.message);
+	}
+	public void testPerfms() {
+		/*
+		 * this test takes snapshots before (1) and after (2) opening the Java
+		 * Perspective. The delta between snapshots can be used to calculate the time required to open
+		 * the Java perspective.
+		 */
+		try {
+			PerfMsrCorePlugin.getPerformanceMonitor(true).snapshot(1);
+			PlatformUI.getWorkbench().openWorkbenchWindow(
+					"org.eclipse.jdt.ui.JavaPerspective", null);
+			PerfMsrCorePlugin.getPerformanceMonitor(true).snapshot(2);
+		} catch (WorkbenchException e) {
+			e.printStackTrace();
+		} 
+	}
+}
