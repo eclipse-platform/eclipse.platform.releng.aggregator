509d6ac9c949ef6892f850bfbe6b0bb919519e6b
Sonia Dimitrov <sdimitro>
2004-09-07 14:56:15 +0000

    remove dependency on org.eclipse.perfms.core plugin.  Add template for File size test.

---
+++ bundles/org.eclipse.releng.tests/src/org/eclipse/releng/tests/performance/DownloadFileSizeTest.java
@@ -0,0 +1,25 @@
+/*
+ * Created on Aug 31, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package org.eclipse.releng.tests.performance;
+
+/**
+ * @author SDimitrov
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class DownloadFileSizeTest {
+
+    /**
+     * 
+     */
+    public DownloadFileSizeTest() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+}
