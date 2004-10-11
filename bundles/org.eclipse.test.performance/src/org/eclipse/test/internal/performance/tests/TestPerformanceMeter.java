db4bd4c38c1dfa7403474d9c94d4f0375597a98b
Andre Weinand <aweinand>
2004-10-11 22:28:40 +0000

    comment changes

--- bundles/org.eclipse.test.performance/src/org/eclipse/test/internal/performance/tests/TestPerformanceMeter.java
+++ bundles/org.eclipse.test.performance/src/org/eclipse/test/internal/performance/tests/TestPerformanceMeter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2004 IBM Corporation and others.
+ * Copyright (c) 2004 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
@@ -24,7 +24,7 @@ import org.eclipse.test.internal.performance.data.Unit;
 
 
 /**
- * Performance meter that generates deterministic values.
+ * Mock performance meter that generates deterministic values for two dimensions.
  */
 class TestPerformanceMeter extends InternalPerformanceMeter {
     
