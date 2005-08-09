22a8f6a54e137ea779b0e071d4224f9a932e5ac3
Sonia Dimitrov <sdimitro>
2005-08-09 19:27:16 +0000

    revert

--- bundles/org.eclipse.releng.tests/src/org/eclipse/releng/tests/BuildTests.java
+++ bundles/org.eclipse.releng.tests/src/org/eclipse/releng/tests/BuildTests.java
@@ -515,11 +515,6 @@ public class BuildTests extends TestCase {
 	}
 	
 	public void testChkpii() {
-			if (!new File(getExec()).exists()){
-				System.out.println(getExec()+" not installed.  Skipping chkpii testing.");
-				assertTrue(true);
-				return;
-			}
 			
 			String zipFile = locateEclipseZip();
 
