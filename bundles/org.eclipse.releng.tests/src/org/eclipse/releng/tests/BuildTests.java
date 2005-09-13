544ac77ff9dbc78a96909153a1a0b4fb7d950422
Kim Moir <kmoir>
2005-09-13 14:44:08 +0000

    version 709 of chkpii

--- bundles/org.eclipse.releng.tests/src/org/eclipse/releng/tests/BuildTests.java
+++ bundles/org.eclipse.releng.tests/src/org/eclipse/releng/tests/BuildTests.java
@@ -680,7 +680,7 @@ public class BuildTests extends TestCase {
 		
 		private String getExec() {
 
-			return new File("chkpw708.exe").getPath();
+			return new File("chkpw709.exe").getPath();
 		}
 		
 		/**
