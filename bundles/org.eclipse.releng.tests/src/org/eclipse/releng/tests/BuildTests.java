66204d42bb16060bd77bbaf7f3eb71646f5f876e
Kim Moir <kmoir>
2005-12-09 19:20:32 +0000

    new version of chkpii v712

--- bundles/org.eclipse.releng.tests/src/org/eclipse/releng/tests/BuildTests.java
+++ bundles/org.eclipse.releng.tests/src/org/eclipse/releng/tests/BuildTests.java
@@ -252,7 +252,7 @@ public class BuildTests extends TestCase {
 		
 		private String getExec() {
 
-			return new File("chkpw711.exe").getPath();
+			return new File("chkpw712.exe").getPath();
 		}
 		
 		/**
