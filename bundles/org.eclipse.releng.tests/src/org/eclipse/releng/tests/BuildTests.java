38e244e9e1abf77dd627434b6b6a98086eb099e4
Sonia Dimitrov <sdimitro>
2006-08-08 20:08:22 +0000

    update version of chkpii (for post 3.3M1)

--- bundles/org.eclipse.releng.tests/src/org/eclipse/releng/tests/BuildTests.java
+++ bundles/org.eclipse.releng.tests/src/org/eclipse/releng/tests/BuildTests.java
@@ -252,7 +252,7 @@ public class BuildTests extends TestCase {
 		
 		private String getExec() {
 
-			return new File("chkpw807.exe").getPath();
+			return new File("chkpw808.exe").getPath();
 		}
 		
 		/**
