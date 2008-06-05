19c98a22e7f876d29beaa0baefcc06d8cba18b73
Kim Moir <kmoir>
2008-06-05 13:18:09 +0000

    chkpii

--- bundles/org.eclipse.releng.tests/src/org/eclipse/releng/tests/BuildTests.java
+++ bundles/org.eclipse.releng.tests/src/org/eclipse/releng/tests/BuildTests.java
@@ -289,7 +289,7 @@ public class BuildTests extends TestCase {
 
 	private String getExec() {
 
-		return new File("chkpw1005.exe").getPath();
+		return new File("chkpw1006.exe").getPath();
 	}
 
 	/**
