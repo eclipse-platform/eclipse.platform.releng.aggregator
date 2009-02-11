f96b3cff007ceab67385b4551f6fd6f12f412533
Kim Moir <kmoir>
2009-02-11 14:58:52 +0000

    Bug 153429 JUnit4 in Eclipse Testing Framework

--- bundles/org.eclipse.test/src/org/eclipse/test/CoreTestApplication.java
+++ bundles/org.eclipse.test/src/org/eclipse/test/CoreTestApplication.java
@@ -10,77 +10,22 @@
  *******************************************************************************/
 package org.eclipse.test;
 
+
 import java.io.IOException;
-import org.eclipse.core.runtime.IPlatformRunnable;
-import org.eclipse.core.runtime.Platform;
 
 /**
- * A an application that launches tests once it is started.
+ * An application that launches tests using the EclipseTestRunner.
  */
-public class CoreTestApplication implements IPlatformRunnable {
-	/** true if workspace tests should log their deltas */
-	private static boolean deltas= false;
+public class CoreTestApplication {
 	
 	/**
 	 * Runs a set of tests as defined by the given command line args.
-	 * This is the platform application entry point.
-	 * @see IPlatformRunnable
 	 */
-	public Object run(Object arguments) throws Exception {
-		String[] args= Platform.getCommandLineArgs();//getCommand//processCommandLine((String[]) arguments);
+	public Object run(String[] args) throws Exception {
 		return new Integer(runTests(args));
 	}
 
 	protected int runTests(String[] args) throws IOException {
 		return EclipseTestRunner.run(args);
-	}
-
-	public static boolean deltasEnabled() {
-		return deltas;
-	}
-		
-	protected String[] processCommandLine(String[] args) {
-		int[] configArgs = new int[100];
-		configArgs[0] = -1; // need to initialize the first element to something that could not be an index.
-		int configArgIndex = 0;
-		for (int i = 0; i < args.length; i++) {
-			boolean found = false;
-			// check for args without parameters (i.e., a flag arg)
-			// see if we should be logging deltas
-			if (args[i].equalsIgnoreCase("-deltas")) {
-				found = true;
-				deltas = true;
-			}
-			if (found) {
-				configArgs[configArgIndex++] = i;
-				continue;
-			}
-	
-			// check for args with parameters
-			if (i == args.length - 1 || args[i + 1].startsWith("-")) {
-				continue;
-			}
-			++i;
-
-			// done checking for args.  Remember where an arg was found 
-			if (found) {
-				configArgs[configArgIndex++] = i - 1;
-				configArgs[configArgIndex++] = i;
-			}
-		}
-	
-		//remove all the arguments consumed by this argument parsing
-		if (configArgIndex == 0)
-			return args;
-		String[] passThruArgs = new String[args.length - configArgIndex];
-		configArgIndex = 0;
-		int j = 0;
-		for (int i = 0; i < args.length; i++) {
-			if (i == configArgs[configArgIndex])
-				configArgIndex++;
-			else
-				passThruArgs[j++] = args[i];
-		}
-		return passThruArgs;
 	}
 }
