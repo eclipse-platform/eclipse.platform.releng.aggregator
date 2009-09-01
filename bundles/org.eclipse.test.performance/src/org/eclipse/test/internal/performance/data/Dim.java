3a1cf6f6737fb42576786560bb25f0669d036c29
Frederic Fusier <ffusier>
2009-09-01 11:04:42 +0000

    HEAD - 288234

--- bundles/org.eclipse.test.performance/src/org/eclipse/test/internal/performance/data/Dim.java
+++ bundles/org.eclipse.test.performance/src/org/eclipse/test/internal/performance/data/Dim.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -18,21 +18,21 @@ import org.eclipse.test.performance.Dimension;
  * @since 3.1
  */
 public class Dim implements Dimension {
-    
+
     private static Dim[] fgRegisteredDimensions= new Dim[100];
-    
+
     private final int fId;
 	private final Unit fUnit;
 	private final int fMultiplier;
 	private String shortName;
-	
+
 	public static Dim getDimension(int id) {
         InternalDimensions.COMITTED.getId();	// trigger loading class InternalDimensions
 	    if (id >= 0 && id < fgRegisteredDimensions.length)
 	        return fgRegisteredDimensions[id];
 	    return null;
 	}
-	
+
 	public Dim(int id) {
 		this(id, Unit.CARDINAL, 1);
 	}
@@ -42,42 +42,42 @@ public class Dim implements Dimension {
 	}
 
 	public Dim(int id, Unit unit, int multiplier) {
-	    
+
 	    if (id >= 0 && id < fgRegisteredDimensions.length) {
 		    if (fgRegisteredDimensions[id] == null) {
 		        fgRegisteredDimensions[id]= this;
 		    } else
 			    PerformanceTestPlugin.logError("dimension with id '" + id + "' already registered"); //$NON-NLS-1$ //$NON-NLS-2$
 	    }
-		fId= id;
-		fUnit= unit;
-		fMultiplier= multiplier;
+		this.fId= id;
+		this.fUnit= unit;
+		this.fMultiplier= multiplier;
 	}
 
     public int getId() {
-        return fId;
+        return this.fId;
     }
-	
+
 	public Unit getUnit() {
-		return fUnit;
+		return this.fUnit;
 	}
-	
+
 	public int getMultiplier() {
-		return fMultiplier;
+		return this.fMultiplier;
 	}
 
 	public String getName() {
-		return DimensionMessages.getString(fId);
+		return DimensionMessages.getString(this.fId);
 	}
 
 	/**
-	 * Returns the short name for the current dimension.
-	 * This short name is done keeping only uppercase characters from the name.
+	 * Returns the label for the current dimension.
+	 * This label is done keeping only uppercase characters from the name.
 	 * It's typically used for anchor references based on dimension.
-	 * 
+	 *
 	 * @return The short name of the dimension
 	 */
-	public String getShortName() {
+	public String getLabel() {
 		if (this.shortName == null) {
 			String name = getName();
 			StringBuffer buffer = new StringBuffer();
@@ -93,18 +93,18 @@ public class Dim implements Dimension {
 	}
 
 	public String getDescription() {
-		return DimensionMessages.getString(fId);
+		return DimensionMessages.getString(this.fId);
 	}
-	
+
 	public String toString() {
-		return "Dimension [name=" + getName() + ", " + fUnit + "]"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+		return "Dimension [name=" + getName() + ", " + this.fUnit + "]"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 	}
-	
+
 	public String getDisplayValue(Scalar scalar) {
-		return fUnit.getDisplayValue1(scalar.getMagnitude(), fMultiplier);
+		return this.fUnit.getDisplayValue1(scalar.getMagnitude(), this.fMultiplier);
 	}
-	
+
 	public String getDisplayValue(double scalar) {
-		return fUnit.getDisplayValue1(scalar / fMultiplier);
+		return this.fUnit.getDisplayValue1(scalar / this.fMultiplier);
 	}
 }
