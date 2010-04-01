4a1df773260bd27b17f9a907f414b1d933ae8415
Kim Moir <kmoir>
2010-04-01 15:21:22 +0000

    Bug 270627 -  add releng test to run mirroring with skipErrors disabled and parse the output

--- bundles/org.eclipse.releng.tests/src/org/eclipse/releng/tests/BuildTests.java
+++ bundles/org.eclipse.releng.tests/src/org/eclipse/releng/tests/BuildTests.java
@@ -411,7 +411,7 @@ public class BuildTests extends TestCase {
 		// results from!
 
 		String javadocUrls = System.getProperty("RELENGTEST.JAVADOC.URLS");
-	//	String comparatorUrl = System.getProperty("RELENGTEST.COMPARATOR.URL");		
+		String comparatorUrl = System.getProperty("RELENGTEST.COMPARATOR.URL");		
 	
 		if (javadocUrls != null) {
 			String[] urls = javadocUrls.split(",");
@@ -420,9 +420,9 @@ public class BuildTests extends TestCase {
 				javadocLogs[i] = new URL(urls[i]);
 			}
 		}
-	/*	if (comparatorUrl != null) {
+		if (comparatorUrl != null) {
 				comparatorLogs = new URL(comparatorUrl);
-		} */
+		}
 		// Runtime Workbench - TODI Put me back to Automated status
 		// logFileName = "d:\\results";
 		// sourceDirectoryName = "d:\\sourceFetch";
@@ -726,7 +726,7 @@ public class BuildTests extends TestCase {
 	} 
  
 	
- /*	public void testComparatorLogs() {
+	public void testComparatorLogs() {
 		
 		String os = System.getProperty("os.name");
 		
@@ -795,7 +795,7 @@ public class BuildTests extends TestCase {
 					e.printStackTrace();
 				}
 			}		
-	} */
+	}
 
 	
 	
