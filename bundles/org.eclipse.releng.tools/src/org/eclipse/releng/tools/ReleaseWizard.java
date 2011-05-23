c9a7fad5ee38bbac34ac2e0686d2f3594059f293
Dani Megert <dmegert>
2011-05-23 13:39:26 +0000

    Fixed bug 345771: Release... command tags entire build notes project even if "Release only changed projects" unchecked

--- bundles/org.eclipse.releng.tools/src/org/eclipse/releng/tools/ReleaseWizard.java
+++ bundles/org.eclipse.releng.tools/src/org/eclipse/releng/tools/ReleaseWizard.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -285,9 +285,8 @@ public class ReleaseWizard extends Wizard {
 	public boolean performFinish() {
 		if(!isProjectSelected())return false;
 		
-		// Build notes file update cancelled. Close dialog.
-		if (!buildNotesOperation()) {
-			return true;
+		if (projectSelectionPage.isCompareButtonChecked() && !buildNotesOperation()) {
+				return true; // Build notes file update cancelled. Close dialog.
 		}
 		
 		try {
