cb8bcf1235f80918d6492245913d0e7421decf29
Paul Webster <pwebster@ca.ibm.com>
2012-11-16 13:08:59 -0500

    Bug 393925 - [CBI] replace auto-tagging
    update-build-input.sh will update all repos to their latest
    branch tip based on the repositories.txt file
    
    tag-build-input.sh will tag the current state of the aggregator
    and submodules as the input for that build

---
+++ scripts/update-build-input.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+
+if [ $# -ne 1 ]; then
+	echo USAGE: $0 env_file
+	exit 1
+fi
+
+if [ ! -r "$1" ]; then
+	echo "$1" cannot be read
+	echo USAGE: $0 env_file
+	exit 1
+fi
+
+pushd $( dirname $0 ) >/dev/null
+SCRIPT_PATH=$(pwd)
+popd >/dev/null
+
+. $SCRIPT_PATH/build-functions.sh
+
+. "$1"
+
+
+cd $BUILD_ROOT
+
+# derived values
+gitCache=$( fn-git-cache "$BUILD_ROOT" "$BRANCH" )
+aggDir=$( fn-git-dir "$gitCache" "$AGGREGATOR_REPO" )
+repositories=$( echo $SCRIPT_PATH/repositories.txt )
+repoScript=$( echo $SCRIPT_PATH/git-submodule-checkout.sh )
+
+
+if [ -z "$BUILD_ID" ]; then
+	BUILD_ID=$(fn-build-id "$BUILD_TYPE" )
+fi
+
+
+fn-submodule-checkout "$BUILD_ID" "$aggDir" "$repoScript" "$repositories"
