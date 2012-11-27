090aeef33c099d544da3e0cd202ce61cf1427462
Paul Webster <pwebster@ca.ibm.com>
2012-11-27 10:11:01 -0500

    Bug 393927 - [CBI] Generate Deployable build directory
    Pick up the automated test zips

--- scripts/build-functions.sh
+++ scripts/build-functions.sh
@@ -440,6 +440,22 @@ fn-gather-swt-zips () {
 	popd
 }
 
+# USAGE: fn-gather-test-zips BUILD_ID REPO_DIR BUILD_DIR
+#   BUILD_ID: I20121116-0700
+#   REPO_DIR: /shared/eclipse/builds/R4_2_maintenance/gitCache/eclipse.platform.releng.aggregator
+#   BUILD_DIR: /shared/eclipse/builds/R4_2_maintenance/dirs/M20121120-1747
+fn-gather-test-zips () {
+	BUILD_ID="$1"; shift
+	REPO_DIR="$1"; shift
+	BUILD_DIR="$1"; shift
+	pushd "$REPO_DIR"/eclipse.platform.releng.tychoeclipsebuilder/eclipse-junit-tests/target
+	cp eclipse-junit-tests-bundle.zip "$BUILD_DIR"/eclipse-Automated-Tests-${BUILD_ID}.zip
+	pushd eclipse-test-framework
+	zip "$BUILD_DIR"/eclipse-test-framework-${BUILD_ID}.zip *
+	popd
+	popd
+}
+
 
 # USAGE: fn-gather-main-index BUILD_ID REPO_DIR BUILD_DIR STREAM BUILD_TYPE BUILD_DATE
 #   BUILD_ID: I20121116-0700
