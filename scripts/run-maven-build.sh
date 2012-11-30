b526774d3ef3612ca80430533783d5619a56b183
Paul Webster <pwebster@ca.ibm.com>
2012-11-30 10:08:24 -0500

    Bug 393927 - [CBI] Generate Deployable build directory
    Include the update-branding-plugins profile and temporarily
    change the download.php generated link.

--- scripts/run-maven-build.sh
+++ scripts/run-maven-build.sh
@@ -46,4 +46,4 @@ fn-maven-parent-install "$aggDir" "$localRepo"
 
 fn-maven-cbi-install "$aggDir" "$localRepo"
 
-fn-maven-build-aggregator "$BUILD_ID" "$aggDir" "$localRepo" $COMPARATOR $SIGNING
+fn-maven-build-aggregator "$BUILD_ID" "$aggDir" "$localRepo" $COMPARATOR $SIGNING $UPDATE_BRANDING
