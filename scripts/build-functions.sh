d96815c400aeabe1a121eeb3126ce6e26f3b5261
David Williams <david_williams@us.ibm.com>
2013-01-10 15:47:18 -0500

    Bug 397915 - [CBI] add checkForErrorExit function to build-functions.sh

--- scripts/build-functions.sh
+++ scripts/build-functions.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
-# this is not really to be executed
+# this is not really to be executed, but sourced where needed
+
+# general purpose utility for "hard exit" if return code not zero.
+# especially useful to call/check after basic things that should normally
+# easily succeeed.
+# usage:
+#   checkForErrorExit $? "Failed to copy file (for example)"
+checkForErrorExit () {
+    # arg 1 must be return code, $?
+    # arg 2 (remaining line) can be message to print before exiting do to non-zero exit code
+    exitCode=$1
+    shift
+    message="$*"
+    if [ -z "${exitCode}" ]
+    then
+        echo "PROGRAM ERROR: checkForErrorExit called with no arguments"
+        exit 1
+    fi
+    if [ -z "${message}" ]
+    then
+        echo "WARNING: checkForErrorExit called without message"
+        message="(Calling program provided no message)"
+    fi
+    if [ $exitCode -ne 0 ]
+    then
+        echo
+        echo "   ERROR. exit code: ${exitCode}  ${message}"
+        echo
+        exit $exitCode
+    fi
+}
+
+
 
 # USAGE: fn-git-clone URL [BRANCH [TARGET_DIR] ]
 #   URL: file:///gitroot/platform/eclipse.platform.releng.aggregator.git
