138243fd32fc12609e8ccecf225cdbf6dddc47a7
David Williams <david_williams@us.ibm.com>
2013-01-10 19:46:33 -0500

    Bug 397820 - change /bin/bash <sript> <arguments> construct to instead have a step to chmod +x *.sh - fixing typo

--- scripts/master-build.sh
+++ scripts/master-build.sh
@@ -29,7 +29,8 @@ cd $BUILD_ROOT
 
 BUILD_ID=$(fn-build-id "$BUILD_TYPE" )
 buildDirectory=$( fn-build-dir "$BUILD_ROOT" "$BRANCH" "$BUILD_ID" )
-mkdir -p "$buildDirectory"/buildlogs
+logsDirectory="${buildDirectory}/buildlogs"
+mkdir -p "${logsDirectory}"
 checkForErrorExit $? "Could not create buildlogs directory"
 
 LOG=$buildDirectory/buildlogs/buildOutput.txt
@@ -46,10 +47,10 @@ echo "BUILD_ENV_FILE=$1" >>$BUILD_ENV_FILE
 echo "BUILD_ID=$BUILD_ID" >>$BUILD_ENV_FILE
 echo "BUILD_DATE=\"$(date)\"" >>$BUILD_ENV_FILE
 
-$SCRIPT_PATH/get-aggregator.sh $BUILD_ENV_FILE |& tee $buildDirectory/$buildDirectory/get-aggregator-ouptut.txt
+$SCRIPT_PATH/get-aggregator.sh $BUILD_ENV_FILE |& tee $logsDirectory/get-aggregator-ouptut.txt
 checkForErrorExit $? "Error occurred while getting aggregator"
 
-$SCRIPT_PATH/update-build-input.sh $BUILD_ENV_FILE |& tee $buildDirectory/$buildDirectory/update-build-input-ouptut.txt
+$SCRIPT_PATH/update-build-input.sh $BUILD_ENV_FILE |& tee $logsDirectory/update-build-input-ouptut.txt
 checkForErrorExit $? "Error occurred while updating build input"
 
 pushd "$aggDir"
@@ -62,23 +63,23 @@ $GIT_PUSH origin HEAD
 #checkForErrorExit $? "Error occurred during push of build_id commit"
 popd
 
-$SCRIPT_PATH/tag-build-input.sh $BUILD_ENV_FILE |& tee $buildDirectory/$buildDirectory/tag-build-input-ouptut.txt
+$SCRIPT_PATH/tag-build-input.sh $BUILD_ENV_FILE |& tee $logsDirectory/tag-build-input-ouptut.txt
 checkForErrorExit $? "Error occurred during tag of build input"
 
-$SCRIPT_PATH/install-parent.sh $BUILD_ENV_FILE |& tee $buildDirectory/$buildDirectory/install-parent-ouptut.txt
+$SCRIPT_PATH/install-parent.sh $BUILD_ENV_FILE |& tee $logsDirectory/install-parent-ouptut.txt
 checkForErrorExit $? "Error occurred during install parent script"
 
-$SCRIPT_PATH/pom-version-updater.sh $BUILD_ENV_FILE |& tee $buildDirectory/$buildDirectory/pom-version-updater-ouptut.txt
+$SCRIPT_PATH/pom-version-updater.sh $BUILD_ENV_FILE |& tee $logsDirectory/pom-version-updater-ouptut.txt
 checkForErrorExit $? "Error occurred during pom version updater"
 
 
-$SCRIPT_PATH/run-maven-build.sh $BUILD_ENV_FILE |& tee $buildDirectory/$buildDirectory/run-maven-build-ouptut.txt
+$SCRIPT_PATH/run-maven-build.sh $BUILD_ENV_FILE |& tee $logsDirectory/run-maven-build-ouptut.txt
 checkForErrorExit $? "Error occurred during run maven build"
 
-$SCRIPT_PATH/gather-parts.sh $BUILD_ENV_FILE |& tee $buildDirectory/$buildDirectory/gather-parts-ouptut.txt
+$SCRIPT_PATH/gather-parts.sh $BUILD_ENV_FILE |& tee $logsDirectory/gather-parts-ouptut.txt
 checkForErrorExit $? "Error occurred during gather parts"
 
-$SCRIPT_PATH/parse-logs.sh $BUILD_ENV_FILE |& tee $buildDirectory/parse-logs-ouptut.txt
+$SCRIPT_PATH/parse-logs.sh $BUILD_ENV_FILE |& tee $logsDirectory/parse-logs-ouptut.txt
 checkForErrorExit $? "Error occurred during parse-logs"
 
 
