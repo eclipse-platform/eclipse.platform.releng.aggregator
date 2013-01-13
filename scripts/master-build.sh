fb4d2306009d03ea6c6e3cbae96079f0220270c1
David Williams <david_williams@us.ibm.com>
2013-01-12 21:57:30 -0500

    does update-input need to be called twice?

--- scripts/master-build.sh
+++ scripts/master-build.sh
@@ -79,6 +79,10 @@ checkForErrorExit $? "Error occurred while getting aggregator"
 $SCRIPT_PATH/update-build-input.sh $BUILD_ENV_FILE 2>&1 | tee $logsDirectory/update-build-input-ouptut.txt
 checkForErrorExit $? "Error occurred while updating build input"
 
+# Does this really need to be called twice?
+$SCRIPT_PATH/update-build-input.sh $BUILD_ENV_FILE 2>&1 | tee $logsDirectory/update-build-input-ouptut.txt
+checkForErrorExit $? "Error occurred while updating build input"
+
 pushd "$aggDir"
 git commit -m "Build input for build $BUILD_ID"
 # exits with 1 here? 
