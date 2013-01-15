884db9e57817befb8c5a1cb17e1c2edb663e3a1c
David Williams <david_williams@us.ibm.com>
2013-01-15 13:16:06 -0500

    Bug 398144 - [CBI] eclipse-junit-tests should be (near) identical between branches

--- eclipse.platform.releng.tychoeclipsebuilder/eclipse-junit-tests/src/main/scripts/runtests.bat
+++ eclipse.platform.releng.tychoeclipsebuilder/eclipse-junit-tests/src/main/scripts/runtests.bat
@@ -1,8 +1,8 @@
 @echo off
+SETLOCAL
 
 REM default java executable for outer and test vm
 set vmcmd=java
-set no_proxy=localhost,dev.eclipse.org,hudson.eclipse.org
 
 REM reset list of ant targets in test.xml to execute
 set tests=
@@ -20,6 +20,9 @@ set arch=x86
 
 REM reset ant command line args
 set ANT_CMD_LINE_ARGS=
+
+IF EXIST localTestsProperties.bat CALL localTestsProperties.bat
+
 
 REM ****************************************************************
 REM
@@ -39,14 +42,19 @@ REM ****************************************************************
 
 if x%1==x goto run
 if x%1==x-ws set ws=%2 && shift && shift && goto processcmdlineargs
-if x%1==x-os set os =%2 && shift && shift && goto processcmdlineargs
+if x%1==x-os set os=%2 && shift && shift && goto processcmdlineargs
 if x%1==x-arch set arch=%2 && shift && shift && goto processcmdlineargs
 if x%1==x-noclean set installmode=noclean&& shift && goto processcmdlineargs
 if x%1==x-properties set properties=-propertyfile %2 && shift && shift && goto processcmdlineargs
 if x%1==x-vm set vmcmd="%2" && shift && shift && goto processcmdlineargs
+if x%1==x-extdirprop SET extdirproperty="-Djava.ext.dirs=%2" && shift && shift && goto processcmdlineargs
+
+
 
 set tests=%tests% %1 && shift && goto processcmdlineargs
 
+echo Specified test targets (if any): %tests%
+echo Specified extdirs (if any): %extdirprop%
 
 :run
 REM ***************************************************************************
@@ -56,8 +64,26 @@ REM get name of org.eclipse.equinox.launcher_*.jar with version label
 dir /b eclipse\plugins\org.eclipse.equinox.launcher_*.jar>launcher-jar-name.txt
 set /p launcher-jar=<launcher-jar-name.txt
 
-%vmcmd% -Dosgi.os=%os% -Dosgi.ws=%ws% -Dosgi.arch=%arch% -jar eclipse\plugins\%launcher-jar% -data workspace -application org.eclipse.ant.core.antRunner -file test.xml %tests% -Dws=%ws% -Dos=%os% -Darch=%arch% -D%installmode%=true %properties% -logger org.apache.tools.ant.DefaultLogger
 
-goto end
+echo "list all environment variables in effect as tests start"
+set
 
-:end
+rem -Dtimeout=300000 "%ANT_OPTS%"
+
+IF NOT EXIST %vmcmd% (
+ECHO ERROR: vmcmd not defined or does not exist: %vmcmd%
+exit 1
+)
+
+REM -XshowSettings is supported on windows VMs but ... not every where. So where not supported
+REM causes VM to not start at all. Can be handy for diagnostics. (without running ant <echoproperties/>
+
+IF DEFINED extdirproperty (
+%vmcmd%  %extdirproperty%  -Dosgi.os=%os% -Dosgi.ws=%ws% -Dosgi.arch=%arch% -jar eclipse\plugins\%launcher-jar% -data workspace -application org.eclipse.ant.core.antRunner -file test.xml %tests% -Dws=%ws% -Dos=%os% -Darch=%arch%  -D%installmode%=true %properties% -logger org.apache.tools.ant.DefaultLogger
+GOTO END
+)
+
+%vmcmd%  -Dosgi.os=%os% -Dosgi.ws=%ws% -Dosgi.arch=%arch% -jar eclipse\plugins\%launcher-jar% -data workspace -application org.eclipse.ant.core.antRunner -file test.xml %tests% -Dws=%ws% -Dos=%os% -Darch=%arch%  -D%installmode%=true %properties% -logger org.apache.tools.ant.DefaultLogger
+
+:END
+
--- eclipse.platform.releng.tychoeclipsebuilder/eclipse-junit-tests/src/main/scripts/runtests.sh
+++ eclipse.platform.releng.tychoeclipsebuilder/eclipse-junit-tests/src/main/scripts/runtests.sh
@@ -1,15 +1,23 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-#set the DISPLAY for running tests on Linux
-#DISPLAY=`$HOST`:0.0;export DISPLAY
-export no_proxy=localhost,dev.eclipse.org,hudson.eclipse.org
+# This file should never exist or be needed for production machine,
+# but allows an easy way for a "local user" to provide this file
+# somewhere on the search path ($HOME/bin is common),
+# and it will be included here, thus can provide "override values"
+# to those defined by defaults for production machine.,
+# such as for vmcmd
+
+source localTestsProperties.shsource
+
 
 # by default, use the java executable on the path for outer and test jvm
-#vmcmd=/shared/common/jdk-1.6.x86_64/jre/bin/java
+#vmcmd=${vmcmd:-/shared/common/jdk-1.6.x86_64/jre/bin/java}
 vmcmd=java
 
+echo "vmcmd: $vmcmd"
+
 #this value must be set when using rsh to execute this script, otherwise the script will execute from the user's home directory
-dir=.
+dir=${PWD}
 
 # operating system, windowing system and architecture variables
 os=
@@ -25,6 +33,9 @@ installmode="clean"
 # name of a property file to pass to Ant
 properties=
 
+# ext dir customization. Be sure "blank", if not defined explicitly on command line
+extdirproperty=
+
 # message printed to console
 usage="usage: $0 -os <osType> -ws <windowingSystemType> -arch <architecture> [-noclean] [<test target>][-properties <path>]"
 
@@ -32,58 +43,105 @@ usage="usage: $0 -os <osType> -ws <windowingSystemType> -arch <architecture> [-n
 # proces command line arguments
 while [ $# -gt 0 ]
 do
-	case "$1" in
-		-dir) dir="$2"; shift;;
-		-os) os="$2"; shift;;
-		-ws) ws="$2"; shift;;
-		-arch) arch="$2"; shift;;
-		-noclean) installmode="noclean";;
-		-properties) properties="-propertyfile $2";shift;;
-		-vm) vmcmd="$2"; shift;;
-		*) tests=$tests\ $1;;
-	esac
-	shift
+    case "${1}" in
+        -dir)
+            dir="${2}"; shift;;
+        -os)
+            os="${2}"; shift;;
+        -ws)
+            ws="${2}"; shift;;
+        -arch)
+            arch="${2}"; shift;;
+        -noclean)
+            installmode="noclean";;
+        -properties)
+            properties="-propertyfile ${2}";shift;;
+        -extdirprop)
+            extdirproperty="-Djava.ext.dirs=${2}";shift;;
+        -vm)
+            vmcmd="${2}"; shift;;
+        *)
+            tests=$tests\ ${1};;
+    esac
+    shift
 done
+
+echo "Specified test targets (if any): ${tests}"
+
+echo "Specified ext dir (if any): ${extdirproperty}"
 
 # for *nix systems, os, ws and arch values must be specified
 if [ "x$os" = "x" ]
 then
-	echo >&2 "$usage"
-	exit 1
+    echo >&2 "$usage"
+    exit 1
 fi
 
 if [ "x$ws" = "x" ]
 then
-	echo >&2 "$usage"
-	exit 1
+    echo >&2 "$usage"
+    exit 1
 fi
 
 if [ "x$arch" = "x" ]
 then
-	echo >&2 "$usage"
-	exit 1
+    echo >&2 "$usage"
+    exit 1
 fi
 
 #necessary when invoking this script through rsh
 cd $dir
 
-# verify os, ws and arch values passed in are valid before running tests
-if [ "$os-$ws-$arch" = "linux-gtk-x86" ] || [ "$os-$ws-$arch" = "macosx-cocoa-ppc" ] || [ "$os-$ws-$arch" = "macosx-cocoa-x86" ] || [ "$os-$ws-$arch" = "aix-gtk-ppc" ] || [ "$os-$ws-$arch" = "aix-gtk-ppc64" ]  || [ "$os-$ws-$arch" = "solaris-gtk-sparc" ] || [ "$os-$ws-$arch" = "solaris-gtk-x86" ] || [ "$os-$ws-$arch" = "linux-gtk-ppc64" ] ||  [ "$os-$ws-$arch" = "linux-gtk-ia64" ] ||  [ "$os-$ws-$arch" = "linux-gtk-x86_64" ] ||  [ "$os-$ws-$arch" = "hpux-gtk-ia64_32"]
-then
-	if [ ! -r eclipse ]
-	then
-		tar -xzf eclipse-SDK-*.tar.gz
-		unzip -qq -o -C eclipse-junit-tests-*.zip */plugins/org.eclipse.test* -d eclipse/dropins/
-fi
+    if [ ! -r eclipse ]
+    then
+        tar -xzf eclipse-SDK-*.tar.gz
+        # note, the file pattern to match, must not start with */plugins because there is no leading '/' in the zip file, since they are repos.
+        unzip -qq -o -C eclipse-junit-tests-*.zip plugins/org.eclipse.test* -d eclipse/dropins/
+    fi
 
-# run tests
-launcher=`ls eclipse/plugins/org.eclipse.equinox.launcher_*.jar`
-$vmcmd -Dosgi.os=$os -Dosgi.ws=$ws -Dosgi.arch=$arch -jar $launcher -data workspace -application org.eclipse.ant.core.antRunner -file `pwd`/test.xml $tests -Dws=$ws -Dos=$os -Darch=$arch -D$installmode=true $properties -logger org.apache.tools.ant.DefaultLogger
+    # run tests
+    launcher=`ls eclipse/plugins/org.eclipse.equinox.launcher_*.jar`
 
-# display message to user if os, ws and arch are invalid
-else
-	echo "The os, ws and arch values are either invalid or are an invalid combination"
 
-exit 1
-fi
+    # make sure there is a window manager running. See bug 379026
+    # we should not have to, but may be a quirk/bug of hudson setup
+    # assuming metacity attaches to "current" display by default (which should have
+    # already been set by Hudson). We echo its value here just for extra reference/cross-checks.
 
+    echo "Check if any window managers are running (xfwm|twm|metacity|beryl|fluxbox|compiz):"
+    wmpss=$(ps -ef | egrep -i "xfwm|twm|metacity|beryl|fluxbox|compiz" | grep -v egrep)
+    echo "Window Manager processes: $wmpss"
+    echo
+
+    if [[ -z $wmpss ]]
+    then
+             echo "No window managers processes found running, so will start metacity"
+             metacity --replace --sm-disable  &
+             METACITYPID=$!
+             echo $METACITYPID > epmetacity.pid
+     else
+             echo "Existing window manager found running, so did not force start of metacity"
+    fi
+
+    echo
+
+    # list out metacity processes so overtime we can see if they accumulate, or if killed automatically
+    # when our process exits. If not automatic, should use epmetacity.pid to kill it when we are done.
+    echo "Current metacity processes running (check for accumulation):"
+    ps -ef | grep "metacity" | grep -v grep
+    echo
+
+    echo "Triple check if any window managers are running (at least metacity should be!):"
+    wmpss=$(ps -ef | egrep -i "xfwm|twm|metacity|beryl|fluxbox|compiz" | grep -v egrep)
+    echo "Window Manager processes: $wmpss"
+    echo
+    echo "extdirprop in runtest: ${extdirprop}"
+    echo "extdirproperty in runtest: ${extdirproperty}"
+
+    # -Dtimeout=300000 "${ANT_OPTS}"
+ if [[ ! -z "${extdirproperty}" ]]
+ then
+	$vmcmd "${extdirproperty}" -Dosgi.os=$os -Dosgi.ws=$ws -Dosgi.arch=$arch -jar $launcher -data workspace -application org.eclipse.ant.core.antRunner -file ${PWD}/test.xml $tests -Dws=$ws -Dos=$os -Darch=$arch -D$installmode=true $properties -logger org.apache.tools.ant.DefaultLogger
+ else
+	$vmcmd -Dosgi.os=$os -Dosgi.ws=$ws -Dosgi.arch=$arch  -jar $launcher -data workspace -application org.eclipse.ant.core.antRunner -file ${PWD}/test.xml $tests -Dws=$ws -Dos=$os -Darch=$arch -D$installmode=true $properties -logger org.apache.tools.ant.DefaultLogger
+ fi
