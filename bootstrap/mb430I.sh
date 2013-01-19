c6f50b63d0294abd5f4e545aae7cbeffa14dd3f0
David Williams <david_williams@us.ibm.com>
2013-01-19 02:32:54 -0500

    Bug 398201 - [CBI] Issues with org.eclipse.osgi (temp hack)

--- bootstrap/mb430I.sh
+++ bootstrap/mb430I.sh
@@ -3,8 +3,31 @@
 # Simple utility to run as cronjob to run Eclipse Platform builds
 # Normally resides in $BUILD_HOME
 
-# basic path required when starting from cron job
+# set minimal path for consistency across machines
+# plus, cron jobs do not inherit an environment
 export PATH=/usr/local/bin:/usr/bin:/bin
+# unset common variables which we don't want (or, set ourselves)
+unset JAVA_HOME
+unset JAVA_ROOT
+unset JAVA_JRE
+unset CLASSPATH
+unset JAVA_BINDIR
+
+export BUILD_ROOT=/shared/eclipse/builds/localbuild-$BRANCH
+export JAVA_HOME=/shared/common/jdk1.7.0
+export ANT_HOME=/shared/common/apache-ant-1.8.4
+export TMP_DIR=$BUILD_ROOT/tmp
+export ANT_OPTS="-Dbuild.sysclasspath=ignore -Dincludeantruntime=false"
+export MAVEN_OPTS="-Xmx3072m -XX:MaxPermSize=512m -Dtycho.localArtifacts=ignore -Djava.io.tmpdir=${TMP_DIR} ${ANT_OPTS}"
+export MAVEN_PATH=/shared/common/apache-maven-3.0.4/bin
+export PATH=$JAVA_HOME/bin:$MAVEN_PATH:$ANT_HOME/bin:$PATH
+
+env > env.txt
+
+echo "= = = = = " >> ../env.txt
+java -version  >> ../env.txt 2>&1
+ant -version >> ../env.txt
+mvn -version >> ../env.txt
 
 # 0002 is often the default for shell users, but it is not when ran from
 # a cron job, so we set it explicitly, so releng group has write access to anything
