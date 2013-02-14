cfb44233c7c16d9e1f6d58cfa26f0bc92c62468e
David Williams <david_williams@us.ibm.com>
2013-02-14 03:18:51 -0500

    Bug 400633 - Need to exit build on (bad) errors and send mail

--- production/run-maven-build.sh
+++ production/run-maven-build.sh
@@ -2,14 +2,14 @@
 #
 
 if [ $# -ne 1 ]; then
-	echo USAGE: $0 env_file
-	exit 1
+    echo USAGE: $0 env_file
+    exit 1
 fi
 
 if [ ! -r "$1" ]; then
-	echo "$1" cannot be read
-	echo USAGE: $0 env_file
-	exit 1
+    echo "$1" cannot be read
+    echo USAGE: $0 env_file
+    exit 1
 fi
 
 pushd $( dirname $0 ) >/dev/null
@@ -30,11 +30,11 @@ signingDir=$( fn-git-dir "$gitCache" "$SIGNING_REPO" )
 
 
 if [ -z "$BUILD_ID" ]; then
-	BUILD_ID=$(fn-build-id "$BUILD_TYPE" )
+    BUILD_ID=$(fn-build-id "$BUILD_TYPE" )
 fi
 
 if $SIGNING; then
-	fn-maven-signer-install "$signingDir" "$LOCAL_REPO"
+    fn-maven-signer-install "$signingDir" "$LOCAL_REPO"
 fi
 
 fn-maven-cbi-install "$aggDir" "$LOCAL_REPO"
@@ -42,20 +42,22 @@ fn-maven-parent-install "$aggDir" "$LOCAL_REPO"
 
 exitCode=$( fn-maven-build-aggregator "$BUILD_ID" "$aggDir" "$LOCAL_REPO" $COMPARATOR $SIGNING $UPDATE_BRANDING $MAVEN_BREE )
 
-    # first make sure exit code is well formed
-    if [[ "${exitCode}" =~ [0] ]]
-    then
-        echo "exitcode was empty or zero"
-        exitrc=0
-    else
-        if [[ "${exitCode}" =~ ^-?[0-9]+$ ]]  
-        then
-            echo "exitcode was a legal, non-zero numeric return code"
-            exitrc=$exitCode
-        else
-            echo "exitode was not numeric, so will force to 1"
-            exitrc=1
-        fi  
-    fi 
+# first make sure exit code is well formed
+if [[ -z "${exitCode}" ]]
+then 
+    echo "exitcode was empty"
+    exitrc=0
+elif [[ "${exitCode}" =~ [0] ]]
+then
+    echo "exitcode was zero"
+    exitrc=0
+elif [[ "${exitCode}" =~ ^-?[0-9]+$ ]]  
+then
+    echo "exitcode was a legal, non-zero numeric return code"
+    exitrc=$exitCode
+else
+    echo "exitode was not numeric, so will force to 1"
+    exitrc=1
+fi  
 
 exit $exitrc
