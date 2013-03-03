c5aa6f049a2ad3e2fa017b791bfcfb7c42e5f1b2
David Williams <david_williams@us.ibm.com>
2013-03-02 20:46:42 -0500

    Bug 402201 - updateIndexes.sh misses some cases

--- production/sdk/updateIndexes.sh
+++ production/sdk/updateIndexes.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Utility to update both 3.x and 4.x index pages
+# Utility to update both 3.x and 4.x index pages, for all build technologies
 
 # for testing, may not be in "production" location
 if [[ -f /shared/eclipse/sdk/updateIndexFilesFunction.shsource ]] 
@@ -9,9 +9,15 @@ then
 else
     source updateIndexFilesFunction.shsource
 fi
-updateIndex 3 PDE
-updateIndex 4 PDE
-updateIndex 3 CBI
-updateIndex 4 CBI
-updateIndex 3 MAIN
-updateIndex 4 MAIN
+updateIndex
+
+#updateIndex 3 PDE
+#updateIndex 4 PDE
+
+# remember, two argument case, with "CBI" 
+# is now interpreted as "MAIN". 
+#updateIndex 3 CBI
+#updateIndex 4 CBI
+
+#updateIndex 3 MAIN
+#updateIndex 4 MAIN
