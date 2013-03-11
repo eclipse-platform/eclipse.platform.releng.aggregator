73b5a11a3d3bcdb3eb6a0aec825e4eefa244fa52
David Williams <david_williams@us.ibm.com>
2013-03-10 21:54:47 -0400

    [releng] utilities for build machine (and local builds)

--- bootstrap/wgetFresh.sh
+++ bootstrap/wgetFresh.sh
@@ -17,7 +17,6 @@ wget --no-verbose --no-cache  -O mb4M.sh http://git.eclipse.org/c/platform/eclip
 wget --no-verbose --no-cache  -O mb4I.sh http://git.eclipse.org/c/platform/eclipse.platform.releng.aggregator.git/plain/bootstrap/mb4I.sh?h=$initScriptTag 2>&1;
 wget --no-verbose --no-cache  -O mb4N.sh http://git.eclipse.org/c/platform/eclipse.platform.releng.aggregator.git/plain/bootstrap/mb4N.sh?h=$initScriptTag 2>&1;
 wget --no-verbose --no-cache  -O bootstrap.sh http://git.eclipse.org/c/platform/eclipse.platform.releng.aggregator.git/plain/bootstrap/bootstrap.sh?h=$initScriptTag 2>&1;
-wget --no-verbose --no-cache  -O re-mb4I.sh http://git.eclipse.org/c/platform/eclipse.platform.releng.aggregator.git/plain/bootstrap/re-mb4I.sh?h=$initScriptTag 2>&1;
 
 
 # handy script to "wrap" a normal build script such as mb4I.sh to set global test/debug settings
