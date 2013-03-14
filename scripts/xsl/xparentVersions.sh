5c222b3f038197e455f9820589b760f4d3a7c2fa
Paul Webster <pwebster@ca.ibm.com>
2013-03-14 09:14:57 -0400

    Fix for updating all build related poms
    1) fix the <project> element so it has namespace
    2) update all packaging: pom and eclipse-repository
    3) update all poms containing a parent
    4) add forceQualifierUpdate.txt to all bundles and features and test plugins

--- scripts/xsl/xparentVersions.sh
+++ scripts/xsl/xparentVersions.sh
@@ -3,7 +3,16 @@
 
 DIR=$( dirname $0 )
 
+find * -name pom.xml -print0 | xargs -0 grep "^<project>" | cut -f1 -d: | sort -u >/tmp/t1_$$.txt
+
+for POM in $( cat /tmp/t1_$$.txt ); do
+sed 's!<project>!<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">!g' ${POM} >/tmp/out_$$.txt
+mv /tmp/out_$$.txt ${POM}
+done
+
+
 find * -name pom.xml -print0 | xargs -0 grep packaging.pom..packaging | cut -f1 -d: | sort -u >/tmp/t1_$$.txt
+find * -name pom.xml -print0 | xargs -0 grep packaging.eclipse-repository..packaging | cut -f1 -d: | sort -u >>/tmp/t1_$$.txt
 
 for POM in $( cat /tmp/t1_$$.txt ); do
 xsltproc -o "${POM}.out" $DIR/fix-pom-version.xsl "${POM}"
@@ -15,5 +24,18 @@ find * -name pom.xml | sort -u >/tmp/t1_$$.txt
 for POM in $( cat /tmp/t1_$$.txt ); do
 xsltproc -o "${POM}.out" $DIR/fix-pom-parent-version.xsl "${POM}"
 mv "${POM}.out" "${POM}"
+done
+
+find * -name pom.xml -print0 | xargs -0 grep eclipse-plugin | cut -f1 -d: | sort -u >/tmp/t1_$$.txt
+find * -name pom.xml -print0 | xargs -0 grep eclipse-feature | cut -f1 -d: | sort -u >>/tmp/t1_$$.txt
+find * -name pom.xml -print0 | xargs -0 grep eclipse-test-plugin | cut -f1 -d: | sort -u >>/tmp/t1_$$.txt
+
+
+for POM in $( cat /tmp/t1_$$.txt ); do
+POM_DIR=$( dirname $POM )
+cat >${POM_DIR}/forceQualifierUpdate.txt <<EOF
+# To force a version qualifier update add the bug here
+Bug 403237 - o.e.e4.tools cannot be build with "mvn clean install"
+EOF
 done
 
