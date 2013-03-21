845b4da2f4201706de5acfd0ead849b0518d90ba
David Williams <david_williams@us.ibm.com>
2013-03-21 17:36:46 -0400

    Bug 398256 - [CBI] distinguish N and I builds in repositories.txt

--- production/git-submodule-checkout.sh
+++ production/git-submodule-checkout.sh
@@ -5,6 +5,15 @@ REPOSITORIES_TXT="$1"; shift
 name="$1"; shift
 
 VAL=$( grep "^${name}:" "$REPOSITORIES_TXT" | cut -f2 -d" ")
+
+ # Here we count on $BUILD_TYPE being exported. TODO: make parameter later? 
+if [[ -n "$BUILD_TYPE" && "$BUILD_TYPE" == "N" ]] 
+then
+    echo "INFO: Branch forced to master, instead of $VAL, since doing N-Build"
+    VAL="master"
+fi
+
+
 if [ -z "$VAL" ]; then
 	echo No tag or branch specified for $name
 	exit
