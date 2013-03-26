bca1523dfbe04e1c8820748cdf9e69082b4bf11a
David Williams <david_williams@us.ibm.com>
2013-03-26 14:35:08 -0400

    [releng] update cleaner cronjobs

--- production/sdk/cleaners/dailyClean.sh
+++ production/sdk/cleaners/dailyClean.sh
@@ -4,30 +4,26 @@
 #       This logic, though, in theory, depending on when ran, could find say 6 builds,
 #       then remove them all if all older than 4 days.
 nbuilds=$( find /home/data/httpd/download.eclipse.org/eclipse/downloads/drops4 -maxdepth 1 -name "N*" -exec echo '{}' \; | wc -l )
-if [[ nbuilds > 4 ]]
+if [[ $nbuilds > 4 ]]
 then
     echo "Number of builds before cleaning: $nbuilds"
-    find /home/data/httpd/download.eclipse.org/eclipse/downloads/drops4 -maxdepth 1 -ctime +3 -name "N*" -exec echo '{}' \;
-    find /home/data/httpd/download.eclipse.org/eclipse/downloads/drops4 -maxdepth 1 -ctime +3 -name "N*" -exec rm -fr '{}' \;
+    find /home/data/httpd/download.eclipse.org/eclipse/downloads/drops4 -maxdepth 1 -ctime +3 -name "N*" -ls -exec rm -fr '{}' \;
     nbuilds=$( find /home/data/httpd/download.eclipse.org/eclipse/downloads/drops4 -maxdepth 1 -name "N*" -exec echo '{}' \; | wc -l )
     echo "Number of builds after cleaning: $nbuilds"
     source /shared/eclipse/sdk/updateIndexFilesFunction.shsource
-    updateIndex 4 PDE
+    updateIndex 
 
 else
     echo "Nothing cleaned, not more than 4 days"
 fi
 
 # shared (build machine)
-# can be aggressive in removing builds from "downloads", but not "updates"
-find /opt/public/eclipse/eclipse4N/siteDir/eclipse/downloads/drops4 -maxdepth 1 -ctime +1 -name "N*" -exec echo '{}' \;
-find /opt/public/eclipse/eclipse4N/siteDir/eclipse/downloads/drops4 -maxdepth 1 -ctime +1 -name "N*" -exec rm -fr '{}' \;
-
-find /opt/public/eclipse/eclipse4N/siteDir/equinox/drops -maxdepth 1 -ctime +1 -name "N*" -exec echo '{}' \;
-find /opt/public/eclipse/eclipse4N/siteDir/equinox/drops -maxdepth 1 -ctime +1 -name "N*" -exec rm -fr '{}' \;
+# can be aggressive in removing builds from "downloads" and now, with CBI build, from updates too
+#find /shared/eclipse/eclipse4N/siteDir/eclipse/downloads/drops4 -maxdepth 1 -ctime +1 -name "N*" -ls -exec rm -fr '{}' \;
+find /shared/eclipse/builds/4N/siteDir/eclipse/downloads/drops4 -maxdepth 1 -ctime +1 -name "N*" -ls -exec rm -fr '{}' \;
+find /shared/eclipse/builds/4N/siteDir/equinox/drops -maxdepth 1 -ctime +1 -name "N*" -ls -exec rm -fr '{}' \;
+find /shared/eclipse/builds/4N/siteDir/updates/4.3-N-builds -maxdepth 1 -ctime +1 -name "N*" -ls -exec rm -fr '{}' \;
 #
 # promotion scripts
-find /opt/public/eclipse/sdk/promotion/queue -name "RAN*" -ctime +2 -exec echo '{}' \;
-find /opt/public/eclipse/sdk/promotion/queue -name "RAN*" -ctime +2 -exec rm '{}' \;
-
+find /shared/eclipse/sdk/promotion/queue -name "RAN*" -ctime +2 -ls -exec rm '{}' \;
 
--- production/sdk/cleaners/weeklyClean.sh
+++ production/sdk/cleaners/weeklyClean.sh
@@ -1,38 +1,27 @@
 #!/usr/bin/env bash
 
 # clean 4.x M builds
-find /home/data/httpd/download.eclipse.org/eclipse/downloads/drops4 -maxdepth 1 -ctime +30 -name "M*" -exec echo '{}' \;
-find /home/data/httpd/download.eclipse.org/eclipse/downloads/drops4 -maxdepth 1 -ctime +30 -name "M*" -exec rm -fr '{}' \;
+find /home/data/httpd/download.eclipse.org/eclipse/downloads/drops4 -maxdepth 1 -ctime +30 -name "M*" -ls -exec rm -fr '{}' \;
 # clean 3.x M builds
-find /home/data/httpd/download.eclipse.org/eclipse/downloads/drops -maxdepth 1 -ctime +30 -name "M*" -exec echo '{}' \;
-find /home/data/httpd/download.eclipse.org/eclipse/downloads/drops -maxdepth 1 -ctime +30 -name "M*" -exec rm -fr '{}' \;
+find /home/data/httpd/download.eclipse.org/eclipse/downloads/drops -maxdepth 1 -ctime +30 -name "M*" -ls -exec rm -fr '{}' \;
 
 source /shared/eclipse/sdk/updateIndexFilesFunction.shsource
-/shared/eclipse/sdk/updateIndexes.sh
+updateIndex 
 
 
 # shared (build machine)
-# can be aggressive in removing builds from "downloads", but not "updates"
+# can be aggressive in removing builds from "downloads", and even "updates" now with new method used in CBI builds
 
-find /opt/public/eclipse/eclipse4I/siteDir/eclipse/downloads/drops4 -maxdepth 1 -ctime +2 -name "I*" -exec echo '{}' \;
-find /opt/public/eclipse/eclipse4I/siteDir/eclipse/downloads/drops4 -maxdepth 1 -ctime +2 -name "I*" -exec rm -fr '{}' \;
+find /shared/eclipse/builds/4I/siteDir/eclipse/downloads/drops4 -maxdepth 1 -ctime +2 -name "I*" -ls -exec rm -fr '{}' \;
 
-find /opt/public/eclipse/eclipse4I/siteDir/equinox/drops -maxdepth 1 -ctime +2 -name "I*" -exec echo '{}' \;
-find /opt/public/eclipse/eclipse4I/siteDir/equinox/drops -maxdepth 1 -ctime +2 -name "I*" -exec rm -fr '{}' \;
+find /shared/eclipse/builds/4I/siteDir/equinox/drops -maxdepth 1 -ctime +2 -name "I*" -ls -exec rm -fr '{}' \;
 
-find /opt/public/eclipse/eclipse4M/siteDir/eclipse/downloads/drops4 -maxdepth 1 -ctime +2 -name "M*" -exec echo '{}' \;
-find /opt/public/eclipse/eclipse4M/siteDir/eclipse/downloads/drops4 -maxdepth 1 -ctime +2 -name "M*" -exec rm -fr '{}' \;
-find /opt/public/eclipse/eclipse4M/siteDir/equinox/drops -maxdepth 1 -ctime +2 -name "M*" -exec echo '{}' \;
-find /opt/public/eclipse/eclipse4M/siteDir/equinox/drops -maxdepth 1 -ctime +2 -name "M*" -exec rm -fr '{}' \;
+find /shared/eclipse/builds/4I/siteDir/updates/4.3-I-builds -maxdepth 1 -ctime +2 -name "I*" -ls -exec rm -fr '{}' \;
 
-find /opt/public/eclipse/eclipse3M/siteDir/eclipse/downloads/drops -maxdepth 1 -name "M*" | wc -l
-find /opt/public/eclipse/eclipse3M/siteDir/eclipse/downloads/drops -maxdepth 1 -ctime +2 -name "M*" -exec echo '{}' \;
-find /opt/public/eclipse/eclipse3M/siteDir/eclipse/downloads/drops -maxdepth 1 -ctime +2 -name "M*" -exec rm -fr '{}' \;
-find /opt/public/eclipse/eclipse3M/siteDir/eclipse/downloads/drops -maxdepth 1 -name "M*" | wc -l
+# don't really need these yet, until after Kepler 
+find /shared/eclipse/builds/4M/siteDir/eclipse/downloads/drops4 -maxdepth 1 -ctime +2 -name "M*" -ls -exec rm -fr '{}' \;
+find /shared/eclipse/builds/4M/siteDir/equinox/drops -maxdepth 1 -ctime +2 -name "M*" -ls -exec rm -fr '{}' \;
+find /shared/eclipse/builds/4M/siteDir/updates/4.3-M-builds -maxdepth 1 -ctime +2 -name "M*" -ls -exec rm -fr '{}' \;
 
-find /opt/public/eclipse/eclipse3M/siteDir/equinox/drops3 -maxdepth 1 -name "M*" | wc -l
-find /opt/public/eclipse/eclipse3M/siteDir/equinox/drops3 -maxdepth 1 -ctime +2 -name "M*" -exec echo '{}' \;
-find /opt/public/eclipse/eclipse3M/siteDir/equinox/drops3 -maxdepth 1 -ctime +2 -name "M*" -exec rm -fr '{}' \;
-find /opt/public/eclipse/eclipse3M/siteDir/equinox/drops3 -maxdepth 1 -name "M*" | wc -l
 
 
