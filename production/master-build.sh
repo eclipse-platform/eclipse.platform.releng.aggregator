0e3bca777c5288f57341ecaafcad9e58edd8b992
David Williams <david_williams@us.ibm.com>
2013-03-27 21:52:01 -0400

    Bug 400633 - Need to exit build on (bad) errors and send mail

--- production/master-build.sh
+++ production/master-build.sh
@@ -120,7 +120,9 @@ $SCRIPT_PATH/get-aggregator.sh $BUILD_ENV_FILE 2>&1 | tee ${GET_AGGREGATOR_BUILD
 if [[ -f "${buildDirectory}/buildFailed-get-aggregator" ]]
 then
     getAggregatorFailed=true
-    /bin/grep "\[ERROR\]" "${GET_AGGREGATOR_BUILD_LOG}" >> "${buildDirectory}/buildFailed-get-aggregator"
+    # Git sometimes returns non-zero error codes for "warnings". 
+    # In most cases, but not all, these would be considered errors. 
+    /bin/grep  "^warning: \|\[ERROR\]"  "${GET_AGGREGATOR_BUILD_LOG}" >> "${buildDirectory}/buildFailed-get-aggregator"
     BUILD_FAILED="${GET_AGGREGATOR_BUILD_LOG}"
     fn-write-property BUILD_FAILED
 fi
