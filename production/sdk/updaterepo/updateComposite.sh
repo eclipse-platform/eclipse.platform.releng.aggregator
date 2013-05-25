ca2dd744f6d09c12814389a853a3a3697dd8217d
David Williams <david_williams@us.ibm.com>
2013-05-25 02:07:34 -0400

    [releng] files to cleanup repos

--- production/sdk/updaterepo/updateComposite.sh
+++ production/sdk/updaterepo/updateComposite.sh
@@ -6,9 +6,10 @@
 
 #JAVA_5_HOME=${JAVA_5_HOME:-/home/shared/orbit/apps/ibm-java2-i386-50/jre}
 JAVA_5_HOME=${JAVA_5_HOME:-/shared/common/jdk-1.5.0-22.x86_64/jre}
-JAVA_6_HOME=${JAVA_6HOME:-/shared/common/sun-jdk1.6.0_21_x64}
+JAVA_6_HOME=${JAVA_6_HOME:-/shared/common/sun-jdk1.6.0_21_x64}
+JAVA_7_HOME=${JAVA_7_HOME:-/shared/common/jdk1.7.0_11}
 
-export JAVA_HOME=${JAVA_6_HOME}
+export JAVA_HOME=${JAVA_7_HOME}
 
 devJRE="${JAVA_HOME}"/bin/java
 
@@ -24,7 +25,7 @@ fi
 
 
 # remember, the eclispe install must match the VM used (e.g. both 64 bit, both 32 bit, etc).
-ECLIPSE_EXE=${ECLIPSE_EXE:-/shared/eclipse/eclipsesdk372/eclipse/eclipse}
+ECLIPSE_EXE=${ECLIPSE_EXE:-/shared/eclipse/sdk/eclipse43RC2/eclipse/eclipse}
 
 if [ ! -n ${ECLIPSE_EXE} -a -x ${ECLIPSE_EXE} ]
 then
@@ -35,12 +36,12 @@ fi
 BUILDFILE=$1
 if [[ -z "${BUILDFILE}" ]]
 then
-    printf "/n/t%s/t%s/n" "ERROR" "must provide ant file to perform composite update as first argument"
+    printf "\n\t%s\t%s\n" "ERROR" "must provide ant file to perform composite update as first argument"
     exit 1
 fi
 if [[ ! -e "${BUILDFILE}" ]]
 then
-    print "/n/t%s/t%s/n" "ERROR" "BUILDFILE does not exist: ${BUILDFILE}"
+    printf "\n\t%s\t%s\n" "ERROR" "BUILDFILE does not exist: ${BUILDFILE}"
     exit 1
 fi
 BUILDFILESTR="-f ${BUILDFILE}"
@@ -48,27 +49,27 @@ BUILDFILESTR="-f ${BUILDFILE}"
 repodir=$2
 if [[ -z "${repodir}" ]]
 then
-    print "/n/t%s/t%s/n" "ERROR" "repodir must be specified as second argument"
+    printf "\n\t%s\t%s\n" "ERROR" "repodir must be specified as second argument"
     exit 1
 fi
 if [[ ! -e "${repodir}" ]]
 then
-    print "/n/t%s/t%s/n" "ERROR" "repodir does not exist: ${repodir}"
+    printf "\n\t%s\t%s\n" "ERROR" "repodir does not exist: ${repodir}"
     exit 1
+else
+    REPODIRSTR="-Drepodir=${repodir}"
 fi
 
 complocation=$3
 if [[ -z "${complocation}" ]]
 then
-    print "/n/t%s/t%s/n" "ERROR" "complocation must be specified as third argument"
-    exit 1
+    printf "\n\t%s\t%s\n" "WARNING" "complocation not specified. Assumed to be in ant xml file."
+else
+    COMPLOCATIONSTR="-Dcomplocation=${complocation}"
 fi
 
 
-REPODIRSTR="-Drepodir=${repodir}"
-COMPLOCATION="-Dcomplocation=${complocation}"
-
-devArgs="$devArgs $REPODIRSTR $COMPLOCATION"
+devArgs="$devArgs $REPODIRSTR $COMPLOCATIONSTR"
 
 devworkspace=workspace-updateComposite
 
@@ -82,9 +83,12 @@ fi
 
 # sanity check existence of directory or else risk removing wrong stuff
 compdir="${repodir}/${complocation}"
-if [[ -d ${compdir} ]]
+if [[ -n {$complocation} && -d ${compdir} ]]
 then
-    rm -fr "${compdir}"
+    # comment out/echo for now, since we don't expect to use 
+    # this and echo can serve as a test if working as intended. 
+    # otherwise, we'd remove whole repo! 
+    echo "rm -fr ${compdir}"
     RC=$?
     if [[ $RC != 0 ]]
     then
@@ -92,8 +96,7 @@ then
         exit $RC
     fi
 else
-    echo "ERROR: expected directory did not exist: ${compdir}"
-    exit 1
+    echo "WARNING: expected complocation directory did not exist. Nothing removed. Assumed done in ant xml file or remove manually."
 fi
 
 exit 0
