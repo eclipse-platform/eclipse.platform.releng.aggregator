88ae409a987704e00a8497614662c31ab3e1e6d5
David Williams <david_williams@us.ibm.com>
2013-06-04 17:01:37 -0400

    Bug 409817 - All API Tools reports are missing
    work in test branch: david_williams/apitooling

--- bootstrap/mb4I.sh
+++ bootstrap/mb4I.sh
@@ -69,7 +69,7 @@ export BUILD_HOME=${BUILD_HOME:-/shared/eclipse/builds}
 # no override for minimal $PATH
 #export PATH=$JAVA_HOME/bin:$MAVEN_PATH:$ANT_HOME/bin:$PATH
 
-export BRANCH=master
+export BRANCH=david_williams/apitooling
 export BUILD_TYPE=I
 export STREAM=4.3.0
 
