ec91014119bce40fb9ee2d647b854d6880b42b80
David Williams <david_williams@us.ibm.com>
2014-03-07 09:11:52 -0500

    [releng] prep for M6 promotion

--- production/sdk/promoteStableRelease/promoteLuna/promoteSites.sh
+++ production/sdk/promoteStableRelease/promoteLuna/promoteSites.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-export DROP_ID=I20140123-1600
+export DROP_ID=I20140306-1200
 
-export DL_LABEL=4.4M5
-export DL_LABEL_EQ=LunaM5
+export DL_LABEL=4.4M6
+export DL_LABEL_EQ=LunaM6
 
-# for I builds, stable and RCs to in milestones
+# for I builds, stable and RCs go to in milestones
 export REPO_SITE_SEGMENT=4.4milestones
 #REPO_SITE_SEGMENT=4.4
 
@@ -22,7 +22,7 @@ export DL_TYPE=S
 
 # variables used on tagging aggregator for milestones (and RCs?) 
 # Could probably compute this tag ... but for now easier to type it in each time. 
-export NEW_TAG=S4_4_0_M5
+export NEW_TAG=S4_4_0_M6
 # For now, we'll just use handy Equinox label for tag annotation, but could elaborate in future
 export NEW_ANNOTATION="${DL_LABEL_EQ}"
 # later combined with BUILD_ROOT, so we get the correct clone
