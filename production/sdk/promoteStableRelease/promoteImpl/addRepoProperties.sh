d464ba67649d7bc927b96a4ef012882ee25a2557
David Williams <david_williams@us.ibm.com>
2014-03-28 15:38:38 -0400

    Bug 431478 - add p2.statsURI to Java 8 patch feature site

--- production/sdk/promoteStableRelease/promoteImpl/addRepoProperties.sh
+++ production/sdk/promoteStableRelease/promoteImpl/addRepoProperties.sh
@@ -6,18 +6,26 @@
 
 export PROMOTE_IMPL=${PROMOTE_IMPL:-/shared/eclipse/sdk/promoteStableRelease/promoteImpl}
 
-source ${PROMOTE_IMPL}/promoteUtilities.shsource
-source promoteUtilities.shsource
-
 APP_NAME=org.eclipse.wtp.releng.tools.addRepoProperties
 
 devworkspace=${devworkspace:-${PWD}/workspaceAddRepoProperties}
 
-REPO=$1
-REPO_TYPE=$2
-BUILD_ID=$3
-STATS_TAG_VERSIONINDICATOR=$4
-STATS_TAG_SUFFIX=$5
+
+export REPO=$1
+export REPO_TYPE=$2
+export BUILD_ID=$3
+export STATS_TAG_FEATURE_LIST=$4
+#export STATS_TAG_VERSIONINDICATOR=$5
+export STATS_TAG_SUFFIX=$5
+
+source promoteUtilities.shsource
+
+
+echo "REPO: ${REPO}"
+echo "REPO_TYPE: ${REPO_TYPE}"
+echo "BUILD_ID: ${BUILD_ID}"
+echo "STATS_TAG_FEATURE_LIST=${STATS_TAG_FEATURE_LIST}"
+echo "STATS_TAG_SUFFIX: ${STATS_TAG_SUFFIX}"
 
 if [[ -z "${REPO}" ]]
 then
@@ -25,11 +33,6 @@ then
     exit 1
 fi
 
-echo "REPO: ${REPO}"
-echo "REPO_TYPE: ${REPO_TYPE}"
-echo "BUILD_ID: ${BUILD_ID}"
-echo "STATS_TAG_VERSIONINDICATOR: ${STATS_TAG_VERSIONINDICATOR}"
-echo "STATS_TAG_SUFFIX: ${STATS_TAG_SUFFIX}"
 
 if [[ -z "${REPO_TYPE}" || -z "${BUILD_ID}"  ]]
 then 
@@ -39,7 +42,7 @@ else
     MIRRORURL="/eclipse/updates/${REPO_TYPE}/${BUILD_ID}"
 fi
 
-if [ ! -z $MIRRORURL ] 
+if [[ ! -z "${MIRRORURL}" ]] 
 then 
     # remember, the '&' should NOT be unescaped here ... the p2 api (or underlying xml) will escape it. 
     MIRRORURL_ARG="http://www.eclipse.org/downloads/download.php?format=xml&file=${MIRRORURL}"
@@ -52,16 +55,23 @@ fi
 ART_REPO_NAME="Eclipse Project Java 8 Patch Repository for Kepler SR2"
 CON_REPO_NAME="Eclipse Project Java 8 Patch Repository for Kepler SR2"
 
-MIRRORS_URL_ARG="-Dp2MirrorsURL=${MIRRORURL_ARG}"
+MIRRORS_URL_ARG="-Dp2MirrorsURL=${MIRRORURL}"
 ART_REPO_ARG="-DartifactRepoDirectory=${REPO}"
 CON_REPO_ARG="-DmetadataRepoDirectory=${REPO}"
 ART_REPO_NAME_ARG="-Dp2ArtifactRepositoryName=${ART_REPO_NAME}"
 CON_REPO_NAME_ARG="-Dp2MetadataRepositoryName=${CON_REPO_NAME}"
 
-# not currently used
-#-Dp2StatsURI=http://download.eclipse.org/stats/eclipse/updates${STATS_TAG_VERSIONINDICATOR} -DstatsArtifactsSuffix="${STATS_TAG_SUFFIX}" -DstatsTrackedArtifacts=org.eclipse.wst.jsdt.feature,org.eclipse.wst.xml_ui.feature,org.eclipse.wst.web_ui.feature,org.eclipse.jst.enterprise_ui.feature"
--Dp2StatsURI=http://download.eclipse.org/stats/eclipse/updates${STATS_TAG_VERSIONINDICATOR} -DstatsArtifactsSuffix="${STATS_TAG_SUFFIX}" -DstatsTrackedArtifacts=org.eclipse.jdt.java8patch"
-
+if [[ -n "${STATS_TAG_FEATURE_LIST}" ]]
+then
+    STATS_TAG_FEATURE_LIST_ARG="-DstatsTrackedArtifacts=${STATS_TAG_FEATURE_LIST}"
+    # no sense setting these others, if features not set
+    # TODO: more error checking could be done, to warn, such if features list set, but other values not
+    STATS_TAG_VERSIONINDICATOR_ARG="-Dp2StatsURI=http://download.eclipse.org/stats${MIRRORURL}"
+    if [[ -n "${STATS_TAG_SUFFIX}" ]]
+    then 
+        STATS_TAG_SUFFIX_ARG="-DstatsArtifactsSuffix=${STATS_TAG_SUFFIX}"
+    fi
+fi
 
 echo "dev:               ${BASH_SOURCE}"
 echo "devworkspace:      ${devworkspace}"
@@ -72,12 +82,34 @@ echo "ART_REPO_ARG:      ${ART_REPO_ARG}"
 echo "CON_REPO_ARG:      ${CON_REPO_ARG}"
 echo "ART_REPO_NAME_ARG: ${ART_REPO_NAME_ARG}"
 echo "CON_REPO_NAME_ARG: ${CON_REPO_NAME_ARG}"
+echo "STATS_TAG_FEATURE_LIST_ARG: ${STATS_TAG_FEATURE_LIST_ARG}"
+echo "STATS_TAG_VERSIONINDICATOR_ARG: ${STATS_TAG_VERSIONINDICATOR_ARG}"
+echo "STATS_TAG_SUFFIX_ARG: ${STATS_TAG_SUFFIX_ARG}"
 
-echo
-$JAVA_CMD -version
-echo
+if [[ -x ${JAVA_CMD} ]]
+then 
+    echo
+    $JAVA_CMD -version
+    echo
 
-${ECLIPSE_EXE} -nosplash -consolelog -debug -data ${devworkspace} -vm ${JAVA_EXEC_DIR} -application ${APP_NAME}  -vmargs "${MIRRORS_URL_ARG}" "${ART_REPO_ARG}" "${CON_REPO_ARG}" "${ART_REPO_NAME_ARG}" "${CON_REPO_NAME_ARG}"
-RC=$?
-
+    findEclipseExe $BUILD_ID
+    RC=$?
+    if [[ $RC != 0 ]]
+    then
+        echo "non zero return code returned from findEclipse: $RC"
+    else 
+        if [[ -x ${ECLIPSE_EXE} ]]
+        then
+            ${ECLIPSE_EXE} --launcher.suppressErrors -nosplash -consolelog -debug -data ${devworkspace} -vm ${JAVA_EXEC_DIR} -application ${APP_NAME} -vmargs "${MIRRORS_URL_ARG}" "${ART_REPO_ARG}" "${CON_REPO_ARG}" "${ART_REPO_NAME_ARG}" "${CON_REPO_NAME_ARG}" "${STATS_TAG_FEATURE_LIST_ARG}" "${STATS_TAG_VERSIONINDICATOR_ARG}" "${STATS_TAG_SUFFIX_ARG}"
+            RC=$?
+        else
+            printf "\n\tERROR: %s\n\n" "The Eclipse commmand, ${ECLIPSE_EXE}, was not executable or not specified"
+            RC=1
+        fi
+    fi
+else
+    printf "\n\tERROR: %s\n\n" "The Java commmand, ${JAVA_CMD}, was not executable or not specified"
+    RC=1
+fi
+echo "RC: $RC"
 exit $RC
--- production/sdk/promoteStableRelease/promoteImpl/addRepoPropertiesStandalone.sh
+++ production/sdk/promoteStableRelease/promoteImpl/addRepoPropertiesStandalone.sh
@@ -6,9 +6,19 @@
 # This "standalone" version is to add the stats property "after the fact". 
 # It is not used in the automated "promoteRepos". 
 
+export BUILD_ROOT=${BUILD_ROOT:-/shared/eclipse/builds/4P}
 
-./addRepoProperties.sh \
-  file:///home/data/users/david_williams/downloads/eclipse/updates/4.3-P-builds/
-  4.3-P-builds \
-  P20140317-1600 \
-  1.0.0.v20140317-1956
+printf "\n\tDEBUG: %s\n" "BUILD_ROOT: ${BUILD_ROOT}"
+
+#./addRepoProperties.sh \
+#  file:///home/data/users/david_williams/downloads/eclipse/updates/4.3-P-builds/
+#  4.3-P-builds \
+#  P20140317-1600 \
+#  1.0.0.v20140317-1956
+./addRepoProperties.sh\
+  "/data/httpd/download.eclipse.org/eclipse/updates/4.3-P-builds/P20140311-1530"\
+  "4.3-P-builds"\
+  "P20140220-1424"\
+  "org.eclipse.jdt.java8patch"\
+  "_1.0.0.v20140220-1916"
+
