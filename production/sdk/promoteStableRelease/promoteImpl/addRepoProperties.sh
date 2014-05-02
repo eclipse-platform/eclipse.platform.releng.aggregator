c2f9f4af18bd57af589264d545035846e9413c36
David Williams <david_williams@us.ibm.com>
2014-05-02 02:01:18 -0400

    [releng] prep for M7 promotion

--- production/sdk/promoteStableRelease/promoteImpl/addRepoProperties.sh
+++ production/sdk/promoteStableRelease/promoteImpl/addRepoProperties.sh
@@ -39,12 +39,12 @@ then
     echo "WARNING: no mirror URL specified.";
     MIRRORURL=""
 else
-    MIRRORURL="/eclipse/updates/${REPO_TYPE}/${BUILD_ID}"
+    MIRRORURL=/eclipse/updates/${REPO_TYPE}/${BUILD_ID}
 fi
 
 if [[ ! -z "${MIRRORURL}" ]] 
 then 
-    # remember, the '&' should NOT be unescaped here ... the p2 api (or underlying xml) will escape it. 
+    # remember, the '&' should NOT be XML escaped here ... the p2 api (or underlying xml) will escape it. 
     MIRRORURL_ARG="http://www.eclipse.org/downloads/download.php?format=xml&file=${MIRRORURL}"
 else
     MIRRORURL_ARG=""
@@ -53,24 +53,24 @@ fi
 # TODO: control with variable!
 ART_REPO_NAME="Eclipse Project Repository for ${TRAIN_NAME}"
 CON_REPO_NAME="Eclipse Project Repository for ${TRAIN_NAME}"
-#ART_REPO_NAME="Eclipse Project Java 8 Patch Repository for Kepler SR2"
-#CON_REPO_NAME="Eclipse Project Java 8 Patch Repository for Kepler SR2"
+#ART_REPO_NAME=Eclipse Project Java 8 Patch Repository for Kepler SR2
+#CON_REPO_NAME=Eclipse Project Java 8 Patch Repository for Kepler SR2
 
-MIRRORS_URL_ARG="-Dp2MirrorsURL=${MIRRORURL_ARG}"
-ART_REPO_ARG="-DartifactRepoDirectory=${REPO}"
-CON_REPO_ARG="-DmetadataRepoDirectory=${REPO}"
-ART_REPO_NAME_ARG="-Dp2ArtifactRepositoryName=${ART_REPO_NAME}"
-CON_REPO_NAME_ARG="-Dp2MetadataRepositoryName=${CON_REPO_NAME}"
+MIRRORS_URL_ARG=-Dp2MirrorsURL=${MIRRORURL_ARG}
+ART_REPO_ARG=-DartifactRepoDirectory=${REPO}
+CON_REPO_ARG=-DmetadataRepoDirectory=${REPO}
+ART_REPO_NAME_ARG=-Dp2ArtifactRepositoryName=\"${ART_REPO_NAME}\"
+CON_REPO_NAME_ARG=-Dp2MetadataRepositoryName=\"${CON_REPO_NAME}\"
 
 if [[ -n "${STATS_TAG_FEATURE_LIST}" ]]
 then
-    STATS_TAG_FEATURE_LIST_ARG="-DstatsTrackedArtifacts=${STATS_TAG_FEATURE_LIST}"
+    STATS_TAG_FEATURE_LIST_ARG="-DstatsTrackedArtifacts=\"${STATS_TAG_FEATURE_LIST}\""
     # no sense setting these others, if features not set
     # TODO: more error checking could be done, to warn, such if features list set, but other values not
-    STATS_TAG_VERSIONINDICATOR_ARG="-Dp2StatsURI=http://download.eclipse.org/stats${MIRRORURL}"
-    if [[ -n "${STATS_TAG_SUFFIX}" ]]
+    STATS_TAG_VERSIONINDICATOR_ARG="-Dp2StatsURI=\"http://download.eclipse.org/stats${MIRRORURL}\""
+    if [[ -n ${STATS_TAG_SUFFIX} ]]
     then 
-        STATS_TAG_SUFFIX_ARG="-DstatsArtifactsSuffix=${STATS_TAG_SUFFIX}"
+        STATS_TAG_SUFFIX_ARG="-DstatsArtifactsSuffix=\"${STATS_TAG_SUFFIX}\""
     fi
 fi
 
@@ -78,9 +78,13 @@ echo "dev:               ${BASH_SOURCE}"
 echo "devworkspace:      ${devworkspace}"
 echo "JAVA_EXEC_DIR:     ${JAVA_EXEC_DIR}"
 echo "APP_NAME:          ${APP_NAME}"
+echo "MIRRORURL:         ${MIRRORURL}"
+echo "MIRRORURL_ARG:     ${MIRRORURL_ARG}"
 echo "MIRRORS_URL_ARG:   ${MIRRORS_URL_ARG}"
 echo "ART_REPO_ARG:      ${ART_REPO_ARG}"
 echo "CON_REPO_ARG:      ${CON_REPO_ARG}"
+echo "ART_REPO_NAME:     ${ART_REPO_NAME}"
+echo "CON_REPO_NAME:     ${CON_REPO_NAME}"
 echo "ART_REPO_NAME_ARG: ${ART_REPO_NAME_ARG}"
 echo "CON_REPO_NAME_ARG: ${CON_REPO_NAME_ARG}"
 echo "STATS_TAG_FEATURE_LIST_ARG: ${STATS_TAG_FEATURE_LIST_ARG}"
@@ -101,8 +105,11 @@ then
     else 
         if [[ -x ${ECLIPSE_EXE} ]]
         then
-            # we 'clean' here, since using an installation that has been moved.
-            ${ECLIPSE_EXE} -clean --launcher.suppressErrors -nosplash -consolelog -debug -data ${devworkspace} -vm ${JAVA_EXEC_DIR} -application ${APP_NAME} -vmargs "${MIRRORS_URL_ARG}" "${ART_REPO_ARG}" "${CON_REPO_ARG}" "${ART_REPO_NAME_ARG}" "${CON_REPO_NAME_ARG}" "${STATS_TAG_FEATURE_LIST_ARG}" "${STATS_TAG_VERSIONINDICATOR_ARG}" "${STATS_TAG_SUFFIX_ARG}"
+            echo "found eclipse executable, will execute:"
+            echo "${ECLIPSE_EXE} --launcher.suppressErrors -nosplash -consolelog -debug -data ${devworkspace} -application ${APP_NAME} -vm ${JAVA_EXEC_DIR} -vmargs ${MIRRORS_URL_ARG} ${ART_REPO_ARG} ${CON_REPO_ARG} ${ART_REPO_NAME_ARG} ${CON_REPO_NAME_ARG} ${STATS_TAG_FEATURE_LIST_ARG} ${STATS_TAG_VERSIONINDICATOR_ARG} ${STATS_TAG_SUFFIX_ARG}"
+            # we may need to 'clean' here, since using an installation that has been moved?
+            #${ECLIPSE_EXE} --launcher.suppressErrors -nosplash -consolelog -debug -data ${devworkspace} -application ${APP_NAME} -vm ${JAVA_EXEC_DIR} -vmargs ${MIRRORS_URL_ARG} ${ART_REPO_ARG} ${CON_REPO_ARG} ${ART_REPO_NAME_ARG} ${CON_REPO_NAME_ARG} ${STATS_TAG_FEATURE_LIST_ARG} ${STATS_TAG_VERSIONINDICATOR_ARG} ${STATS_TAG_SUFFIX_ARG}
+            ${ECLIPSE_EXE} --launcher.suppressErrors -nosplash -consolelog -debug -data ${devworkspace} -application ${APP_NAME} -vm ${JAVA_EXEC_DIR} -vmargs ${MIRRORS_URL_ARG} -Dp2ArtifactRepositoryName="${ART_REPO_NAME}" -Dp2MetadataRepositoryName="${CON_REPO_NAME}" ${ART_REPO_ARG} ${CON_REPO_ARG}
             RC=$?
         else
             printf "\n\tERROR: %s\n\n" "The Eclipse commmand, ${ECLIPSE_EXE}, was not executable or not specified"
