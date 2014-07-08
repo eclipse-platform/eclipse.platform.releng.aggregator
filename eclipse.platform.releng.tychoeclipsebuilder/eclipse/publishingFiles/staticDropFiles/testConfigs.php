4963b1d0b591a7a7eae80682d0a9780e0a12b844
David Williams <david_williams@us.ibm.com>
2014-07-08 05:44:33 -0400

    Bug 387066 - Remove Red X's (and the few green checkmarks) from main DL page
    Changes to "main" script, to make "test platform names" more variable.

---
+++ eclipse.platform.releng.tychoeclipsebuilder/eclipse/publishingFiles/staticDropFiles/testConfigs.php
@@ -0,0 +1,9 @@
+<?php
+
+// This file is "static" for now, but eventually may compute and upload
+// this file more dynamically.
+
+$expectedTestConfigs = array();
+$expectedTestConfigs[]="linux.gtk.x86_64_8.0";
+$expectedTestConfigs[]="macosx.cocoa.x86_64_7.0";
+$expectedTestConfigs[]="win32.win32.x86_7.0";
