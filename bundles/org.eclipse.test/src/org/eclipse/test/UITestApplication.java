466ba5c95611abf0f7bd1321605596a8e346d851
Szymon Ptaszkiewicz <szymon.ptaszkiewicz@pl.ibm.com>
2014-07-09 18:23:01 +0200

    Bug 439267 - Remove dangling references to org.eclipse.core.boot
    Change-Id: Icf7b67cb06e167f629feefa8980a13677c4bbc28
    Signed-off-by: Szymon Ptaszkiewicz <szymon.ptaszkiewicz@pl.ibm.com>

--- bundles/org.eclipse.test/src/org/eclipse/test/UITestApplication.java
+++ bundles/org.eclipse.test/src/org/eclipse/test/UITestApplication.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -47,7 +47,7 @@ public class UITestApplication  implements IPlatformRunnable, ITestHarness, IApp
 	
 	
 	/* (non-Javadoc)
-	 * @see org.eclipse.core.boot.IPlatformRunnable
+	 * @see org.eclipse.core.runtime.IPlatformRunnable
 	 */
 	public Object run(final Object args) throws Exception {
 		// Get the application to test
