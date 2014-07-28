5f231418d0d55ee840ed8f1c4e2662061afa27e6
David Williams <david_williams@us.ibm.com>
2014-07-28 15:10:23 -0400

    [releng] add small "test build" directory

---
+++ production/buildScriptsUnitTests/testAssertNotEmpty2.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+source bashUtilities.shsource
+
+T1="some value"
+assertNotEmpty T1
+
+echo "Should see this line"  
+
+function testDiagMsg
+{
+  assertNotEmpty T2
+  echo "Should NOT see this line"
+}
+
+testDiagMsg
+
