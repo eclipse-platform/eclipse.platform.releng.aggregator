3864623e3401ac13d0f2e22bb2185fec966bf239
David Williams <david_williams@us.ibm.com>
2014-08-27 10:50:50 -0400

    Bug 441889 - refactor "production test scripts"

--- production/testScripts/configuration/sdk.tests/testConfigs/linux/testAll.sh
+++ production/testScripts/configuration/sdk.tests/testConfigs/linux/testAll.sh
@@ -1,32 +1,58 @@
 #!/usr/bin/env bash
 
+echo "command line as passed into $(basename ${0}): ${*}"
+echo "command line (quoted) as passed into $(basename ${0}): ${@}"
 
-# This file should never exist or be needed for production machine,
+# This file, localTestsProperties.shsource, should never exist or be needed for production machine,
 # but allows an easy way for a "local user" to provide this file
 # somewhere on the search path ($HOME/bin is common),
 # and it will be included here, thus can provide "override values"
 # to those defined by defaults for production machine.,
-# such as for vmcmd
-
+# such as for jvm
 source localTestsProperties.shsource 2>/dev/null
 
+if [[ -z "${propertyFile}" ]]
+then
+   echo "expect property file as environment variable for production runs"
+   exit 1
+fi
+
+if [[ -z "${jvm}" ]]
+then
+   echo "expect jvm as environment variable for production runs"
+   exit 1
+fi
+if [[ -z "${testedPlatform}" ]]
+then
+   echo "expect testedPlatform as environment variable for production runs"
+   exit 1
+fi
+
 echo "PWD: $PWD"
-vmcmd=${vmcmd:-/shared/common/jdk1.8.0_x64-latest/jre/bin/java}
+# in production tests, should already be set by runTests2.xml, so 
+# we set to an old version here, to make obvious if not. 
+export jvm=${jvm:-/shared/common/jdk-1.6.x86_64/jre/bin/java}
 
 # production machine is x86_64, but some local setups may be 32 bit and will need to provide
-# this value in localTestsProperties.shsource.
+# this value in localTestsProperties.shsource. (
 eclipseArch=${eclipseArch:-x86_64}
 
-# vm.properties is used by default on production machines, but will
-# need to override on local setups to specify appropriate vm (usually same as vmcmd).
+# production.properties is used in production tests, 
+# need to override on local setups to specify appropriate vm (usually same as jvm).
 # see bug 388269
-propertyFile=${propertyFile:-vm.properties}
+export propertyFile=${propertyFile:-platformSpecific.properties}
 
-echo "vmcmd in testAll: ${vmcmd}"
-echo "extdir in testAll (if any): ${extdir}"
-echo "propertyFile in testAll: ${propertyFile}"
-echo "buildId in testAll: ${buildId}"
+# in product tests, should be set by runTests2.xml, 
+# so we use "vm value",  "x.0" at end, to make obvious if that's not working.
+export testedPlatform=${testedPlatform:-linux.gtk.x86_64_x.0}
 
+echo "=== properties in testAll.sh"
+echo "    jvm in testAll: ${jvm}"
+echo "    extdir in testAll (if any): ${extdir}"
+echo "    propertyFile in testAll: ${propertyFile}"
+echo "    buildId in testAll: ${buildId}"
+echo "    testedPlatform: ${testedPlatform}"
+echo "    ANT_OPTS: ${ANT_OPTS}"
 
 #execute command to run tests
 /bin/chmod 755 runtests.sh
@@ -34,9 +60,8 @@ echo "buildId in testAll: ${buildId}"
 
 if [[ -n "${extdir}" ]]
 then
-  ./runtests.sh -os linux -ws gtk -arch $eclipseArch -extdirprop "${extdir}" -vm "${vmcmd}" -properties ${propertyFile} $* > results/consolelogs/linux.gtk.x86_64_8.0_consolelog.txt
+  ./runtests.sh -os linux -ws gtk -arch $eclipseArch -extdirprop "${extdir}" -vm "${jvm}"  -properties ${propertyFile} "${@}"  > results/consolelogs/${testedPlatform}_consolelog.txt
 else
-  ./runtests.sh -os linux -ws gtk -arch $eclipseArch -vm "${vmcmd}" -properties ${propertyFile} $* > results/consolelogs/linux.gtk.x86_64_8.0_consolelog.txt
+  ./runtests.sh -os linux -ws gtk -arch $eclipseArch -vm "${jvm}" -properties ${propertyFile} "${@}" > results/consolelogs/${testedPlatform}_consolelog.txt
 fi
-
 
---
+++ production/testScripts/test_runTests2.xml.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+
+
+#   Utility to test the runTests2.xml file. It's intended to allow rapid confirmation
+#   that the runTests2.xml file computes correct values, for various versions platforms
+#   and various versions of java and ant, etc. 
+
+#   In particular, ant can be invoked with one version of java, but another version
+#   of java used to actually run the tests.
+
+#   In addition to the console output. which should be captured, with something like
+#      ./testTextXMLScript.sh | tee testoutput.txt
+#   some data or set-up files will be created as they normally would if running the real tests.
+#   The can be checked to be sure they are correct, and in correct (relative) locations.
+
+# unset some common variables which we don't want (or, set ourselves)
+unset JAVA_HOME
+unset JAVA_ROOT
+unset JAVA_JRE
+unset CLASSPATH
+unset JAVA_BINDIR
+unset JRE_HOME
+
+function clean()
+{
+
+  echo -e "\n Cleaning previous results, as requested."
+  # remember, do not want to rm "out.txt" since we are currently piping to it via 'tee'.
+  rm -fr eclipse.platform.releng.aggregator/ eclipse.platform.releng.aggregator-master.zip eclipse.platform.releng.basebuilder-R38M6PlusRC3G/ getEBuilder.xml org.eclipse.releng.basebuilder/ runTests2.xml tempEBuilder/
+
+  rm -fr production.properties propertiesAllFromRunTest2.properties sdk.tests streamSpecific-build.properties workarea
+
+}
+
+source localbuildproperties.shsource
+#   Different versions of Ant are specified here in test script, just to confirm
+#   nothing is specific to any recent version of ant. (Though, some of the machines
+#   have ant 1.6 set as 'default'!)
+#export ANT_HOME=/shared/common/apache-ant-1.7.1
+export ANT_HOME=/shared/common/apache-ant-1.8.4/
+#export ANT_HOME=/shared/common/apache-ant-1.9.2
+
+#   JAVA_HOME is, at least, what runs the ant instance. If no 'jvm' option is specified,
+#   it also becomes the instance that runs the tests.
+#export JAVA_HOME=/shared/common/jdk1.5.0-latest
+#export JAVA_HOME=/shared/common/jdk1.6.0-latest
+#export JAVA_HOME=/shared/common/jdk1.7.0-latest
+#export JAVA_HOME=/shared/common/jdk1.8.0_x64-latest
+export JAVA_HOME=/shared/common/jdk1.7.0-latest
+export WORKSPACE=${HOME}/tempworkarea/
+export PATH=${JAVA_HOME}/bin:${ANT_HOME}/bin:/usr/local/bin:/usr/bin:/bin:${HOME}/bin
+
+# This variable signals parts of the script that we are testing the test scripts, 
+# and should not actually start the tests.
+export TESTING_TEST_XML=true
+export ANT_OPTS=-Djava.io.tmpdir=${WORKSPACE}/tmp -Xms500m -Xmx500m 
+
+if [[ "$1" == "-c" ]]
+then
+  clean
+fi
+
+# print basic diagnostics and properties
+ant -diagnostics
+java -XshowSettings -version
+
+
+#    There are a number of test-<something> methods in test xml which, by convention, mean
+#    to simply test the test script itself. The test-all target runs all of those tests.
+
+# Note: currently this file always comes from master, no matter what branch is being built/tested.
+wget -O getEBuilder.xml --no-verbose   http://git.eclipse.org/c/platform/eclipse.platform.releng.aggregator.git/plain/production/testScripts/hudsonBootstrap/getEBuilder.xml 2>&1
+
+# Can only test the "downloadURL form" if there is a current, accurate build. During development, should use git/master version.
+ 
+
+ANTFILE=getEBuilder.xml
+buildId=N20140823-1500
+eclipseStream=4.5.0
+EBUILDER_HASH=d4ca36a125742e490be6b22caca84d7769030758
+
+ant -f "${ANTFILE}" -DbuildId=$buildId -DeclipseStream=$eclipseStream -Dosgi.os=linux -Dosgi.ws=gtk -Dosgi.arch=x86_64 -DEBUILDER_HASH=${EBUILDER_HASH} -DdownloadURL=http://download.eclipse.org/eclipse/downloads/drops4/${buildId} -Dtest.target=performance -DskipDerby=true
+
+
