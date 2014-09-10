14d3298dfcc7494c4be8baa7fd83d946559a8d64
David Williams <david_williams@us.ibm.com>
2014-09-10 10:26:14 -0400

    [releng] make sure bootstrap script stay uniform

--- bootstrap/mb4M.sh
+++ bootstrap/mb4M.sh
@@ -3,41 +3,6 @@
 # Simple utility to run as cronjob to run Eclipse Platform builds
 # Normally resides in $BUILD_HOME
 
-# this localBuildProperties.shsource file is to ease local builds to override some variables.
-# It should not be used for production builds.
-source localBuildProperties.shsource 2>/dev/null
-export BUILD_HOME=${BUILD_HOME:-/shared/eclipse/builds}
-
-function usage() {
-printf "\n\tSimple script start a build of a certain stream." >&2
-printf "\n\tUsage: %s [[-h] | [-t]] " $(basename $0) >&2
-printf "\n\t\t%s\n" "where h==help, t==test build " >&2
-}
-# all optional
-# normally, when ran from crobjob, none should be specified
-while getopts 'ht' OPTION
-do
-  case $OPTION in
-    h)    usage
-      exit
-      ;;
-    t)    export testbuildonly=true
-      ;;
-#        ?)    usage
-#        exit 2
-#        ;;
-  esac
-done
-
-SCRIPT_NAME=$0
-
-
-LOG_BASE_NAME=${SCRIPT_NAME##*/}
-LOG_OUT_NAME=${BUILD_HOME}/${LOG_BASE_NAME%.*}.out.log
-LOG_ERR_NAME=${BUILD_HOME}/${LOG_BASE_NAME%.*}.err.log
-
-echo "Starting $SCRIPT_NAME at $( date +%Y%m%d-%H%M ) " 1>$LOG_OUT_NAME 2>$LOG_ERR_NAME
-
 # Start with minimal path for consistency across machines
 # plus, cron jobs do not inherit an environment
 # care is needed not have anything in ${HOME}/bin that would effect the build
@@ -61,9 +26,40 @@ oldumask=`umask`
 umask 0002
 echo "umask explicitly set to 0002, old value was $oldumask" 1>>$LOG_OUT_NAME 2>>$LOG_ERR_NAME
 
+function usage() {
+printf "\n\tSimple script start a build of a certain stream." >&2
+printf "\n\tUsage: %s [[-h] | [-t]] " $(basename $0) >&2
+printf "\n\t\t%s\n" "where h==help, t==test build " >&2
+}
+# all optional
+# normally, when ran from crobjob, none should be specified
+while getopts 'ht' OPTION
+do
+  case $OPTION in
+    h)    usage
+      exit
+      ;;
+    t)    export testbuildonly=true
+      ;;
+#        ?)    usage
+#        exit 2
+#        ;;
+  esac
+done
+
 # this localBuildProperties.shsource file is to ease local builds to override some variables.
 # It should not be used for production builds.
 source localBuildProperties.shsource 2>/dev/null
+export BUILD_HOME=${BUILD_HOME:-/shared/eclipse/builds}
+
+SCRIPT_NAME=$0
+LOG_BASE_NAME=${SCRIPT_NAME##*/}
+LOG_OUT_NAME=${BUILD_HOME}/${LOG_BASE_NAME%.*}.out.log
+LOG_ERR_NAME=${BUILD_HOME}/${LOG_BASE_NAME%.*}.err.log
+
+echo "Starting $SCRIPT_NAME at $( date +%Y%m%d-%H%M ) " 1>$LOG_OUT_NAME 2>$LOG_ERR_NAME
+
+
 
 export BRANCH=R4_4_maintenance
 export BUILD_TYPE=M
@@ -80,7 +76,6 @@ export PRODUCTION_SCRIPTS_DIR=production
 
 source $BUILD_HOME/bootstrap.shsource
 
-
+# run rest in "back ground"
 ${BUILD_ROOT}/${PRODUCTION_SCRIPTS_DIR}/master-build.sh "${BUILD_ROOT}/${PRODUCTION_SCRIPTS_DIR}/build_eclipse_org.shsource" 1>>$LOG_OUT_NAME 2>>$LOG_ERR_NAME &
-
 
