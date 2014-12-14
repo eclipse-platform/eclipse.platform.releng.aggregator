e0f05c38371b98574853d28625454b1a4fc80251
David Williams <david_williams@us.ibm.com>
2014-12-14 16:36:05 -0500

    [releng] Simple change to a simple test

--- production/testScripts/test_runTests2.xml.sh
+++ production/testScripts/test_runTests2.xml.sh
@@ -53,7 +53,8 @@ export PATH=${JAVA_HOME}/bin:${ANT_HOME}/bin:/usr/local/bin:/usr/bin:/bin:${HOME
 # This variable signals parts of the script that we are testing the test scripts,
 # and should not actually start the tests.
 export TESTING_TEST_XML=true
-export ANT_OPTS=-Xms1024m -Xmx1024m -Djava.io.tmpdir=${WORKSPACE}/tmp
+#export ANT_OPTS=-Xms1024m -Xmx1024m -Djava.io.tmpdir=${WORKSPACE}/tmp
+export ANT_OPTS=-Djava.io.tmpdir=${WORKSPACE}/tmp
 
 if [[ "$1" == "-c" ]]
 then
