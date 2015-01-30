dc63f9a45f424f419b007deebe54fb6c53e857e9
David Williams <david_williams@us.ibm.com>
2015-01-30 09:29:01 -0500

    [releng] prep for RC2 promotion

--- eclipse.platform.releng.tychoeclipsebuilder/eclipse/publishingFiles/staticDropFiles/computeRepoURLs.php
+++ eclipse.platform.releng.tychoeclipsebuilder/eclipse/publishingFiles/staticDropFiles/computeRepoURLs.php
@@ -5,6 +5,12 @@
 // TODO: better to have this as function ... but since there are several 
 // "outputs", as-is, would take some rethinking? 
 
+// "RCs" have complicated rules, but in short: 
+//   RC's for an initial release (in June) go from 4.x-M-build repos to 4xmilestone repos
+//   RC's for SRs "stay" in the 4.x-M-build repo. And, no easy way to tell if service release, 
+//   from the build id alone. The hard way is to part the first "digit groups" of build id, and, 
+//   if service field is 0 (or does not exist) then it is not a service release. 
+//   TODO: still need to fix. Hard coded for now, for 4.4.2 RCs
   $pos = strpos($BUILD_ID, "RC");
   if ($pos === false) {
     $isRC = false;
@@ -12,13 +18,14 @@
     $isRC = true;
   }
 
+
   // We expect $BUILD_ID to be defined in buildproperties.php
   // But it can be defined several times in reference URI, such as once in directory name,
   // and once in filename. We want the directory-like part.
   // And to complicate things, in S and R builds, the segment is no longer BUILD_ID,
   // but a more complicated concatenation.
   if ($BUILD_TYPE === "M" && $isRC ) {
-    $STREAM_REPO_NAME=$STREAM_MAJOR.".".$STREAM_MINOR."milestones";
+    $STREAM_REPO_NAME=$STREAM_MAJOR.".".$STREAM_MINOR."-M-builds";
   }
   else {
     if ($BUILD_TYPE === "N" || $BUILD_TYPE === "I" || $BUILD_TYPE === "M" || $BUILD_TYPE === "P" || $BUILD_TYPE === "X" || $BUILD_TYPE === "Y") {
