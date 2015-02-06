f8c9cb73b96d0e99faa49b7efc0a598eeec1cf88
David Williams <david_williams@us.ibm.com>
2015-02-06 05:05:01 -0500

    Bug 457767 - "computeUpdateURL.php" is incorrect for maintenance RCs Correct to "how used" by index.php.

--- eclipse.platform.releng.tychoeclipsebuilder/eclipse/publishingFiles/staticDropFiles/computeRepoURLs.php
+++ eclipse.platform.releng.tychoeclipsebuilder/eclipse/publishingFiles/staticDropFiles/computeRepoURLs.php
@@ -27,71 +27,73 @@ $relativePath3="../../..";
 
 
 function isSRRC ( $buildid ) {
-  $SRPATTERN="/([0-9]+)\\.([0-9]+)\\.([1-9]+)RC[1-4]/";
-  return (preg_match($SRPATTERN, $buildid));
+    $SRPATTERN="/([0-9]+)\\.([0-9]+)\\.([1-9]+)RC[1-4]/";
+    return (preg_match($SRPATTERN, $buildid));
 }
 
 function isRRC ( $buildid ) {
-  $RPATTERN="/([0-9]+)\\.([0-9]+)((\\.0)?)RC[1-4]/";
-  return (preg_match($RPATTERN, $buildid));
+    $RPATTERN="/([0-9]+)\\.([0-9]+)((\\.0)?)RC[1-4]/";
+    return (preg_match($RPATTERN, $buildid));
 }
 
 function computeSTREAM_REPO_NAME () {
-  global $BUILD_ID, $BUILD_TYPE, $STREAM_MAJOR, $STREAM_MINOR;
+    global $BUILD_ID, $BUILD_TYPE, $STREAM_MAJOR, $STREAM_MINOR;
     // We expect $BUILD_ID to be defined in buildproperties.php
     // But it can be defined several times in reference URI, such as once in directory name,
     // and once in filename. We want the directory-like part.
     // And to complicate things, in S and R builds, the segment is no longer BUILD_ID,
     // but a more complicated concatenation.
     if ($BUILD_TYPE === "M" && isSRRC($BUILD_ID)) {
-      $STREAM_REPO_NAME=$STREAM_MAJOR.".".$STREAM_MINOR."-"."$BUILD_TYPE"."-builds";
+        $STREAM_REPO_NAME=$STREAM_MAJOR.".".$STREAM_MINOR."-"."$BUILD_TYPE"."-builds";
     } else {
-      if ($BUILD_TYPE === "M" && isRRC($BUILD_ID)) {
-        $STREAM_REPO_NAME=$STREAM_MAJOR.".".$STREAM_MINOR."milestones";
-      } else {
-        if ($BUILD_TYPE === "N" || $BUILD_TYPE === "I" || $BUILD_TYPE === "M" || $BUILD_TYPE === "P" || $BUILD_TYPE === "X" || $BUILD_TYPE === "Y") {
-          $STREAM_REPO_NAME=$STREAM_MAJOR.".".$STREAM_MINOR."-".$BUILD_TYPE."-"."builds";
-        } else {
-          if ($BUILD_TYPE === "S") {
+        if ($BUILD_TYPE === "M" && isRRC($BUILD_ID)) {
             $STREAM_REPO_NAME=$STREAM_MAJOR.".".$STREAM_MINOR."milestones";
-          } else {
-            if ($BUILD_TYPE === "R") {
-              $STREAM_REPO_NAME=$STREAM_MAJOR.".".$STREAM_MINOR;
+        } else {
+            if ($BUILD_TYPE === "N" || $BUILD_TYPE === "I" || $BUILD_TYPE === "M" || $BUILD_TYPE === "P" || $BUILD_TYPE === "X" || $BUILD_TYPE === "Y") {
+                $STREAM_REPO_NAME=$STREAM_MAJOR.".".$STREAM_MINOR."-".$BUILD_TYPE."-"."builds";
+            } else {
+                if ($BUILD_TYPE === "S") {
+                    $STREAM_REPO_NAME=$STREAM_MAJOR.".".$STREAM_MINOR."milestones";
+                } else {
+                    if ($BUILD_TYPE === "R") {
+                        $STREAM_REPO_NAME=$STREAM_MAJOR.".".$STREAM_MINOR;
+                    }
+                    else {
+                        echo "Unexpected value of BUILD_TYPE: $BUILD_TYPE. <br />\n";
+                        // We will make an assumption that might work.
+                        $STREAM_REPO_NAME=$STREAM_MAJOR.".".$STREAM_MINOR."-".$BUILD_TYPE."-"."builds";
+                    }
+                }
             }
-            else {
-              echo "Unexpected value of BUILD_TYPE: $BUILD_TYPE. <br />\n";
-              // We will make an assumption that might work.
-              $STREAM_REPO_NAME=$STREAM_MAJOR.".".$STREAM_MINOR."-".$BUILD_TYPE."-"."builds";
-            }
-          }
         }
-      }
     }
-  return $STREAM_REPO_NAME;
+    return $STREAM_REPO_NAME;
 }
 
 function computeSTREAM_REPO_URL () {
-  $STREAM_REPO_URL="http://download.eclipse.org/eclipse/updates/".computeSTREAM_REPO_NAME ();
-  return $STREAM_REPO_URL;
+    $STREAM_REPO_URL="http://download.eclipse.org/eclipse/updates/".computeSTREAM_REPO_NAME ();
+    return $STREAM_REPO_URL;
 }
 
-function computeBUILD_REPO_URL () {
-  global $BUILD_ID, $BUILD_TYPE, $TIMESTAMP ; 
-  // There are two types of M builds, some RCs, some not.
-  if ($BUILD_TYPE === "N" || $BUILD_TYPE === "I" || $BUILD_TYPE === "P" || $BUILD_TYPE === "X" || $BUILD_TYPE === "Y" || $BUILD_TYPE === "M" && ! ((isSRRC($BUILD_ID)) || isRRC($BUILD_ID))) {
-    $BUILD_REPO_NAME=computeSTREAM_REPO_NAME ()."/".$BUILD_ID;
-  } else {
-    $timestamp = str_replace('-', '', $TIMESTAMP);
-    if ($BUILD_TYPE === "S" || $BUILD_TYPE === "R" || ($BUILD_TYPE === "M" && (isSRRC($BUILD_ID) || isRRC($BUILD_ID)))) {
-      $BUILD_REPO_NAME=computeSTREAM_REPO_NAME ()."/".$BUILD_TYPE."-".$BUILD_ID."-"."$timestamp";
+function computeBUILD_REPO_NAME () {
+    global $BUILD_ID, $BUILD_TYPE, $TIMESTAMP ; 
+    // There are two types of M builds, some RCs, some not.
+    if ($BUILD_TYPE === "N" || $BUILD_TYPE === "I" || $BUILD_TYPE === "P" || $BUILD_TYPE === "X" || $BUILD_TYPE === "Y" || $BUILD_TYPE === "M" && ! ((isSRRC($BUILD_ID)) || isRRC($BUILD_ID))) {
+        $BUILD_REPO_NAME=computeSTREAM_REPO_NAME ()."/".$BUILD_ID;
     } else {
-      echo "Unexpected value of BUILD_TYPE: $BUILD_TYPE. <br />\n";
-      // We will make an assumption that might work.
-      $BUILD_REPO_NAME=computeSTREAM_REPO_NAME ()."/".$BUILD_ID;
+        $timestamp = str_replace('-', '', $TIMESTAMP);
+        if ($BUILD_TYPE === "S" || $BUILD_TYPE === "R" || ($BUILD_TYPE === "M" && (isSRRC($BUILD_ID) || isRRC($BUILD_ID)))) {
+            $BUILD_REPO_NAME=computeSTREAM_REPO_NAME ()."/".$BUILD_TYPE."-".$BUILD_ID."-"."$timestamp";
+        } else {
+            echo "Unexpected value of BUILD_TYPE: $BUILD_TYPE. <br />\n";
+            // We will make an assumption that might work.
+            $BUILD_REPO_NAME=computeSTREAM_REPO_NAME ()."/".$BUILD_ID;
+        }
     }
-  }
-
-  $BUILD_REPO_URL="http://download.eclipse.org/eclipse/updates/".$BUILD_REPO_NAME;
-  return $BUILD_REPO_URL;
+    return $BUILD_REPO_NAME;
+}
+function computeBUILD_REPO_URL () {
+    $BUILD_REPO_URL="http://download.eclipse.org/eclipse/updates/".computeBUILD_REPO_NAME();
+    return $BUILD_REPO_URL;
 }
 
