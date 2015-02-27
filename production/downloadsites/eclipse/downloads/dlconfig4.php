f251c5111b192972180bf41587d70c832b657871
David Williams <david_williams@us.ibm.com>
2015-02-26 20:19:55 -0500

    Bug 460650 - Suggest to 'archive' (at least) 4.3.2 (and it's Java 8 patch)
    main changes for bug 460650

--- production/downloadsites/eclipse/downloads/dlconfig4.php
+++ production/downloadsites/eclipse/downloads/dlconfig4.php
@@ -8,13 +8,14 @@ $dropPrefix[]="N";
 $dropPrefix[]="P";
 $dropType = array();
 $dropType[]="Latest Release";
-$dropType[]="4.5 Stable Build";
-$dropType[]="4.5 Integration Build";
-$dropType[]="4.4 Maintenance Build";
-$dropType[]="4.5 Nightly Build";
+$dropType[]="4.5 Stable Builds";
+$dropType[]="4.5 Integration Builds";
+$dropType[]="4.4 Maintenance Builds";
+$dropType[]="4.5 Nightly Builds";
 $dropType[]="Patch Builds";
 // the "prefix" array and dropType array must be of same size, defined in right order
 for ($i = 0; $i < count($dropType); $i++) {
     $typeToPrefix[$dropType[$i]] = $dropPrefix[$i];
 }
+
 
