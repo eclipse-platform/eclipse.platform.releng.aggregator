762fa89b894fd7bd1656f40ff9e6bc9a35a38760
David Williams <david_williams@us.ibm.com>
2015-03-21 19:07:18 -0400

    [releng] improve getEBuilder and it's test

--- production/testScripts/test_runTests2.xml.sh
+++ production/testScripts/test_runTests2.xml.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 #   Utility to test the runTests2.xml file. It's intended to allow rapid confirmation
 #   that the runTests2.xml file computes correct values, for various versions platforms
 #   and various versions of java and ant, etc.
@@ -25,10 +24,10 @@ function clean()
 {
 
   echo -e "\n Cleaning previous results, as requested."
-  # remember, do not want to rm "out.txt" since we are currently piping to it via 'tee'.
-  rm -fr eclipse.platform.releng.aggregator/ eclipse.platform.releng.aggregator-master.zip eclipse.platform.releng.basebuilder-R38M6PlusRC3G/ getEBuilder.xml org.eclipse.releng.basebuilder/ runTests2.xml tempEBuilder/
+  # remember, do not want to rm "out.txt" since we may be currently piping to it via 'tee'.
+  rm -fr ${WORKSPACE}/eclipse.platform.releng.aggregator/ eclipse.platform.releng.aggregator-master.zip eclipse.platform.releng.basebuilder-R38M6PlusRC3G/ getEBuilder.xml org.eclipse.releng.basebuilder/ runTests2.xml tempEBuilder/
 
-  rm -fr production.properties propertiesAllFromRunTest2.properties sdk.tests streamSpecific-build.properties workarea
+  rm -fr ${WORKSPACE}/production.properties propertiesAllFromRunTest2.properties sdk.tests streamSpecific-build.properties workarea
 
 }
 
@@ -48,15 +47,16 @@ export ANT_HOME=/shared/common/apache-ant-1.9.2
 #export JAVA_HOME=/shared/common/jdk1.8.0_x64-latest
 
 export JAVA_HOME=/shared/common/jdk1.7.0-latest
-export WORKSPACE=${HOME}/tempworkarea/
 export PATH=${JAVA_HOME}/bin:${ANT_HOME}/bin:/usr/local/bin:/usr/bin:/bin:${HOME}/bin
+# Be sure to set to a "fresh" non-cluttered area, so can be easily cleaned up.
+export WORKSPACE=${HOME}/tempworkarea
 
 # This variable signals parts of the script that we are testing the test scripts,
 # and should not actually start the tests.
 export TESTING_TEST_XML=true
 #export ANT_OPTS=-Xms1024m -Xmx1024m -Djava.io.tmpdir=${WORKSPACE}/tmp
 export ANT_OPTS=-Djava.io.tmpdir=${WORKSPACE}/tmp
-mkdir ${WORKSPACE}/tmp
+mkdir -p ${WORKSPACE}/tmp
 if [[ "$1" == "-c" ]]
 then
   clean
@@ -71,22 +71,35 @@ fi
 ant -diagnostics
 java -XshowSettings -version
 
-
-#    There are a number of test-<something> methods in test xml which, by convention, mean
-#    to simply test the test script itself. The test-all target runs all of those tests.
-
 # Note: currently this file always comes from master, no matter what branch is being built/tested.
-wget -O getEBuilder.xml --no-verbose   http://${GIT_HOST}/c/platform/eclipse.platform.releng.aggregator.git/plain/production/testScripts/hudsonBootstrap/getEBuilder.xml 2>&1
+wget -O ${WORKSPACE}/getEBuilder.xml --no-verbose   http://${GIT_HOST}/c/platform/eclipse.platform.releng.aggregator.git/plain/production/testScripts/hudsonBootstrap/getEBuilder.xml 2>&1
 
-# Can only test the "downloadURL form" if there is a current, accurate build. During development, should use git/master version.
+# or test the local version, checked-out version from tree location, 
+# not necessarily checked in, yet, though many other
+# files are still checked out.
+# cp -v hudsonBootstrap/getEBuilder.xml ${WORKSPACE}/
 
+# Can only test the "downloadURL form" if there is a current, accurate build. 
+# During development, should use git/master version. (Not sure what this downloads, then?)
 
 ANTFILE=getEBuilder.xml
 buildId=I20150320-0800
 eclipseStream=4.5.0
 EBUILDER_HASH=master
-#EBUILDER_HASH=d4ca36a125742e490be6b22caca84d7769030758
+#EBUILDER_HASH=4295494c43e464a0c4ee39b7e0c847fbc3263f2a
 
-ant -f "${ANTFILE}" -DbuildId=$buildId -DeclipseStream=$eclipseStream -Dosgi.os=linux -Dosgi.ws=gtk -Dosgi.arch=x86_64 -DEBUILDER_HASH=${EBUILDER_HASH} -DdownloadURL=http://download.eclipse.org/eclipse/downloads/drops4/${buildId} -Dtest.target=performance -DskipDerby=true
+# Record where we are executing from.
+echo "PWD: $PWD"
 
+#  ? There are a number of test-<something> methods in test xml which, by convention, mean
+#    to simply test the test script itself. The test-all target runs all of those tests.
 
+ant -f "${WORKSPACE}/${ANTFILE}" -DbuildId=$buildId -DeclipseStream=$eclipseStream -Dosgi.os=linux -Dosgi.ws=gtk -Dosgi.arch=x86_64 -DEBUILDER_HASH=${EBUILDER_HASH} -DdownloadURL=http://download.eclipse.org/eclipse/downloads/drops4/${buildId} -Dtest.target=performance -DskipDerby=true
+rc=$?
+if [[ $rc == 0 ]] 
+then
+   echo "Ant returned non-zero return code: $rc."
+else 
+   echo "Ant returned normally. (Check log for BUILD FAILED/SUCCESS)"
+fi
+echo "check output in ${WORKSPACE}, especially ${WORKSPACE}/workarea"
