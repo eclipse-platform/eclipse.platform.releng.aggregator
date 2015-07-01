401d07349ed820809805502cfcb8a90c32bc6f11
David Williams <david_williams@us.ibm.com>
2015-07-01 14:42:26 -0400

    Bug 469765 - Prep for 4.6 (Neon) and 4.5 (Mars) maintenance builds

--- bootstrap/mb4M.sh
+++ bootstrap/mb4M.sh
@@ -76,9 +76,9 @@ echo "Starting $SCRIPT_NAME at $( date +%Y%m%d-%H%M ) " 1>$LOG_OUT_NAME 2>$LOG_E
 
 echo "umask explicitly set to $NEWUMASK, old value was $oldumask" 1>>$LOG_OUT_NAME 2>>$LOG_ERR_NAME
 
-export BRANCH=R4_4_maintenance
+export BRANCH=R4_5_maintenance
 export BUILD_TYPE=M
-export STREAM=4.4.2
+export STREAM=4.5.1
 
 eclipseStreamMajor=${STREAM:0:1}
 
