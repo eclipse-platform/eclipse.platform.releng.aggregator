dce23fc706f0dfadc892d67af6c0c6002ebd0a78
David Williams <david_williams@us.ibm.com>
2015-08-11 02:03:59 -0400

    Bug 473904 - Run official platform tests with Java 8

--- eclipse.platform.releng.tychoeclipsebuilder/eclipse/publishingFiles/staticDropFiles/testConfigs.php
+++ eclipse.platform.releng.tychoeclipsebuilder/eclipse/publishingFiles/staticDropFiles/testConfigs.php
@@ -9,5 +9,5 @@
 
 $expectedTestConfigs = array();
 $expectedTestConfigs[]="linux.gtk.x86_64_8.0";
-$expectedTestConfigs[]="macosx.cocoa.x86_64_7.0";
-$expectedTestConfigs[]="win32.win32.x86_7.0";
+$expectedTestConfigs[]="macosx.cocoa.x86_64_8.0";
+$expectedTestConfigs[]="win32.win32.x86_8.0";
