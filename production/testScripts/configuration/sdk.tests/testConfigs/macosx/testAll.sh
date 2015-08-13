c437be487ddfe1850905e8f2447721fa0e63da79
David Williams <david_williams@us.ibm.com>
2015-08-13 13:56:27 -0400

    Bug 474822 - May need to better control/setup Windows environment for tests

--- production/testScripts/configuration/sdk.tests/testConfigs/macosx/testAll.sh
+++ production/testScripts/configuration/sdk.tests/testConfigs/macosx/testAll.sh
@@ -6,23 +6,23 @@ ulimit -c unlimited
 # somewhere on the search path ($HOME/bin is common),
 # and it will be included here, thus can provide "override values"
 # to those defined by defaults for production machine.,
-# such as for vmcmd
+# such as for jvm
 
 source localBuildProperties.shsource 2>/dev/null
 
 echo "PWD: $PWD"
-vmcmd=${vmcmd:-/Library/Java/JavaVirtualMachines/jdk1.7.0_51.jdk/Contents/Home/jre/bin/java}
+jvm=${jvm:-/Library/Java/JavaVirtualMachines/jdk1.7.0_51.jdk/Contents/Home/jre/bin/java}
 
 # production machine is x86_64, but some local setups may be 32 bit and will need to provide
 # this value in localBuildProperties.shsource.
 eclipseArch=${eclipseArch:-x86_64}
 
 # vm.properties is used by default on production machines, but will
-# need to override on local setups to specify appropriate vm (usually same as vmcmd).
+# need to override on local setups to specify appropriate vm (usually same as jvm).
 # see bug 388269
 propertyFile=${propertyFile:-vm.properties}
 
-echo "vmcmd in testAll: ${vmcmd}"
+echo "jvm in testAll: ${jvm}"
 echo "extdir in testAll (if any): ${extdir}"
 echo "propertyFile in testAll: ${propertyFile}"
 echo "contents of propertyFile:"
@@ -34,7 +34,7 @@ cat ${propertyFile}
 
 if [[ -n "${extdir}" ]]
 then
-  ./runtestsmac.sh -os macosx -ws cocoa -arch $eclipseArch -extdirprop "${extdir}" -vm "${vmcmd}" -properties ${propertyFile} $* > results/consolelogs/${testedPlatform}_consolelog.txt
+  ./runtestsmac.sh -os macosx -ws cocoa -arch $eclipseArch -extdirprop "${extdir}" -vm "${jvm}" -properties ${propertyFile} $* > results/consolelogs/${testedPlatform}_consolelog.txt
 else
-  ./runtestsmac.sh -os macosx -ws cocoa -arch $eclipseArch -vm "${vmcmd}" -properties ${propertyFile} $* > results/consolelogs/${testedPlatform}_consolelog.txt
+  ./runtestsmac.sh -os macosx -ws cocoa -arch $eclipseArch -vm "${jvm}" -properties ${propertyFile} $* > results/consolelogs/${testedPlatform}_consolelog.txt
 fi
