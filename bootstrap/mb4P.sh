24e42ed4ed652b83c825bacaa3ad9a4b9d01cae0
David Williams <david_williams@us.ibm.com>
2015-08-28 00:02:52 -0400

    [releng] Preparation for java 9 builds

--- bootstrap/mb4P.sh
+++ bootstrap/mb4P.sh
@@ -78,7 +78,7 @@ echo "umask explicitly set to $NEWUMASK, old value was $oldumask" 1>>$LOG_OUT_NA
 
 export BRANCH=BETA_JAVA9
 export BUILD_TYPE=P
-export STREAM=4.5.0
+export STREAM=4.6.0
 export PATCH_BUILD=java9patch
 
 eclipseStreamMajor=${STREAM:0:1}
