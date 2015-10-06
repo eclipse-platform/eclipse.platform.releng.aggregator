761c6fe927a8527b4669012fe528ba4dbe4805ec
David Williams <david_williams@us.ibm.com>
2015-10-05 21:28:55 -0400

    [releng] update test project, to match current parent pom.

--- tests/eclipse-releng-test-parent/runTest.sh
+++ tests/eclipse-releng-test-parent/runTest.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # need to execute this from "git tree", so, if needed, first run
-git clone git@git.eclipse.org/gitroot/platform/eclipse.platform.releng.git
+# git clone git@git.eclipse.org/gitroot/platform/eclipse.platform.releng.git
+# then change to appropriate directory.
 
 mvn clean verify  -X -e -Pbree-libs --fail-fast -V \
  -DskipTests=true -Dmaven.repo.local=/shared/eclipse/builds/4N/localMavenRepo \
