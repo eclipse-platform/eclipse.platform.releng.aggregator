128ff6e0f791611865e3feac06e4eca68e271190
David Williams <david_williams@us.ibm.com>
2015-11-04 13:16:30 -0500

    [releng] formatted, and removed "temporary code" for old DB

--- bundles/org.eclipse.test.performance/src/org/eclipse/test/internal/performance/PerformanceMonitor.java
+++ bundles/org.eclipse.test.performance/src/org/eclipse/test/internal/performance/PerformanceMonitor.java
@@ -15,36 +15,32 @@ import org.eclipse.test.internal.performance.data.Scalar;
 
 class PerformanceMonitor {
 
-    private static PerformanceMonitor fgPerformanceMonitor;
+  private static PerformanceMonitor fgPerformanceMonitor;
 
-    public static PerformanceMonitor getPerformanceMonitor() {
-        if (fgPerformanceMonitor == null) {
-            String os = System.getProperty("os.name"); //$NON-NLS-1$
-            if (os.startsWith("Windows")) //$NON-NLS-1$
-                fgPerformanceMonitor = new PerformanceMonitorWindows();
-            else if (os.startsWith("Mac OS X")) //$NON-NLS-1$
-                fgPerformanceMonitor = new PerformanceMonitorMac();
-            else
-                fgPerformanceMonitor = new PerformanceMonitorLinux();
-        }
-        return fgPerformanceMonitor;
+  public static PerformanceMonitor getPerformanceMonitor() {
+    if (fgPerformanceMonitor == null) {
+      String os = System.getProperty("os.name"); //$NON-NLS-1$
+      if (os.startsWith("Windows")) //$NON-NLS-1$
+        fgPerformanceMonitor = new PerformanceMonitorWindows();
+      else if (os.startsWith("Mac OS X")) //$NON-NLS-1$
+        fgPerformanceMonitor = new PerformanceMonitorMac();
+      else
+        fgPerformanceMonitor = new PerformanceMonitorLinux();
     }
+    return fgPerformanceMonitor;
+  }
 
-    protected void collectOperatingSystemCounters(Map scalars) {
-        if (PerformanceTestPlugin.isOldDB()) {
-            addScalar(scalars, InternalDimensions.SYSTEM_TIME, System.currentTimeMillis());
-        } else {
-            Runtime runtime = Runtime.getRuntime();
-            // runtime.gc();
-            addScalar(scalars, InternalDimensions.USED_JAVA_HEAP, runtime.totalMemory() - runtime.freeMemory());
-        }
-    }
+  protected void collectOperatingSystemCounters(Map scalars) {
+    Runtime runtime = Runtime.getRuntime();
+    // runtime.gc();
+    addScalar(scalars, InternalDimensions.USED_JAVA_HEAP, runtime.totalMemory() - runtime.freeMemory());
+  }
 
-    protected void collectGlobalPerformanceInfo(Map scalars) {
-        // no default implementation
-    }
+  protected void collectGlobalPerformanceInfo(Map scalars) {
+    // no default implementation
+  }
 
-    void addScalar(Map scalars, Dim dimension, long value) {
-        scalars.put(dimension, new Scalar(dimension, value));
-    }
+  void addScalar(Map scalars, Dim dimension, long value) {
+    scalars.put(dimension, new Scalar(dimension, value));
+  }
 }
