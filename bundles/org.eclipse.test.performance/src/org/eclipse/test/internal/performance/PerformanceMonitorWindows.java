9dfe86e976ea87828a1315f9bf2eff003edaa27e
Lars Vogel <Lars.Vogel@vogella.com>
2016-01-08 14:34:46 +0100

    Bug 485418 - Delete commented out method getUUID in PerformanceMonitorWindows
    Change-Id: Iad368476c910aac9c1cd94abd19bc16591634be2
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.test.performance/src/org/eclipse/test/internal/performance/PerformanceMonitorWindows.java
+++ bundles/org.eclipse.test.performance/src/org/eclipse/test/internal/performance/PerformanceMonitorWindows.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2003, 2005 IBM Corporation and others. All rights reserved. This program and the accompanying materials are made
+ * Copyright (c) 2003, 2016 IBM Corporation and others. All rights reserved. This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
@@ -88,14 +88,4 @@ class PerformanceMonitorWindows extends PerformanceMonitor {
             super.collectGlobalPerformanceInfo(scalars);
         }
     }
-
-    // protected String getUUID() {
-    // if (org.eclipse.perfmsr.core.PerformanceMonitor.isLoaded()) {
-    // try {
-    // return org.eclipse.perfmsr.core.PerformanceMonitor.nativeGetUUID();
-    // } catch (Exception e) {
-    // }
-    // }
-    // return super.getUUID();
-    // }
 }
