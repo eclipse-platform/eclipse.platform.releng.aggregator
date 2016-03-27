4539f7a2565b1a0ee5a629ad62da516bea81e059
David Williams <david_williams@us.ibm.com>
2016-03-27 11:57:06 -0400

    Bug 490440 Fix up minor errors

--- eclipse.platform.releng.tychoeclipsebuilder/eclipse/publishingFiles/staticDropFiles/testConfigs.php
+++ eclipse.platform.releng.tychoeclipsebuilder/eclipse/publishingFiles/staticDropFiles/testConfigs.php
@@ -7,10 +7,10 @@
 // while not sure how that can be controlled, eventually, in either case
 // we'd probably want a consistent ordering.
 
-include_once("buildproperties.php");
+include("buildproperties.php");
 
 $expectedTestConfigs = array();
-$expectedTestConfigs[]="ep46${BUILD_TYPE}-unit-lin64_linux.gtk.x86_64_8.0";
-$expectedTestConfigs[]="ep46${BUILD_TYPE}-unit-mac64_macosx.cocoa.x86_64_8.0";
-$expectedTestConfigs[]="ep46${BUILD_TYPE}-unit-win32_win32.win32.x86_8.0";
-$expectedTestConfigs[]="ep46${BUILD_TYPE}-unit-cen64_linux.gtk.x86_64_8.0";
+$expectedTestConfigs[]="ep46$BUILD_TYPE-unit-lin64_linux.gtk.x86_64_8.0";
+$expectedTestConfigs[]="ep46$BUILD_TYPE-unit-mac64_macosx.cocoa.x86_64_8.0";
+$expectedTestConfigs[]="ep46$BUILD_TYPE-unit-win32_win32.win32.x86_8.0";
+$expectedTestConfigs[]="ep46$BUILD_TYPE-unit-cen64_linux.gtk.x86_64_8.0";
