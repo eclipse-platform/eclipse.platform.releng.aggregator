8a7187c3c80f948d0916377539c363ef17f7d547
David Williams <david_williams@us.ibm.com>
2016-04-05 12:21:52 -0400

    [releng] updating BETA_JAVA9_46 branch to match master

---
+++ eclipse.platform.releng.tychoeclipsebuilder/eclipse/publishingFiles/templateFiles/perfBaselineAltResults.template.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<?php
+//ini_set("display_errors", "true");
+//error_reporting (E_ALL);
+include ('buildproperties.php');
+include ('testConfigs.php');
+if (array_key_exists("SERVER_NAME", $_SERVER)) {
+    $servername = $_SERVER["SERVER_NAME"];
+    if ($servername === "build.eclipse.org") {
+          $imagesource="http://download.eclipse.org/eclipse.org-common/themes/Phoenix/images";
+          $csssource="http://download.eclipse.org/eclipse.org-common//themes/Phoenix/css";
+          $appsource="/home/data/httpd/download.eclipse.org/eclipse.org-common/system";
+          $clickthroughstr="";
+      }
+      else {
+          $imagesource="../../../eclipse.org-common/stylesheets";
+          $csssource="../../../eclipse.org-common/stylesheets";
+          $appsource="../../../eclipse.org-common/system";
+          $clickthroughstr="download.php?dropFile=";
+
+      }
+}
+else {
+    $servername = "localhost";
+          $imagesource="http://download.eclipse.org/eclipse.org-common/themes/Phoenix/images";
+          $csssource="http://download.eclipse.org/eclipse.org-common//themes/Phoenix/css";
+          $appsource="NONE";
+          $clickthroughstr="";
+}
+
+?>
+<head>
+
+<?php
+
+     echo "<title>Test Results for $BUILD_ID Performance Unit Tests</title>";
+?>
+
+
+
+<STYLE TYPE="text/css">
+<!--
+P {text-indent: 30pt; margin: inherit}
+-->
+</STYLE>
+
+
+<title>Drop Test Results</title>
+     <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+     <meta name="author" content="Eclipse Foundation, Inc." />
+     <meta name="keywords" content="eclipse,project,plug-ins,plugins,java,ide,swt,refactoring,free java ide,tools,platform,open source,development environment,development,ide" />
+     <link rel="stylesheet" type="text/css" href="<?php echo $csssource;?>/visual.css" media="screen" />
+     <link rel="stylesheet" type="text/css" href="<?php echo $csssource;?>/layout.css" media="screen" />
+     <link rel="stylesheet" type="text/css" href="<?php echo $csssource;?>/print.css" media="print" />
+     <script type="text/javascript">
+
+sfHover = function() {
+     var sfEls = document.getElementById("leftnav").getElementsByTagName("LI");
+     for (var i=0; i<sfEls.length; i++) {
+          sfEls[i].onmouseover=function() {
+               this.className+=" sfhover";
+          }
+          sfEls[i].onmouseout=function() {
+               this.className=this.className.replace(new RegExp(" sfhover\\b"), "");
+          }
+     }
+}
+if (window.attachEvent) window.attachEvent("onload", sfHover);
+</script>
+</head>
+<body>
+
+<?php if (! isset ($BUILD_FAILED) ) { ?>
+
+<div id="leftcol">
+<ul id="leftnav">
+<li><a href="#Logs">Logs</a></li>
+<li><a href="#UnitTest">Performance Unit Test Results</a></li>
+
+</ul>
+
+</div>
+<!-- end 'not build failed' -->
+<?php } ?>
+
+<div id="midcolumn">
+<p><b><font face="Verdana" size="+3">Test Results</font></b> </p>
+<div class="homeitem3col">
+<h3><a name="Logs"> Logs <?php echo "$BUILD_ID"; ?></h3>
+<ul>
+
+<li> <a href="perflogs.php#console"><b> Console Output Logs </b></a>
+</li>
+
+</div>
+
+<div class="homeitem3col">
+<h3><a name="UnitTest">Performance Unit Test Results for <?php echo "$BUILD_ID"; ?> </a></h3>
+
+<p>The table shows the unit test results for this build on the platforms
+tested. You may access the test results page specific to each
+component on a specific platform by clicking the cell link.
+Normally, the number of errors is indicated in the cell.
+A "-1" or "DNF" means the test "Did Not Finish" for unknown reasosns
+and hence no results page is available. In that case,
+more information can sometimes be found in
+the <a href="logs.php#console">console logs</a>.</p>
+<?php
+if (file_exists("perfNotes.html")) {
+    $my_file = file_get_contents("testNotes.html");
+    echo $my_file;
+}
+?>
+
+<table width="85%" border="1" bgcolor="#EEEEEE" rules="groups" align="center">
+<tr bgcolor="#9999CC">
+<th rowspan="2" width="40%" align="center"> org.eclipse <br> Component </th>
+<th colspan="5" align="center"> Test Configurations </th></tr>
+<tr bgcolor="#9999CC">
+<!-- The order of the columns is "hard coded". Linux, Mac, Windows -->
+<th width="20%"><?= $expectedTestConfigs[0] ?></th>
+<th width="20%"><?= $expectedTestConfigs[1] ?></th>
+<th width="20%"><?= $expectedTestConfigs[2] ?></th>
+<th><th width="20%"></th>
+</tr>
+%testresults%
+</table>
+</br>
+</div>
+</div>
+
+</body>
+</html>
---
+++ production/sdk/bootstrap/mb4N_hudson.sh
@@ -0,0 +1,113 @@
+#!/usr/bin/env bash
+
+# This job is just like its cronjob counter part, except it 
+# turns off verbose debugging (else Hudson logs would be 300 MB)
+# and does not pipe output to separate files, but lets it all go 
+# to Hudson't "console". 
+# Normally resides in $BUILD_HOME
+
+function usage()
+{
+  printf "\n\tSimple script start a build of a certain stream." >&2
+  printf "\n\tUsage: %s [[-h] | [-t]] " $(basename $0) >&2
+  printf "\n\t\t%s\n" "where h==help, t==test build " >&2
+}
+
+# Start with minimal path for consistency across machines
+# plus, cron jobs do not inherit an environment
+# care is needed not have anything in ${HOME}/bin that would effect the build
+# unintentionally, but is required to make use of "source localBuildProperties.shsource" on
+# local machines.
+# Likely only a "release engineer" would be interested, such as to override "SIGNING" (setting it
+# to false) for a test I-build on a remote machine.
+export PATH=/usr/local/bin:/usr/bin:/bin:${HOME}/bin
+# unset common variables (some defined for e4Build) which we don't want (or, set ourselves)
+unset JAVA_HOME
+unset JAVA_ROOT
+unset JAVA_JRE
+unset CLASSPATH
+unset JAVA_BINDIR
+unset JRE_HOME
+
+# 0002 is often the default for shell users, but it is not when ran from
+# a cron job, so we set it explicitly, so releng group has write access to anything
+# we create.
+NEWUMASK="0002"
+oldumask=$(umask)
+umask $NEWUMASK
+
+echo "ulimit (file handles): $( ulimit -n ) "
+ulimit -n 2048
+echo "ulimit (file handles): $( ulimit -n ) "
+
+echo "locale charmap: $(locale charmap)"
+echo "LC_ALL: $LC_ALL"
+echo "LANG: $LANG"
+echo "LANGUAGE: $LANGUAGE"
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+echo "LC_ALL: $LC_ALL"
+echo "LANG: $LANG"
+echo "LANGUAGE: $LANGUAGE"
+echo "locale charmap: $(locale charmap)"
+
+# all optional
+# normally, when ran from crobjob, none should be specified
+while getopts 'ht' OPTION
+do
+  case $OPTION in
+    h)    usage
+      exit
+      ;;
+    t)    export testbuildonly=true
+      ;;
+  esac
+done
+
+
+# Copied from Hudson's standard variables
+echo -e "\n\tANT_OPTS: $ANT_OPTS"
+echo -e "\n\thttp_proxy: $http_proxy"
+echo -e "\n\tftp_proxy: $ftp_proxy"
+export ANT_OPTS="-Dhttp.proxyHost=proxy.eclipse.org  -Dhttp.proxyPort=9898  -Dhttps.proxyHost=proxy.eclipse.org -Dhttps.proxyPort=9898 -Dhttp.nonProxyHosts=\"172.30.206.*\" -Dhttps.nonProxyHosts=\"172.30.206.*\" -Dftp.proxyHost=proxy.eclipse.org -Dftp.proxyPort=9898 -Dftp.nonProxyHosts=\"172.30.206.*\""
+export http_proxy=http://proxy.eclipse.org:9898
+export ftp_proxy=http://proxy.eclipse.org:9898
+echo -e "\n\tANT_OPTS: $ANT_OPTS"
+echo -e "\n\thttp_proxy: $http_proxy"
+echo -e "\n\tftp_proxy: $ftp_proxy"
+#export JVM_OPTS=-Dhttp.proxyHost=proxy.eclipse.org -Dhttp.proxyPort=9898 -Dhttps.proxyHost=proxy.eclipse.org -Dhttps.proxyPort=9898 -Dhttp.nonProxyHosts="172.30.206.*" -Dhttps.nonProxyHosts="172.30.206.*" -Dftp.proxyHost=proxy.eclipse.org -Dftp.proxyPort=9898 -Dftp.nonProxyHosts="172.30.206.*"
+#export JAVA_ARGS=-Dhttp.proxyHost=proxy.eclipse.org -Dhttp.proxyPort=9898 -Dhttps.proxyHost=proxy.eclipse.org -Dhttps.proxyPort=9898 -Dhttp.nonProxyHosts="172.30.206.*" -Dhttps.nonProxyHosts="172.30.206.*" -Dftp.proxyHost=proxy.eclipse.org -Dftp.proxyPort=9898 -Dftp.nonProxyHosts="172.30.206.*"
+#export ANT_ARGS=-Dhttp.proxyHost=proxy.eclipse.org -Dhttp.proxyPort=9898 -Dhttps.proxyHost=proxy.eclipse.org -Dhttps.proxyPort=9898 -Dhttp.nonProxyHosts="172.30.206.*" -Dhttps.nonProxyHosts="172.30.206.*" -Dftp.proxyHost=proxy.eclipse.org -Dftp.proxyPort=9898 -Dftp.nonProxyHosts="172.30.206.*"onProxyHosts="*.eclipse.org" -Dftp.proxyHost=proxy.eclipse.org -Dftp.proxyPort=9898 -Dftp.nonProxyHosts="*.eclipse.org"
+#export JAVA_PROXIES=-Dhttp.proxyHost=proxy.eclipse.org -Dhttp.proxyPort=9898 -Dhttps.proxyHost=proxy.eclipse.org -Dhttps.proxyPort=9898 -Dhttp.nonProxyHosts="172.30.206.*" -Dhttps.nonProxyHosts="172.30.206.*" -Dftp.proxyHost=proxy.eclipse.org -Dftp.proxyPort=9898 -Dftp.nonProxyHosts="172.30.206.*"
+
+
+# this localBuildProperties.shsource file is to ease local builds to override some variables.
+# It should not be used for production builds.
+source localBuildProperties.shsource 2>/dev/null
+export BUILD_HOME=${BUILD_HOME:-/shared/eclipse/builds}
+
+SCRIPT_NAME=$0
+
+echo "Starting $SCRIPT_NAME at $( date +%Y%m%d-%H%M ) "
+
+echo "umask explicitly set to $NEWUMASK, old value was $oldumask"
+
+export BRANCH=master
+export BUILD_TYPE=N
+export STREAM=4.6.0
+
+eclipseStreamMajor=${STREAM:0:1}
+
+# unique short name for stream and build type
+BUILDSTREAMTYPEDIR=${eclipseStreamMajor}$BUILD_TYPE
+
+export BUILD_ROOT=${BUILD_HOME}/${BUILDSTREAMTYPEDIR}
+
+export PRODUCTION_SCRIPTS_DIR=production
+
+source $BUILD_HOME/bootstrap.shsource
+export MVN_DEBUG=false
+
+${BUILD_ROOT}/${PRODUCTION_SCRIPTS_DIR}/master-build.sh "${BUILD_ROOT}/${PRODUCTION_SCRIPTS_DIR}/build_eclipse_org.shsource" 
+
