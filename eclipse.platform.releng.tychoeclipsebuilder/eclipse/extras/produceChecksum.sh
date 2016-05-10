2ac1b072f67afccacee502adc27d1b988d9a950b
David Williams <david_williams@us.ibm.com>
2016-05-09 23:04:36 -0400

    Bug 454784 - Revisit checksums - remove MD5

--- eclipse.platform.releng.tychoeclipsebuilder/eclipse/extras/produceChecksum.sh
+++ eclipse.platform.releng.tychoeclipsebuilder/eclipse/extras/produceChecksum.sh
@@ -6,17 +6,14 @@ then
 fi
 source "${SCRIPT_PATH}/bashUtilities.shsource"
 checkSumStart="$(date +%s )"
-allCheckSumsMD5=checksum/SUMSMD5.txt
+
 allCheckSumsSHA1=checksum/SUMSSHA1.txt
 allCheckSumsSHA256=checksum/SUMSSHA256.txt
 allCheckSumsSHA512=checksum/SUMSSHA512.txt
 
 #  Remove the "all" files, here at beginning if they all ready exist, 
 #  so that subsequent calls can all use append (i.e. ">>") 
-if [[ -e ${allCheckSumsMD5} ]] 
-then
-  rm ${allCheckSumsMD5}
-fi
+
 if [[ -e ${allCheckSumsSHA1} ]] 
 then
   rm ${allCheckSumsSHA1}
@@ -35,8 +32,6 @@ zipfiles=`ls *.zip`
 
 for zipfile in ${zipfiles}
 do
-  echo [md5] ${zipfile}
-  md5sum -b ${zipfile} | tee checksum/${zipfile}.md5 >>${allCheckSumsMD5}
   echo [sha1] ${zipfile}
   sha1sum -b ${zipfile} | tee checksum/${zipfile}.sha1 >>${allCheckSumsSHA1}
   echo [sha256] ${zipfile}
@@ -50,8 +45,6 @@ gzipfiles=`ls *.gz`
 
 for gzipfile in ${gzipfiles}
 do
-  echo [md5] ${gzipfile}
-  md5sum -b ${gzipfile} | tee checksum/${gzipfile}.md5 >>${allCheckSumsMD5}
   echo [sha1] ${gzipfile}
   sha1sum -b ${gzipfile} | tee checksum/${gzipfile}.sha1 >>${allCheckSumsSHA1}
   echo [sha256] ${gzipfile}
@@ -66,8 +59,6 @@ jarfiles=`ls *.jar`
 
 for jarfile in ${jarfiles}
 do
-  echo [md5] ${jarfile}
-  md5sum -b ${jarfile} | tee checksum/${jarfile}.md5 >>${allCheckSumsMD5}
   echo [sha1] ${jarfile}
   sha1sum -b ${jarfile} | tee checksum/${jarfile}.sha1 >>${allCheckSumsSHA1}
   echo [sha256] ${jarfile}
@@ -75,6 +66,7 @@ do
   echo [sha512] ${jarfile}
   sha512sum -b ${jarfile} | tee checksum/${jarfile}.sha512 >>${allCheckSumsSHA512}
 done
+
 checkSumEnd="$(date +%s )"
 elapsedTime $checkSumStart $checkSumEnd "Elapsed Time Checksums"
 
