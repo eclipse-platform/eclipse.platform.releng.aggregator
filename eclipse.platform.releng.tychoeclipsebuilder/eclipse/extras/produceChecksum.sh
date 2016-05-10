e42d38889c78a57e26ec639cdab1d3593651e283
David Williams <david_williams@us.ibm.com>
2016-05-10 19:49:24 -0400

    [releng] checksums: exclude eclipse.platform.releng.aggregator*zip

--- eclipse.platform.releng.tychoeclipsebuilder/eclipse/extras/produceChecksum.sh
+++ eclipse.platform.releng.tychoeclipsebuilder/eclipse/extras/produceChecksum.sh
@@ -9,10 +9,10 @@ else
   checkSumStart="$(date +%s )"
 fi
 
-# unclear if this script has access to build ID or not. 
+# unclear if this script has access to build ID or not.
 # Note: if it does, and becomes part of DL, then "promote script" will
 # need to be modified to change the name.
-if [[ -z "${BUILD_ID}" ]] 
+if [[ -z "${BUILD_ID}" ]]
 then
   allCheckSumsSHA256=checksum/SUMSSHA256
   allCheckSumsSHA512=checksum/SUMSSHA512
@@ -21,14 +21,14 @@ else
   allCheckSumsSHA512=checksum/${BUILD_ID}-SUMSSHA512
 fi
 
-#  Remove the "all" files, here at beginning if they all ready exist, 
-#  so that subsequent calls can all use append (i.e. ">>") 
+#  Remove the "all" files, here at beginning if they all ready exist,
+#  so that subsequent calls can all use append (i.e. ">>")
 
-if [[ -e ${allCheckSumsSHA256} ]] 
+if [[ -e ${allCheckSumsSHA256} ]]
 then
   rm ${allCheckSumsSHA256}
 fi
-if [[ -e ${allCheckSumsSHA512} ]] 
+if [[ -e ${allCheckSumsSHA512} ]]
 then
   rm ${allCheckSumsSHA512}
 fi
@@ -38,10 +38,16 @@ zipfiles=`ls *.zip`
 
 for zipfile in ${zipfiles}
 do
-  echo [sha256] ${zipfile}
-  sha256sum -b ${zipfile} | tee checksum/${zipfile}.sha256 >>${allCheckSumsSHA256}
-  echo [sha512] ${zipfile}
-  sha512sum -b ${zipfile} | tee checksum/${zipfile}.sha512  >>${allCheckSumsSHA512}
+  # There is one zip file to not list, eclipse.platform.releng.aggregator-<hash>.zip, which is merely
+  # a collected utility scripts used to run unit tests.
+  aggrPattern="^eclipse.platform.releng.aggregator.*.zip$"
+  if [[ ! "${zipfile}" =~ $aggrPattern ]]
+  then
+    echo [sha256] ${zipfile}
+    sha256sum -b ${zipfile} | tee checksum/${zipfile}.sha256 >>${allCheckSumsSHA256}
+    echo [sha512] ${zipfile}
+    sha512sum -b ${zipfile} | tee checksum/${zipfile}.sha512  >>${allCheckSumsSHA512}
+  fi
 done
 
 #array of tar.gzip files
@@ -69,7 +75,7 @@ done
 
 if [[ -n "${SCRIPT_PATH}" ]]
 then
-  # This checkSums script is called twice, once while publishing Eclipse DL site, again 
+  # This checkSums script is called twice, once while publishing Eclipse DL site, again
   # when publishing Equinox DL site. We use a simple heuristic to add "Eclipse" or "Equinox" to the output message.
   currentDirectory="${PWD}"
   equinoxPattern="^.*equinox.*$"
