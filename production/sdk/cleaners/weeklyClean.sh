752b5621f1c33be78c4c1513f148af4a44ce0c20
David Williams <david_williams@us.ibm.com>
2016-05-14 18:05:38 -0400

    [releng] Update or add copyrights to cleaning scripts
    Change-Id: Ia62209130be017cb9ee88220c8fd9119a0ab0a78

--- production/sdk/cleaners/weeklyClean.sh
+++ production/sdk/cleaners/weeklyClean.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+#*******************************************************************************
+# Copyright (c) 2016 IBM Corporation and others.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#     David Williams - initial API and implementation
+#*******************************************************************************
 
 # Utility to clean download machine
 echo -e "\n\tWeekly clean of ${HOSTNAME} download server on $(date )\n"
