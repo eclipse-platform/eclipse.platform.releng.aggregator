4964ff1d1ce37181ffc7c704cc663bf61b51af9a
David Williams <david_williams@us.ibm.com>
2016-05-23 23:39:28 -0400

    Bug 494360 - index.php page broken for I20160523-2000
    Change-Id: Ie3d247101df9599fd46ca39a292d26d1710d3e6f

--- eclipse.platform.releng.tychoeclipsebuilder/eclipse/publishingFiles/templateFiles/index.template.php
+++ eclipse.platform.releng.tychoeclipsebuilder/eclipse/publishingFiles/templateFiles/index.template.php
@@ -302,7 +302,7 @@ else {
     <li><a href="https://www.eclipse.org/eclipse/development/plans/eclipse_project_plan_<?php echo $STREAMMajor; ?>_<?php echo $STREAMMinor; ?>.xml#target_environments">Target Platforms and Environments</a></li>
     <li><a href="directory.txt">View the Git repositories used for the current build.</a></li>
     <li><a href="http://wiki.eclipse.org/Platform-releng/How_to_check_integrity_of_downloads">How to verify a download.</a></li>
-  <ul>
+  </ul>
 <?php
   //$sums256file="checksum/eclipse-$BUILD_ID-SUMSSHA256";
   //if (file_exists($sums256file)) {
