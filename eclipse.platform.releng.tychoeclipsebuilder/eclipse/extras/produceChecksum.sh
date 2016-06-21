4451481d3afea403111a4f684fca7f28a7ab2388
David Williams <david_williams@us.ibm.com>
2016-06-21 09:42:20 -0400

    Bug 496460 - improve GPG signing so can be controlled by same variable as other signing
    Change-Id: Ia56a8f87320e78a9982b70310738d2946fe11cea

--- eclipse.platform.releng.tychoeclipsebuilder/eclipse/extras/produceChecksum.sh
+++ eclipse.platform.releng.tychoeclipsebuilder/eclipse/extras/produceChecksum.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# this localBuildProperties.shsource file is to ease local builds to 
+# this localBuildProperties.shsource file is to ease local builds to
 # override some variables.
 # It should not be used for production builds.
 source localBuildProperties.shsource 2>/dev/null
@@ -16,7 +16,7 @@ else
 fi
 
 # This checkSums script is called twice, once while publishing Eclipse DL site, again
-# when publishing Equinox DL site. We use a simple heuristic to 
+# when publishing Equinox DL site. We use a simple heuristic to
 # make use of "eclipse" or "equinox".
 # TODO: better design to require it to be passed in?
 currentDirectory="${PWD}"
@@ -88,21 +88,23 @@ do
   sha512sum -b ${jarfile} | tee checksum/${jarfile}.sha512 >>${allCheckSumsSHA512}
 done
 
-echo "[DEBUG] Producing GPG signatures starting."
-# We make double use of the "client". One to simplify signing script. Second to identify times in timefile.
-# remember, this "HOME" is for e4Build for production builds.
-# TODO: put in error checking for file existence/readable
-key_passphrase_file=${key_passphrase_file:-${HOME}/${client}-dev.passphrase}
+if [[ $SIGNING != "false" ]]
+then
+  echo "[DEBUG] Producing GPG signatures starting."
+  # We make double use of the "client". One to simplify signing script. Second to identify times in timefile.
+  # remember, this "HOME" is for e4Build for production builds.
+  # TODO: put in error checking for file existence/readable
+  key_passphrase_file=${key_passphrase_file:-${HOME}/${client}-dev.passphrase}
 
-signer=${signer:-${client}-dev@eclipse.org}
-signature_file256=${allCheckSumsSHA256}.asc
-signature_file512=${allCheckSumsSHA512}.asc
-fileToSign256=${allCheckSumsSHA256}
-fileToSign512=${allCheckSumsSHA512}
+  signer=${signer:-${client}-dev@eclipse.org}
+  signature_file256=${allCheckSumsSHA256}.asc
+  signature_file512=${allCheckSumsSHA512}.asc
+  fileToSign256=${allCheckSumsSHA256}
+  fileToSign512=${allCheckSumsSHA512}
 
-cat ${key_passphrase_file} | gpg --local-user ${signer} --sign --armor --output ${signature_file256} --batch --yes --passphrase-fd 0 --detach-sig ${fileToSign256}
-cat ${key_passphrase_file} | gpg --local-user ${signer} --sign --armor --output ${signature_file512} --batch --yes --passphrase-fd 0 --detach-sig ${fileToSign512}
-
+  cat ${key_passphrase_file} | gpg --local-user ${signer} --sign --armor --output ${signature_file256} --batch --yes --passphrase-fd 0 --detach-sig ${fileToSign256}
+  cat ${key_passphrase_file} | gpg --local-user ${signer} --sign --armor --output ${signature_file512} --batch --yes --passphrase-fd 0 --detach-sig ${fileToSign512}
+fi
 # if SCRIPT_PATH not defined, we can not call elapsed time
 if [[ -n "${SCRIPT_PATH}" ]]
 then
