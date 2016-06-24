1bf98ea4ffae07847ec964c5f6b11a899cc6a792
David Williams <david_williams@us.ibm.com>
2016-06-24 08:41:31 -0400

    Bug 496698 - "Abort the build if it's stuck" continues to run if periodic build if cancelled and restarted
    Change-Id: I6c65f78ee2dc1914682b338c2aec5725bc6177db

---
+++ production/miscToolsAndNotes/test496698/runFor10Minutes.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+max=10
+count=1
+echo -e "\n\tScript that runs for $max minutes: Loop $count of $max\n"
+sleep 1m
+count=$((count + 1)
+if [[ $count >= $max ]]
+then 
+  exit 0
+fi
+
