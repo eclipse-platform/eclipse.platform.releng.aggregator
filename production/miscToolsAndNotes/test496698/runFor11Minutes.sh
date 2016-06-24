e5465063e0048f5daa7ed670f95b3f3352131f67
David Williams <david_williams@us.ibm.com>
2016-06-24 14:37:08 -0400

    Bug 496698 - "Abort the build if it's stuck" continues to run
    Change-Id: I3bb734c7fc84fa2fde049d4b0121877cc52a1399

--- production/miscToolsAndNotes/test496698/runFor11Minutes.sh
+++ production/miscToolsAndNotes/test496698/runFor11Minutes.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-max=10
+max=11
 count=1
 echo -e "\tScript that runs for $max minutes: Loop $count of $max\n" 
 while [[ $count -lt $max ]]
 do
   sleep 1m
   count=$(($count + 1))
-  echo -e "\n\n\tcount: $count of $max\n"
+  echo -e "\n\n\tcount: $count of $max from ${0##*/}\n"
   echo -e "\n\tps:\n"
   ps -f
   echo -e "\n\tps -ef | grep \"${0##*/}\":\n"
