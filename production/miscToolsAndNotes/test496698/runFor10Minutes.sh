284c68775c943b8c5f8fe5304d845a3945383bc2
David Williams <david_williams@us.ibm.com>
2016-06-24 13:15:28 -0400

    Bug 496698 - "Abort the build if it's stuck" continues to run
    Change-Id: Ifa903d10c6b47b2ee97aeac0f08c5a2115a746e2

--- production/miscToolsAndNotes/test496698/runFor10Minutes.sh
+++ production/miscToolsAndNotes/test496698/runFor10Minutes.sh
@@ -2,11 +2,20 @@
 
 max=10
 count=1
-echo -e "\n\tScript that runs for $max minutes: Loop $count of $max\n"
-sleep 1m
-count=$((count + 1))
-if [[ $count >= $max ]]
-then 
-  exit 0
-fi
+echo -e "\tScript that runs for $max minutes: Loop $count of $max\n"
+while [[ $count -lt $max ]]
+do
+  sleep 1m
+  count=$(($count + 1))
+  echo "count: $count"
+  if [[ $count -ge $max ]]
+  then
+    echo -e "\n\tExiting ${0##*/} normally\n"
+    exit 0
+  fi
+done
+
+# if "exits" loop for some other reason, we will exit with error code
+echo -e "\n\tExiting ${0##*/} under abnormal conditions\n"
+exit 1
 
