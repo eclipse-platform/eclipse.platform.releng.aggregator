bdc95d5ba695d17937286e49d0593b331a079847
Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>
2017-07-25 18:10:15 +0530

    Bug 518239 - [JUnit 5] Feature patch for JUnit 5 support on 4.7
    Change-Id: I1981aba20b429f0be28d2a6b67cdc91d1e4ccde5
    Signed-off-by: Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>

--- production/sdk/bootstrap/mb47U_hudson.sh
+++ production/sdk/bootstrap/mb47U_hudson.sh
@@ -84,7 +84,7 @@ echo "umask explicitly set to $NEWUMASK, old value was $oldumask"
 export BRANCH=master
 export BUILD_TYPE=U
 export STREAM=4.7.0
-export PATCH_BUILD=junit5patch
+export PATCH_BUILD=junit5
 
 eclipseStreamMajor=${STREAM:0:1}
 
