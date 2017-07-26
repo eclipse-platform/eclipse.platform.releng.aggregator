7d7954f7272e76284045e936ca9d5a8d0e9a1b9e
Alexander Kurtakov <akurtako@redhat.com>
2017-07-26 09:25:56 +0300

    Bug 518697 - Remove cvs and map files tooling
    Restore errorly removed pom version code.
    
    Change-Id: Ice80c8aa5cc3185aa394b207b260d9e1fbaad4f7
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

---
+++ bundles/org.eclipse.releng.tools/src/org/eclipse/releng/internal/tools/pomversion/RelEngPluginEarlyStartup.java
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) 2013, 2016 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.releng.internal.tools.pomversion;
+
+import org.eclipse.ui.IStartup;
+
+
+public class RelEngPluginEarlyStartup implements IStartup {
+
+	@Override
+	public void earlyStartup() {
+	}
+}
