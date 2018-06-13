fbfde2b98d1ef111f03d07a904567fe188edeb9d
Simon Scholz <simon.scholz@vogella.com>
2018-06-13 09:39:11 +0200

    [WIP] Bug 535789 - [Tip of the day] Help-> Tips and Tricks -> Platform
    [Tip of the day] Provide tip provider for Help-> Tips and Tricks ->
    Platform
    
    * Loads the contents of the
    org.eclipse.platform.doc.user/tips/platform_tips.html
    * For each table row a tip is created
    * Make use base64 images from other plugins
    * added to the pom modules
    * make use of the DefaultHtmlTip in tips core
    * added about.html
    
    * The same implementation could be used for new and noteworthy because
    it also has a table with subject in the left column and contents in the
    right column
    
    Change-Id: I85cd10c46cd9ed8f85f4bd9a7f12809410236f79
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

---
+++ bundles/org.eclipse.platform.doc.tips/src/org/eclipse/platform/doc/tips/HtmlExtractor.java
@@ -0,0 +1,130 @@
+/*******************************************************************************
+ * Copyright (c) 2018 vogella GmbH
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     simon.scholz@vogella.com - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.platform.doc.tips;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.SubMonitor;
+import org.eclipse.tips.core.DefaultHtmlTip;
+import org.eclipse.tips.core.Tip;
+import org.eclipse.tips.core.TipImage;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
+
+public final class HtmlExtractor {
+
+	/**
+	 * This method is used to parse tables in a HTML document, which have a subject
+	 * in the left column and content in the right column. New and noteworthy or
+	 * help contents from Eclipse is usually structured like that.
+	 * 
+	 * @param html              String containing HTML
+	 * @param lastModifiedValue last modified date of the tip
+	 * @param monitor           {@link IProgressMonitor}
+	 * @return a list of {@link Tip} objects
+	 * @throws OperationCanceledException
+	 * @throws IOException
+	 */
+	public static List<Tip> getTipsFromEclipseHtmlTable(String providerId, String html, Date lastModifiedValue,
+			IProgressMonitor monitor) throws OperationCanceledException, IOException {
+		Document doc = Jsoup.parse(html);
+		Elements trElements = doc.select("tr"); //$NON-NLS-1$
+		if (!trElements.isEmpty()) {
+			List<Tip> subjectAndHtmlList = new ArrayList<>();
+			SubMonitor subMonitor = SubMonitor.convert(monitor, "BrowserTips - Parsing lists", trElements.size());
+			for (Element element : trElements) {
+				DefaultHtmlTip browserTip = createBrowserTip(providerId, lastModifiedValue, element,
+						subMonitor.split(1));
+				subjectAndHtmlList.add(browserTip);
+			}
+			return subjectAndHtmlList;
+		}
+
+		return Collections.emptyList();
+	}
+
+	private static DefaultHtmlTip createBrowserTip(String providerId, Date lastModifiedValue, Element element,
+			SubMonitor subMonitor) throws IOException {
+		Elements tdElements = element.select("td"); //$NON-NLS-1$
+		Element subject = tdElements.first();
+		Element content = tdElements.last();
+
+		String html = createHtmlContent(subject, content);
+
+		DefaultHtmlTip browserTip = new DefaultHtmlTip(providerId, lastModifiedValue, subject.text(), html);
+		return browserTip;
+	}
+
+	private static String createHtmlContent(Element subject, Element content) throws IOException {
+
+		base64ImageRefs(content);
+
+		StringBuilder sb = new StringBuilder();
+		sb.append("<html>"); //$NON-NLS-1$
+		sb.append("<body>"); //$NON-NLS-1$
+		sb.append("<h1>"); //$NON-NLS-1$
+		sb.append(subject.text());
+		sb.append("</h1>"); //$NON-NLS-1$
+		sb.append(content.html());
+		sb.append("</body>"); //$NON-NLS-1$
+		sb.append("</html>"); //$NON-NLS-1$
+		String html = sb.toString();
+		return html;
+	}
+
+	private static void base64ImageRefs(Element content) throws IOException {
+
+		Elements imgs = content.select("img[src]"); //$NON-NLS-1$
+		for (Element imgElement : imgs) {
+			String srcAttr = imgElement.attr("src"); //$NON-NLS-1$
+			if (srcAttr.startsWith("PLUGINS_ROOT")) { //$NON-NLS-1$
+				String[] splittedImgSrc = srcAttr.split("/"); //$NON-NLS-1$
+				String bundleSymbolicName = splittedImgSrc[1];
+				String imgPath = splittedImgSrc[2];
+				changeImgSrc(imgElement, imgPath, bundleSymbolicName);
+			} else if (srcAttr.startsWith("../")) { //$NON-NLS-1$
+				changeImgSrc(imgElement, srcAttr.substring(3), "org.eclipse.platform.doc.user"); //$NON-NLS-1$
+			} else {
+				changeImgSrc(imgElement, "tips/" + srcAttr, "org.eclipse.platform.doc.user"); //$NON-NLS-1$ //$NON-NLS-2$
+			}
+		}
+	}
+
+	private static void changeImgSrc(Element imgElement, String srcAttr, String bundleSymbolicName) throws IOException {
+		Bundle bundle = Platform.getBundle(bundleSymbolicName);
+		URL imgUrl = bundle.getEntry(srcAttr);
+		if (imgUrl == null) {
+			Bundle thisBundle = FrameworkUtil.getBundle(HtmlExtractor.class);
+			Platform.getLog(thisBundle).log(new Status(IStatus.WARNING, thisBundle.getSymbolicName(),
+					srcAttr + " not found in " + bundle.getSymbolicName() + " bundle.")); //$NON-NLS-1$ //$NON-NLS-2$
+			return;
+		}
+		TipImage tipImage = new TipImage(imgUrl);
+		String base64Image = tipImage.getBase64Image();
+		imgElement.attr("src", base64Image); //$NON-NLS-1$
+	}
+}
---
+++ bundles/org.eclipse.platform.doc.tips/src/org/eclipse/platform/doc/tips/HtmlTableProvider.java
@@ -0,0 +1,84 @@
+/*******************************************************************************
+ * Copyright (c) 2018 vogella GmbH
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     simon.scholz@vogella.com - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.platform.doc.tips;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.io.IOUtils;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.SubMonitor;
+import org.eclipse.tips.core.Tip;
+import org.eclipse.tips.core.TipImage;
+import org.eclipse.tips.core.TipProvider;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
+
+public class HtmlTableProvider extends TipProvider {
+
+	private TipImage tipImage;
+
+	@Override
+	public String getDescription() {
+		return "Eclipse Platform Tips";
+	}
+
+	@Override
+	public String getID() {
+		return getClass().getName();
+	}
+
+	@Override
+	public TipImage getImage() {
+		if (tipImage == null) {
+			Bundle bundle = FrameworkUtil.getBundle(getClass());
+
+			try {
+				tipImage = new TipImage(bundle.getEntry("icons/48/tips.png"));
+			} catch (IOException ex) {
+				getManager().log(new Status(IStatus.ERROR, bundle.getSymbolicName(), ex.getMessage(), ex));
+			}
+		}
+		return tipImage;
+	}
+
+	@Override
+	public IStatus loadNewTips(IProgressMonitor monitor) {
+		SubMonitor subMonitor = SubMonitor.convert(monitor);
+
+		Bundle bundle = Platform.getBundle("org.eclipse.platform.doc.user");
+		URL platformTipsURL = bundle.getEntry("tips/platform_tips.html");
+
+		try {
+			String platformTipsHtmlContents = IOUtils.toString(platformTipsURL.openStream(),
+					StandardCharsets.UTF_8.name());
+			List<Tip> browserTips = HtmlExtractor.getTipsFromEclipseHtmlTable(getID(), platformTipsHtmlContents,
+					new Date(), subMonitor);
+			setTips(browserTips);
+		} catch (IOException ex) {
+			return new Status(IStatus.ERROR, bundle.getSymbolicName(), ex.getMessage(), ex);
+		}
+
+		return Status.OK_STATUS;
+	}
+
+	@Override
+	public void dispose() {
+	}
+}
