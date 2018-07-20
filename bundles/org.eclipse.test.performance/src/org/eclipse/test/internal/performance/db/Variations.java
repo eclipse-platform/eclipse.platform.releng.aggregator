535bd80f5b559d4464bc93664a86e0c6c952b535
Alexander Kurtakov <akurtako@redhat.com>
2018-07-20 10:00:23 +0300

    Fix warnings in test.performance.Variations.
    Change-Id: I5372d5d25200d5149b5d16f0c2baf941874c8065
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- bundles/org.eclipse.test.performance/src/org/eclipse/test/internal/performance/db/Variations.java
+++ bundles/org.eclipse.test.performance/src/org/eclipse/test/internal/performance/db/Variations.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2016 IBM Corporation and others.
+ * Copyright (c) 2004, 2019 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
@@ -101,10 +101,9 @@ public class Variations extends Properties {
     /*
      * TODO: we need to escape '=' and ';' characters in key/values.
      */
-    @SuppressWarnings({ "rawtypes", "unchecked" })
     private static String toDB(Properties keyValues, boolean asQuery) {
-        Set set = keyValues.keySet();
-        String[] keys = (String[]) set.toArray(new String[set.size()]);
+        Set<String> set = keyValues.stringPropertyNames();
+        String[] keys = set.toArray(new String[set.size()]);
         Arrays.sort(keys);
         StringBuffer sb = new StringBuffer();
 
