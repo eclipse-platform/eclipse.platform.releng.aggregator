0ac145facecbad245866e21065e3fa1032f3f8fb
jay <jarthana@in.ibm.com>
2018-07-24 21:26:45 +0530

    Bug 537316 - 11] Fixing the incorrect branch name
    Change-Id: I8589e7125a99a8e70107fcc75b0344b41c2beeb8
    Signed-off-by: jay <jarthana@in.ibm.com>

--- production/sdk/bootstrap/mb4P_hudson.sh
+++ production/sdk/bootstrap/mb4P_hudson.sh
@@ -80,7 +80,7 @@ echo "umask explicitly set to $NEWUMASK, old value was $oldumask"
 
 # use BETA_JAVA_18_3 to do a "complete build" that includes Java 10 beta code
 # use master to literally build "just the three" bundles that are required.
-#export BRANCH=BETA_JAVA_11
+#export BRANCH=BETA_JAVA11
 export BRANCH=master
 export BUILD_TYPE=P
 export STREAM=4.9.0
