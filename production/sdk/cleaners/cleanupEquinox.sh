8cbb7a51500f6a78b9ce3b0f4b5953a6cd013b40
Lars Vogel <Lars.Vogel@vogella.com>
2018-08-29 11:51:12 +0200

    Bug 535802 - EPL 2.0 conversion for releng aggegator
    Feature properties hand edited.
    
    Change-Id: I850e2ad16e629a4855a90059b323e6504704cd23
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- production/sdk/cleaners/cleanupEquinox.sh
+++ production/sdk/cleaners/cleanupEquinox.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env bash
 #*******************************************************************************
 # Copyright (c) 2016 IBM Corporation and others.
-# All rights reserved. This program and the accompanying materials
-# are made available under the terms of the Eclipse Public License v1.0
+#
+# This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License 2.0
 # which accompanies this distribution, and is available at
-# http://www.eclipse.org/legal/epl-v10.html
+# https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
 #
 # Contributors:
 #     David Williams - initial API and implementation
 #*******************************************************************************
 
 # Copyright (c) 2011 IBM Corporation and others.
-# All rights reserved. This program and the accompanying materials
-# are made available under the terms of the Eclipse Public License v1.0
+#
+# This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License 2.0
 # which accompanies this distribution, and is available at
-# http://www.eclipse.org/legal/epl-v10.html
+# https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
 #
 # Contributors:
 #     IBM Corporation - initial API and implementation
