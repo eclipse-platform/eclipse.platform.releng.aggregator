faef4d0664f5f92247cbb30df5bb46a107517e7a
Niraj Modi <niraj.modi@in.ibm.com>
2019-01-24 13:40:58 +0530

    Bug 543421 - Results e-mail for GTK+ 3 Java 8 does not mention Java 8
    Change-Id: If7826f55ccfb4373f0e8d32051720b72fe88b9e0
    Signed-off-by: Niraj Modi <niraj.modi@in.ibm.com>

--- eclipse.platform.releng.tychoeclipsebuilder/eclipse/publishingFiles/staticDropFiles/testConfigs.php
+++ eclipse.platform.releng.tychoeclipsebuilder/eclipse/publishingFiles/staticDropFiles/testConfigs.php
@@ -10,7 +10,7 @@
 include("buildproperties.php");
 
 $expectedTestConfigs = array();
-$expectedTestConfigs[]="ep$STREAMMajor$STREAMMinor$TESTED_BUILD_TYPE-unit-cen64-gtk3_linux.gtk.x86_64_8.0";
+$expectedTestConfigs[]="ep$STREAMMajor$STREAMMinor$TESTED_BUILD_TYPE-unit-cen64-gtk3-java8_linux.gtk.x86_64_8.0";
 $expectedTestConfigs[]="ep$STREAMMajor$STREAMMinor$TESTED_BUILD_TYPE-unit-mac64_macosx.cocoa.x86_64_8.0";
 $expectedTestConfigs[]="ep$STREAMMajor$STREAMMinor$TESTED_BUILD_TYPE-unit-win32_win32.win32.x86_64_8.0";
 $expectedTestConfigs[]="ep$STREAMMajor$STREAMMinor$TESTED_BUILD_TYPE-unit-cen64-gtk3-java11_linux.gtk.x86_64_11";
