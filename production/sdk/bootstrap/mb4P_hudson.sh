eb513459ab6ef76bbd182b29cf34bad2ddc5be92
Jay Arthanareeswaran <jarthana@in.ibm.com>
2019-02-22 12:32:47 +0530

    Bug 544692: [12] Create Patch (P) build support for Java 12
    Change-Id: I0e79e5b26e393ceddcdd4058b471bcc28f90c2a4
    Signed-off-by: Jay Arthanareeswaran <jarthana@in.ibm.com>

--- production/sdk/bootstrap/mb4P_hudson.sh
+++ production/sdk/bootstrap/mb4P_hudson.sh
@@ -78,13 +78,13 @@ echo "Starting $SCRIPT_NAME at $( date +%Y%m%d-%H%M ) "
 
 echo "umask explicitly set to $NEWUMASK, old value was $oldumask"
 
-# use BETA_JAVA_18_3 to do a "complete build" that includes Java 10 beta code
-# use master to literally build "just the three" bundles that are required.
-#export BRANCH=BETA_JAVA11
-export BRANCH=R4_9_maintenance
+# use BETA_JAVA_12 to do a "complete build" that includes Java 12 beta code
+# use master to literally build "just the four" bundles that are required.
+#export BRANCH=BETA_JAVA12
+export BRANCH=master
 export BUILD_TYPE=P
-export STREAM=4.9.0
-export PATCH_BUILD=java11patch49
+export STREAM=4.11.0
+export PATCH_BUILD=java12patch411
 
 eclipseStreamMajor=${STREAM:0:1}
 
