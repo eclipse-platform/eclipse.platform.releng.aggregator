986ec84a49185258bc7868c7867b358504024264
kitlo <kitlo@us.ibm.com>
2019-04-03 21:11:22 -0400

    Bug 546098 - Create new scripts to generate repository reports
    Change-Id: I671da4241eb6f3a105f0a48b0c8dfe381539b9ba
    Signed-off-by: kitlo <kitlo@us.ibm.com>

---
+++ cje-production/mbscripts/mb030_downloadBuildToCompare.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -x
+
+#*******************************************************************************
+# Copyright (c) 2019 IBM Corporation and others.
+#
+# This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License 2.0
+# which accompanies this distribution, and is available at
+# https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#     Kit Lo - initial API and implementation
+#*******************************************************************************
+
+if [ $# -ne 1 ]; then
+  echo USAGE: $0 env_file
+  exit 1
+fi
+
+source $CJE_ROOT/scripts/common-functions.shsource
+source $1
+
+mkdir -p $CJE_ROOT/$TMP_DIR
+pushd $CJE_ROOT/$TMP_DIR
+wget --recursive --no-parent --no-verbose http://$BUILD_TO_COMPARE_SITE/$PREVIOUS_RELEASE_VER/$PREVIOUS_RELEASE_ID
+popd
---
+++ cje-production/mbscripts/mb500_createRepoReports.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -x
+
+#*******************************************************************************
+# Copyright (c) 2019 IBM Corporation and others.
+#
+# This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License 2.0
+# which accompanies this distribution, and is available at
+# https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#     Kit Lo - initial API and implementation
+#*******************************************************************************
+
+if [ $# -ne 1 ]; then
+  echo USAGE: $0 env_file
+  exit 1
+fi
+
+source $CJE_ROOT/scripts/common-functions.shsource
+source $1
+
+buildToTest=$CJE_ROOT/$DROP_DIR/$BUILD_ID/repository
+output_dir=$CJE_ROOT/$DROP_DIR/$BUILD_ID/buildlogs
+tar_name=org.eclipse.cbi.p2repo.analyzers.product-linux.gtk.x86_64.tar.gz
+report_app_dir=$CJE_ROOT/$DROP_DIR/$BUILD_ID/reportApplication
+
+wget --no-proxy --no-verbose --no-cache -O $CJE_ROOT/$TMP_DIR/$tar_name https://ci.eclipse.org/cbi/job/cbi.p2repo.analyzers_cleanAndDeploy/lastSuccessfulBuild/artifact/output/products/$tar_name
+
+mkdir -p $report_app_dir
+tar -xf $CJE_ROOT/$TMP_DIR/$tar_name -C $report_app_dir
+
+mkdir -p $output_dir
+$report_app_dir/p2analyze/p2analyze -data $output_dir/workspace-report -vm $JAVA_HOME/bin -vmargs -Xmx1g \
+    -DreferenceRepo=$CJE_ROOT/$TMP_DIR/$BUILD_TO_COMPARE_SITE/$PREVIOUS_RELEASE_VER/$PREVIOUS_RELEASE_ID \
+    -DreportRepoDir=$buildToTest \
+    -DreportOutputDir=$output_dir
