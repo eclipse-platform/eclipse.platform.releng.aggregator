c805be27cee25ed80f537759561ab23ad59954fa
Niraj Modi <niraj.modi@in.ibm.com>
2019-06-07 12:25:33 +0530

    Bug 547907 - POM and product version change for 4.13 release - Also updated updateProductVersion.sh for 4.13
    Change-Id: Ib33abd4cf3a8d80b99e853b33d95e3e3c3cd93db
    Signed-off-by: Niraj Modi <niraj.modi@in.ibm.com>

--- scripts/updateProductVersion.sh
+++ scripts/updateProductVersion.sh
@@ -1,6 +1,6 @@
 #Before running this script configure the source and target POM versions correctly
 cd ..
-find `pwd` -name pom.xml -exec sed -i 's/4.11.0/4.12.0/g' {} \;
-find `pwd` -name MANIFEST.MF -exec sed -i 's/4.11.0/4.12.0/g' {} \;
-find `pwd` -name *.product -exec sed -i 's/4.11.0/4.12.0/g' {} \;
-find `pwd` -name feature.xml -exec sed -i 's/4.11.0/4.12.0/g' {} \;
+find `pwd` -name pom.xml -exec sed -i 's/4.12.0/4.13.0/g' {} \;
+find `pwd` -name MANIFEST.MF -exec sed -i 's/4.12.0/4.13.0/g' {} \;
+find `pwd` -name *.product -exec sed -i 's/4.12.0/4.13.0/g' {} \;
+find `pwd` -name feature.xml -exec sed -i 's/4.12.0/4.13.0/g' {} \;
