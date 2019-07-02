69c1541baa1e4c801e0aa005ed90463aa98072d7
Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>
2019-07-02 02:48:06 -0700

    Bug 548431 - Produce signed windows launcher bundles in the platform repo
    Change-Id: Id2fa0d98fa0639eae1f00201f11e32101f3ad42a
    Signed-off-by: Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>

---
+++ eclipse.platform.releng.tychoeclipsebuilder/win-sign/reSign.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -x
+#*******************************************************************************
+# Copyright (c) 2019 IBM Corporation and others.
+#
+# This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License 2.0
+# which accompanies this distribution, and is available at
+# https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#     Sravan Lakkimsetti - initial API and implementation
+#*******************************************************************************
+reSign ()
+{
+  fileName=$1
+  mkdir tempLauncher
+  pushd tempLauncher
+
+  zipfile=$(basename $fileName)
+  cp ../${fileName} ${zipfile}.zip
+  unzip ${zipfile}.zip
+  #sign files
+  copy ../../win-sign/winSignPom.xml pom.xml
+  mvn clean verify
+
+  zip ${zipfile}new.zip *.exe
+  cp ${zipfile}new.zip ../${fileName}
+  popd
+  rm -r tempLauncher
+}
+
+for i in $(ls target/repository/binary/*win32*)
+do
+  reSign $i
+done
