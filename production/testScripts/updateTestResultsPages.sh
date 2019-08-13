4b146d2aa46fa1733c242c08ac45b379724fcee4
Paul Pazderski <paul-eclipse@ppazderski.de>
2019-08-13 09:31:20 +0200

    Bug 548523 - Performance tests are pending
    Fix invocation of test result importer application.
    
    Change-Id: I16739d76ed53231b95c9195c0a48c4103723150c
    Signed-off-by: Paul Pazderski <paul-eclipse@ppazderski.de>

--- production/testScripts/updateTestResultsPages.sh
+++ production/testScripts/updateTestResultsPages.sh
@@ -345,8 +345,8 @@ then
   echo "   XVFB_RUN_ARGS $XVFB_RUN_ARGS" #>> ${PERF_OUTFILE}
   echo "   current_prefix ${current_prefix}" #>> ${PERF_OUTFILE}
   echo #>> ${PERF_OUTFILE}
-  
-  ${ECLIPSE_EXE} --launcher.suppressErrors -nosplash -consolelog -debug -data $devworkspace -application org.eclipse.test.performance.ui.ImportDataMain -jvm 8.0 -vm ${devJRE} -vmargs ${vmargs} $perfOutput/*-perf-samples.dat
+
+  ${ECLIPSE_EXE} --launcher.suppressErrors -nosplash -consolelog -debug -data $devworkspace -application org.eclipse.test.performance.ui.importPerformanceData -jvm 8.0 $perfOutput/*-perf-samples.dat -vm ${devJRE} -vmargs ${vmargs}
   RC=$?
   if [[ $RC != 0 ]]
   then
