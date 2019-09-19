cffa0094b035b6bce7fea81c0e425af7ccac1e5a
Vikas Chandra <Vikas.Chandra@in.ibm.com>
2019-09-19 11:36:23 +0530

    Bug 551231 - Few reports are not created with I20190918-1800
    Change-Id: I0b340047be95bf7dc5625d2f06559f869e046217
    Signed-off-by: Vikas Chandra <Vikas.Chandra@in.ibm.com>

--- production/createReports.sh
+++ production/createReports.sh
@@ -36,7 +36,7 @@ echo -e "\tbuildIdToTest: ${BUILD_ID}"
 # TODO: we could have a "previous_release" sort of variable that 
 # would be defined in parent pom or build_eclipse_org.shsource so that
 # we do not need to change this source. 
-buildIdToCompare="4.13milestones/R-4.13-201909161045"
+buildIdToCompare="4.13/R-4.13-201909161045"
 
 build_type=${buildIdToTest:0:1}
 echo -e "\tbuild_type: ${build_type}"
@@ -67,7 +67,7 @@ function latestSimpleRepo
 if [[ ${build_type} == "I" ]]
 then
   update_dir_segment="4.14-I-builds"
-  buildIdToCompare="4.13milestones/R-4.13-201909161045"
+  buildIdToCompare="4.13/R-4.13-201909161045"
   echo -e "\tlatest_R_build: R-4.13-201909161045"
 elif [[ ${build_type} == "Y" ]]
 then
