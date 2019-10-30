e1d4060d552b91949315be8ecf7dbe888488e578
Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>
2019-10-30 15:01:17 +0530

    Bug 552550 - Create a script to get eclipse platform product from latest release
    Change-Id: Idb589dc9fcb51be568f3e09ce641f2d07d1e1bb3
    Signed-off-by: Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>

---
+++ cje-production/scripts/getEclipsePlatform.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#*******************************************************************************
+# Copyright (c) 2019 IBM Corporation and others.
+#
+# This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License 2.0
+# which accompanies this distribution, and is available at
+# https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#     Sravan Kumar Lakkimsetti - initial API and implementation
+#*******************************************************************************
+
+# Gets latest release version of eclipse platform product
+
+downloadPath=/home/data/httpd/download.eclipse.org/eclipse/downloads/drops4
+
+#get latest release folder
+epRelDir=$(ssh genie.releng@projects-storage.eclipse.org ls -d --format=single-column ${downloadPath}/R-*|sort|tail -1)
+
+#get eclipse platform product
+scp genie.releng@projects-storage.eclipse.org:${epRelDir}/eclipse-platform-*-linux-gtk-x86_64.tar.gz .
+
+tar xzf eclipse-platform-*-linux-gtk-x86_64.tar.gz
