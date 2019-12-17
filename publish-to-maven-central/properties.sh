58b9d7dc03ec13e09fd72c9b97dd49771539d785
Sarika Sinha <sarika.sinha@in.ibm.com>
2019-12-17 15:26:31 +0530

    Bug 558390 - Publish Eclipse 4.14 to maven central
    Change-Id: I833a995f3f63b0bd562dc729e1a8fbbac61446d1

--- publish-to-maven-central/properties.sh
+++ publish-to-maven-central/properties.sh
@@ -15,12 +15,12 @@
 # ECLIPSE:
 APP_NAME_P2DIRECTOR=org.eclipse.equinox.p2.director
 DROPS4=/home/data/httpd/download.eclipse.org/eclipse/downloads/drops4
-SDK_BUILD_DIR=R-4.13-201909161045
-SDK_VERSION=4.13
+SDK_BUILD_DIR=R-4.14-201912100610
+SDK_VERSION=4.14
 FILE_ECLIPSE=${DROPS4}/${SDK_BUILD_DIR}/eclipse-SDK-${SDK_VERSION}-linux-gtk-x86_64.tar.gz
 
 # JDT / ECJ:
-ECJ_VERSION=3.19.0
+ECJ_VERSION=3.20.0
 
 # AGGREGATOR:
 IU_AGG_PRODUCT=org.eclipse.cbi.p2repo.cli.product
