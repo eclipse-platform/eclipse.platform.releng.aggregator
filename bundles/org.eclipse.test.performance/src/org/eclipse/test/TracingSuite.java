c614ab094bc8c8b07ec6882237933b13991ac2c8
Alexander Kurtakov <akurtako@redhat.com>
2020-02-18 12:54:52 +0200

    Remove unused import in TracingSuite.
    Change-Id: Icbc1559120b341ccca7f4d99572b05a5cb29d5ad
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- bundles/org.eclipse.test.performance/src/org/eclipse/test/TracingSuite.java
+++ bundles/org.eclipse.test.performance/src/org/eclipse/test/TracingSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2016 IBM Corporation and others.
+ * Copyright (c) 2000, 2020 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -30,7 +30,6 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.ConcurrentHashMap;
 
-import org.eclipse.test.Screenshots;
 import org.junit.runner.Description;
 import org.junit.runner.Result;
 import org.junit.runner.Runner;
