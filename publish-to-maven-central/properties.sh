7e3cb0445de0705d6ad6de496978f28e541d6ddf
Vikas Chandra <Vikas.Chandra@in.ibm.com>
2020-03-17 16:41:29 +0530

    Bug 561184 - Publish Eclipse 4.15 to maven central
    Change-Id: I5db4ec367dc4e9acc3c32b08c0c171c2851c3ac9
    Signed-off-by: Vikas Chandra <Vikas.Chandra@in.ibm.com>

--- publish-to-maven-central/properties.sh
+++ publish-to-maven-central/properties.sh
@@ -15,12 +15,12 @@
 # ECLIPSE:
 APP_NAME_P2DIRECTOR=org.eclipse.equinox.p2.director
 DROPS4=/home/data/httpd/download.eclipse.org/eclipse/downloads/drops4
-SDK_BUILD_DIR=R-4.14-201912100610
-SDK_VERSION=4.14
+SDK_BUILD_DIR=R-4.15-202003050155
+SDK_VERSION=4.15
 FILE_ECLIPSE=${DROPS4}/${SDK_BUILD_DIR}/eclipse-SDK-${SDK_VERSION}-linux-gtk-x86_64.tar.gz
 
 # JDT / ECJ:
-ECJ_VERSION=3.20.0
+ECJ_VERSION=3.21.0
 
 # AGGREGATOR:
 IU_AGG_PRODUCT=org.eclipse.cbi.p2repo.cli.product
