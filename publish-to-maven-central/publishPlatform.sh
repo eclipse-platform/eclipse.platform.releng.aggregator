06e7820c1e4f6daee4bcfc926b80d5b8556d4488
Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>
2020-03-27 10:16:36 +0530

    Bug 561184 - Publish Eclipse 4.15 to maven central
    Change-Id: If64882b7bbbeccd6fbd3df891ccc143edcf5827d
    Signed-off-by: Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>

--- publish-to-maven-central/publishPlatform.sh
+++ publish-to-maven-central/publishPlatform.sh
@@ -45,7 +45,7 @@ echo "==== UPLOAD ===="
 
 URL=https://oss.sonatype.org/service/local/staging/deploy/maven2/
 REPO=ossrh
-SETTINGS=(/home/jenkins/.m2/settings-deploy-ossrh-releng.xml
+SETTINGS=/home/jenkins/.m2/settings-deploy-ossrh-releng.xml
 MVN=/opt/tools/apache-maven/latest/bin/mvn
 
 /bin/mkdir .log
