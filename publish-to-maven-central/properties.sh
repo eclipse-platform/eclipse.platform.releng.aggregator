c4d4b93d585c4c4b1fd2e17b931f8c4ed68d0dac
Mat Booth <mat.booth@redhat.com>
2020-06-15 10:09:13 +0100

    Bug 563569 - Publish Eclipse 4.16 to maven central
    Change-Id: I48720444261c0152c2978c68a1f13737d560f349
    Signed-off-by: Mat Booth <mat.booth@redhat.com>

--- publish-to-maven-central/properties.sh
+++ publish-to-maven-central/properties.sh
@@ -15,12 +15,12 @@
 # ECLIPSE:
 APP_NAME_P2DIRECTOR=org.eclipse.equinox.p2.director
 DROPS4=/home/data/httpd/download.eclipse.org/eclipse/downloads/drops4
-SDK_BUILD_DIR=R-4.15-202003050155
-SDK_VERSION=4.15
+SDK_BUILD_DIR=R-4.16-202006040540
+SDK_VERSION=4.16
 FILE_ECLIPSE=${DROPS4}/${SDK_BUILD_DIR}/eclipse-SDK-${SDK_VERSION}-linux-gtk-x86_64.tar.gz
 
 # JDT / ECJ:
-ECJ_VERSION=3.21.0
+ECJ_VERSION=3.22.0
 
 # AGGREGATOR:
 IU_AGG_PRODUCT=org.eclipse.cbi.p2repo.cli.product
