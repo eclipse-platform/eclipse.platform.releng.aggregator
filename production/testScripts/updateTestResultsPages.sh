08e6f312fae7ceb49c347353697e40114be12acc
Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>
2020-06-15 19:06:54 +0530

    Bug 564294 - Use 4.16 GA version across build scripts
    Change-Id: I30a85198f2cfe67cd7d5980a64e6d3de708eca53
    Signed-off-by: Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>

--- production/testScripts/updateTestResultsPages.sh
+++ production/testScripts/updateTestResultsPages.sh
@@ -310,7 +310,7 @@ then
 
   # TODO: avoid this hard coding of baseline value
   # NOTE: value must start with a letter match baselinePerfVersion in testScripts/configuration/streamSpecific.properties
-  baselineCode="S-4.16RC2-202006040540"
+  baselineCode="R-4.16-202006040540"
   # to get time stamp, first remove initial IMN:
   baselineForBuildSuffix=${buildId/[IMN]/}
   #Then remove final '-' in build id
