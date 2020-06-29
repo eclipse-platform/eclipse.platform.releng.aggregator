5ca2b0b978948fdc0f1d5e6e54dc5f692abd8598
Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>
2020-06-29 19:29:41 +0530

    Bug 562087 - [JIRO] Unanticipated comparator must be notified on platform-releng
    Change-Id: Iebacf111c2e56d3f4e61383a6eed9a7dcbda14f5
    Signed-off-by: Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>

--- cje-production/mbscripts/mb300_gatherEclipseParts.sh
+++ cje-production/mbscripts/mb300_gatherEclipseParts.sh
@@ -262,11 +262,11 @@ fi
 if [[ $logSize -gt  ${comparatorLogMinimumSize} ]]
 then
   echo -e "DEBUG: found logsize greater an minimum. preparing message using ${link}"
-  fn-write-property COMPARATOR_ERRORS "- Comparator Errors Found"
-  fn-write-property COMPARATOR_ERRORS_BODY "Check unanticipated comparator messages:<br>    http://download.eclipse.org/eclipse/downloads/drops4/${BUILD_ID}/buildlogs/comparatorlogs/buildtimeComparatorUnanticipated.log.txt<br><br>"
+  fn-write-property COMPARATOR_ERRORS "\"- Comparator Errors Found\""
+  fn-write-property COMPARATOR_ERRORS_BODY "\"Check unanticipated comparator messages:<br>    http://download.eclipse.org/eclipse/downloads/drops4/${BUILD_ID}/buildlogs/comparatorlogs/buildtimeComparatorUnanticipated.log.txt<br><br>\""
 else
   echo -e "DEBUG: comparator logSize of $logSize was not greater than comparatorLogMinimumSize of ${comparatorLogMinimumSize}"
-  fn-write-property COMPARATOR_ERRORS " "
-  fn-write-property COMPARATOR_ERRORS_BODY " "
+  fn-write-property COMPARATOR_ERRORS "\" \""
+  fn-write-property COMPARATOR_ERRORS_BODY "\" \""
 fi
 
