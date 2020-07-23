75cb7f6263d99e0f9da262e11fcd74e132ccbc9a
Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>
2020-07-24 00:18:08 +0530

    Bug 564573 - Investigate need for P-build for Java 15
    Change-Id: Ib4ab17095c1f2cc2fa279c68e8f8ada8a237952e
    Signed-off-by: Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>

---
+++ cje-production/P-build/mb220_buildSdkPatch.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#*******************************************************************************
+# Copyright (c) 2019 IBM Corporation and others.
+#
+# This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License 2.0
+# which accompanies this distribution, and is available at
+# https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#     Kit Lo - initial API and implementation
+#*******************************************************************************
+set -e
+
+if [ $# -ne 1 ]; then
+  echo USAGE: $0 env_file
+  exit 1
+fi
+
+source $CJE_ROOT/scripts/common-functions.shsource
+source $1
+
+if [[ -z "${WORKSPACE}" ]]
+then
+	MVN_ARGS=""
+else
+	MVN_ARGS="-Pbree-libs -Peclipse-sign"
+fi
+
+cd $CJE_ROOT/gitCache/eclipse.platform.releng.aggregator
+mvn -f eclipse.platform.releng.tychoeclipsebuilder/${PATCH_OR_BRANCH_LABEL}/pom.xml \
+  clean verify -P${PATCH_OR_BRANCH_LABEL} \
+  -DskipTests=true ${MVN_ARGS} \
+  -Dtycho.debug.artifactcomparator \
+  -Dtycho.localArtifacts=ignore \
+  -Dcbi.jarsigner.continueOnFail=true \
+  -Djgit.dirtyWorkingTree=error \
+  -Dmaven.repo.local=$LOCAL_REPO \
+  -Djava.io.tmpdir=$CJE_ROOT/$TMP_DIR \
+  -DaggregatorBuild=true \
+  -DbuildTimestamp=$TIMESTAMP \
+  -DbuildType=$BUILD_TYPE \
+  -DbuildId=$BUILD_ID \
+  -Declipse-p2-repo.url=NOT_FOR_PRODUCTION_USE \
+  ${JAVA_DOC_TOOL}
---
+++ cje-production/P-build/mb300_gatherEclipseParts.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#*******************************************************************************
+# Copyright (c) 2019, 2020 IBM Corporation and others.
+#
+# This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License 2.0
+# which accompanies this distribution, and is available at
+# https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#     Kit Lo - initial API and implementation
+#*******************************************************************************
+set -e
+
+if [ $# -ne 1 ]; then
+  echo USAGE: $0 env_file
+  exit 1
+fi
+
+source $CJE_ROOT/scripts/common-functions.shsource
+source $1
+
+mkdir -p $CJE_ROOT/$UPDATES_DIR/$BUILD_ID
+
+JavaCMD=${JAVA_HOME}/bin/java
+
+# gather maven properties
+cp $CJE_ROOT/$AGG_DIR/eclipse-platform-parent/target/mavenproperties.properties  $CJE_ROOT/$DROP_DIR/$BUILD_ID/mavenproperties.properties
+
+# gather repo
+echo $PATCH_BUILD
+PATCH_BUILD_GENERIC=java15patch
+REPO_DIR=$ECLIPSE_BUILDER_DIR/$PATCH_BUILD/eclipse.releng.repository.$PATCH_BUILD_GENERIC/target/repository
+  
+if [ -d $REPO_DIR ]; then
+  pushd $REPO_DIR
+  cp -r * $CJE_ROOT/$UPDATES_DIR/$BUILD_ID
+  popd
+fi
