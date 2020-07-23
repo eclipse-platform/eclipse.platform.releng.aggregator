befe25ae31156701e43e50af9b4f5601c4bdfd05
Lakshmi Shanmugam <lshanmug@in.ibm.com>
2020-07-24 00:12:57 +0530

    Temporarily disable notarization
    build is failing at notatization step due to chromium executable.
    Temporarily disable to complete the build
    
    Change-Id: I7a595aa7cfb17ac9e370a783fec0734c8643a8cf

--- cje-production/mbscripts/mb300_gatherEclipseParts.sh
+++ cje-production/mbscripts/mb300_gatherEclipseParts.sh
@@ -59,7 +59,7 @@ if [ -z $PATCH_BUILD ]; then
     cp org.eclipse.sdk.ide-macosx.cocoa.x86_64.dmg $CJE_ROOT/$DROP_DIR/$BUILD_ID/eclipse-SDK-$BUILD_ID-macosx-cocoa-x86_64.dmg
     cp org.eclipse.sdk.ide-win32.win32.x86_64.zip $CJE_ROOT/$DROP_DIR/$BUILD_ID/eclipse-SDK-$BUILD_ID-win32-x86_64.zip
     popd
-    fn-notarize-macbuild "$CJE_ROOT/$DROP_DIR/$BUILD_ID" eclipse-SDK-${BUILD_ID}-macosx-cocoa-x86_64.dmg
+    # fn-notarize-macbuild "$CJE_ROOT/$DROP_DIR/$BUILD_ID" eclipse-SDK-${BUILD_ID}-macosx-cocoa-x86_64.dmg
   fi
 
   # gather platform
@@ -72,7 +72,7 @@ if [ -z $PATCH_BUILD ]; then
     cp org.eclipse.platform.ide-macosx.cocoa.x86_64.dmg $CJE_ROOT/$DROP_DIR/$BUILD_ID/eclipse-platform-$BUILD_ID-macosx-cocoa-x86_64.dmg
     cp org.eclipse.platform.ide-win32.win32.x86_64.zip $CJE_ROOT/$DROP_DIR/$BUILD_ID/eclipse-platform-$BUILD_ID-win32-x86_64.zip
     popd
-    fn-notarize-macbuild "$CJE_ROOT/$DROP_DIR/$BUILD_ID" eclipse-platform-${BUILD_ID}-macosx-cocoa-x86_64.dmg
+    # fn-notarize-macbuild "$CJE_ROOT/$DROP_DIR/$BUILD_ID" eclipse-platform-${BUILD_ID}-macosx-cocoa-x86_64.dmg
   fi
 
   # gather platform sources
