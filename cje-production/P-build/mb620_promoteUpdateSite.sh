5de15eee4605223cb52519455262ad6ef8cbc1fe
Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>
2020-08-08 11:52:50 +0530

    Bug 564573 - Investigate need for P-build for Java 15
    Change-Id: I8df268327fcf195ddaf7cf8359a175ec92090a6f
    Signed-off-by: Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>

--- cje-production/P-build/mb620_promoteUpdateSite.sh
+++ cje-production/P-build/mb620_promoteUpdateSite.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #*******************************************************************************
-# Copyright (c) 2019 IBM Corporation and others.
+# Copyright (c) 2020 IBM Corporation and others.
 #
 # This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License 2.0
