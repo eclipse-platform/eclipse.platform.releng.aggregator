e58d6999ccb1f26afb36f5127d4584b4087cd775
Alexander Kurtakov <akurtako@redhat.com>
2020-09-03 13:02:38 +0300

    Bug 566356 - POM and product version change for 4.18 release
    Change-Id: Ie9fa75d1ac1829ad127a570eae144a8facf87d14
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- scripts/updateProductVersion.sh
+++ scripts/updateProductVersion.sh
@@ -15,7 +15,7 @@
 
 script_location=$( (cd $(dirname $0) && pwd) )
 
-find $script_location/.. -name pom.xml -exec sed -i 's/4.15.0/4.16.0/g' {} \;
-find $script_location/.. -name MANIFEST.MF -exec sed -i 's/4.15.0/4.16.0/g' {} \;
-find $script_location/.. -type f -name *.product -exec sed -i 's/4.15.0/4.16.0/g' {} \;
-find $script_location/.. -name feature.xml -exec sed -i 's/4.15.0/4.16.0/g' {} \;
+find $script_location/.. -name pom.xml -exec sed -i 's/4.17.0/4.18.0/g' {} \;
+find $script_location/.. -name MANIFEST.MF -exec sed -i 's/4.17.0/4.18.0/g' {} \;
+find $script_location/.. -type f -name *.product -exec sed -i 's/4.17.0/4.18.0/g' {} \;
+find $script_location/.. -name feature.xml -exec sed -i 's/4.17.0/4.18.0/g' {} \;
