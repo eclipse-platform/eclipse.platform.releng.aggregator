51f6ec2dbca7432b358b1f64d1f0a76365a3d356
Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>
2020-09-03 15:30:18 +0530

    Bug 566372 - Update Check composites script to verify 4.18 repositories
    Change-Id: I1ad0a44a37e33987b342bab3755eb4fad2db3c51
    Signed-off-by: Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>

--- cje-production/scripts/checkComposites/checkComposites.sh
+++ cje-production/scripts/checkComposites/checkComposites.sh
@@ -26,11 +26,16 @@ repoHttpAccess=https://download.eclipse.org
 repoAccess=${repoHttpAccess}
 # TODO: reduce this list soon
 repoList="\
-/eclipse/updates/4.16/ \
 /eclipse/updates/4.17/ \
 /eclipse/updates/4.17-I-builds/ \
 /eclipse/updates/4.17milestones/ \
 /eclipse/updates/4.17-Y-builds/ \
+/eclipse/updates/4.17-P-builds/ \
+/eclipse/updates/4.18/ \
+/eclipse/updates/4.18-I-builds/ \
+/eclipse/updates/4.18milestones/ \
+/eclipse/updates/4.18-Y-builds/ \
+/eclipse/updates/4.18-P-builds/ \
 "
 
 
