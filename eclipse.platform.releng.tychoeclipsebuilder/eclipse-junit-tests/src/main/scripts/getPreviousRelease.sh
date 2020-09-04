e8b80797789b110d8a70feee8b4bbd15d7ad39f6
Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>
2020-09-04 11:32:42 +0530

    Bug 566360 - Move previous version to 4.17RC2 across build scripts
    Change-Id: If53c8c4a5c28007b3390ea206e007cb8f1c7e997
    Signed-off-by: Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>

--- eclipse.platform.releng.tychoeclipsebuilder/eclipse-junit-tests/src/main/scripts/getPreviousRelease.sh
+++ eclipse.platform.releng.tychoeclipsebuilder/eclipse-junit-tests/src/main/scripts/getPreviousRelease.sh
@@ -7,9 +7,9 @@
 
 
 mkdir -p workarea/${buildId}/eclipse-testing
-cp /home/files/buildzips/oxygen/R/R-4.16-202006040540/eclipse-platform-4.16-linux-gtk-x86_64.tar.gz ./workarea/${buildId}/eclipse-testing/platformLocation/
+cp /home/files/buildzips/oxygen/R/S-4.17RC2-202009021800/eclipse-platform-4.17RC2-linux-gtk-x86_64.tar.gz ./workarea/${buildId}/eclipse-testing/platformLocation/
 
-cp /home/files/buildzips/oxygen/R/R-4.16-202006040540/eclipse-SDK-4.16-linux-gtk-x86_64.tar.gz ./workarea/${buildId}/eclipse-testing/
+cp /home/files/buildzips/oxygen/R/S-4.17RC2-202009021800/eclipse-SDK-4.17RC2-linux-gtk-x86_64.tar.gz ./workarea/${buildId}/eclipse-testing/
 
 cp eclipse-junit-tests-${buildId}.zip workarea/${buildId}/eclipse-testing/
 
--- production/testScripts/updateTestResultsPages.sh
+++ production/testScripts/updateTestResultsPages.sh
@@ -310,7 +310,7 @@ then
 
   # TODO: avoid this hard coding of baseline value
   # NOTE: value must start with a letter match baselinePerfVersion in testScripts/configuration/streamSpecific.properties
-  baselineCode="R-4.16-202006040540"
+  baselineCode="S-4.17RC2-202009021800"
   # to get time stamp, first remove initial IMN:
   baselineForBuildSuffix=${buildId/[IMN]/}
   #Then remove final '-' in build id
