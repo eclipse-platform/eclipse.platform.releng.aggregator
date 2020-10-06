cfa0379b9451de5fe407c46d3b8925ebc5fa5ec7
Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>
2020-10-06 18:40:28 +0530

    Bug 567003 - Drop java 14 test run and add java 15 test run in automated tests
    Change-Id: I36c2f28bbf160473caf5b1447b73fce58fbb1915
    Signed-off-by: Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>

--- JenkinsJobs/SmokeTests/StartSmokeTests.groovy
+++ JenkinsJobs/SmokeTests/StartSmokeTests.groovy
@@ -54,11 +54,22 @@ pipeline {
                         build job: 'ep-smoke-test-ppcle-java11', parameters: [string(name: 'buildId', value: "${params.buildId}")]
                   }
               }
+              stage('Centos 8.x ppc64le Java15'){
+                  steps {
+                        build job: 'ep-smoke-test-ppcle-java15', parameters: [string(name: 'buildId', value: "${params.buildId}")]
+                  }
+              }
               stage('Raspberry Pi OS arm64 Java11'){
                   steps {
                         build job: 'ep-smoke-test-arm64-java11', parameters: [string(name: 'buildId', value: "${params.buildId}")]
                   }
               }
+              stage('Raspberry Pi Java15'){
+                  steps {
+                        build job: 'ep-smoke-test-arm64-java15', parameters: [string(name: 'buildId', value: "${params.buildId}")]
+                  }
+              }
+
           }
 		}
 	}
