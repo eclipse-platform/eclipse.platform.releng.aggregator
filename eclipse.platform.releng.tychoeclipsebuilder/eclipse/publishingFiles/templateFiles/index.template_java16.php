9008b745a0ae593757ce096df1f53199ab10a956
Alexander Kurtakov <akurtako@redhat.com>
2020-11-12 22:17:46 +0200

    Remove border-radius: 1; CSS
    This property needs measurement unit and either 1px or 1% means still no
    visible radius so better to remove it and have a bit more valid html.
    
    Change-Id: I0ee5b56fa3a06a1ff1af4a7abecd8386eec0ecb2
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- eclipse.platform.releng.tychoeclipsebuilder/eclipse/publishingFiles/templateFiles/index.template_java16.php
+++ eclipse.platform.releng.tychoeclipsebuilder/eclipse/publishingFiles/templateFiles/index.template_java16.php
@@ -64,7 +64,7 @@ require("DL.thin.header.php.html");
 <?php if (! isset ($BUILD_FAILED) ) { ?>
 
 <aside class="col-md-6" id="leftcol" style="margin-top:20px;" >
-<ul class="ul-left-nav fa-ul hidden-print" style="font-color:black; background-color:#EFEBFF; background-size:contain; background-clip:border-box; border-color: black; font-size:12px; font-weight:bold; padding:2px; line-height:1; border-radius: 1;  margin:20px 3px 20px 3px">
+<ul class="ul-left-nav fa-ul hidden-print" style="font-color:black; background-color:#EFEBFF; background-size:contain; background-clip:border-box; border-color: black; font-size:12px; font-weight:bold; padding:2px; line-height:1; margin:20px 3px 20px 3px">
     <li><a href="#Repository">Eclipse p2 Repository</a></li>
     <li><a href="#EclipseSDK">Eclipse SDK</a></li>
     <li><a href="#JUnitPlugin">Tests and Testing Framework</a></li>
