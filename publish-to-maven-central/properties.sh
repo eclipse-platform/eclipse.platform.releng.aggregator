916a16cd507f0dcadfddddb5c55072ec58436a9c
Kit Lo <kitlo@us.ibm.com>
2021-06-14 11:34:09 -0400

    Bug 573749 - Publish Eclipse 4.20 to maven central
    Change-Id: I47c4c6444eec5c043ba6824c4df75ad519ce18d0
    Signed-off-by: Kit Lo <kitlo@us.ibm.com>
    Reviewed-on: https://git.eclipse.org/r/c/platform/eclipse.platform.releng/+/181930

--- publish-to-maven-central/properties.sh
+++ publish-to-maven-central/properties.sh
@@ -15,8 +15,8 @@
 # ECLIPSE:
 APP_NAME_P2DIRECTOR=org.eclipse.equinox.p2.director
 DROPS4=/home/data/httpd/download.eclipse.org/eclipse/downloads/drops4
-SDK_BUILD_DIR=R-4.19-202103031800
-SDK_VERSION=4.19
+SDK_BUILD_DIR=R-4.20-202106111600
+SDK_VERSION=4.20
 FILE_ECLIPSE=${DROPS4}/${SDK_BUILD_DIR}/eclipse-SDK-${SDK_VERSION}-linux-gtk-x86_64.tar.gz
 
 # JDT / ECJ:
