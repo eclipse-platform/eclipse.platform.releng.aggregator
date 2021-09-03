fcb8a29eb73834a14958980959409ed136e19890
Kit Lo <kitlo@us.ibm.com>
2021-09-03 01:27:44 -0400

    Bug 575798 - Update builds and repo cleanup scripts for 4.22
    Change-Id: I20bd453c531f01c9de53a2068d9749c71d818907
    Signed-off-by: Kit Lo <kitlo@us.ibm.com>
    Reviewed-on: https://git.eclipse.org/r/c/platform/eclipse.platform.releng.aggregator/+/184951

--- cje-production/cleaners/cleanupNightlyRepo.sh
+++ cje-production/cleaners/cleanupNightlyRepo.sh
@@ -193,9 +193,9 @@ function cleanRepo ()
 workspace=$1
 remoteBase="/home/data/httpd/download.eclipse.org"
 
-eclipseIRepo="${remoteBase}/eclipse/updates/4.21-I-builds"
-eclipseYRepo="${remoteBase}/eclipse/updates/4.21-Y-builds"
-eclipsePRepo="${remoteBase}/eclipse/updates/4.21-P-builds"
+eclipseIRepo="${remoteBase}/eclipse/updates/4.22-I-builds"
+eclipseYRepo="${remoteBase}/eclipse/updates/4.22-Y-builds"
+eclipsePRepo="${remoteBase}/eclipse/updates/4.22-P-builds"
 eclipseBuildTools="${remoteBase}/eclipse/updates/buildtools"
 
 doDryrun=
