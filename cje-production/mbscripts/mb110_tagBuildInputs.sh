ea3b3882eec4b6d52b2831732dc34451fec0d376
Mickael Istria <mistria@redhat.com>
2021-12-01 14:28:19 +0100

    Bug 577323 - Reference absolute path for modules
    Use absolute path for modules so when aggregator moves to GitHub, it can
    still reference projects.
    Fix the tag process to push to Gerrit SSH repo when applicable
    
    Change-Id: Ic524b20bcbd54c56b03b67ec1163c8397f05cf3b
    Reviewed-on: https://git.eclipse.org/r/c/platform/eclipse.platform.releng.aggregator/+/188410
    Reviewed-by: Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>
    Tested-by: Platform Bot <platform-bot@eclipse.org>

--- cje-production/mbscripts/mb110_tagBuildInputs.sh
+++ cje-production/mbscripts/mb110_tagBuildInputs.sh
@@ -56,7 +56,16 @@ then
 	fi
 fi
 
-git submodule foreach "if grep \"^\${name}:\" ../../../streams/repositories_$PATCH_OR_BRANCH_LABEL.txt > /dev/null; then git tag $BUILD_ID; git push --verbose origin $BUILD_ID; else echo Skipping \$name; fi || :"
+function toPushRepo() {
+	from="$1"
+	if ! [[ "$from" == http* ]]; then
+		echo $from
+	else
+		echo $(sed -e 's,http://git.eclipse.org/gitroot,ssh://genie.releng@git.eclipse.org:29418,' -e 's,https://git.eclipse.org/r,ssh://genie.releng@git.eclipse.org:29418,' <<< $from)
+	fi
+}
+export -f toPushRepo
+git submodule foreach 'if grep "^${name}:" ../../../streams/repositories_$PATCH_OR_BRANCH_LABEL.txt > /dev/null; then git tag $BUILD_ID; PUSH_URL="toPushRepo $(git remote get-url --push origin)"; git push --verbose $PUSH_URL $BUILD_ID; else echo Skipping $name; fi || :'
 git tag $BUILD_ID
 git push --verbose origin $BUILD_ID
 
