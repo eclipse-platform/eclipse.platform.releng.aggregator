29adfe9f3128ad488db73f04d8397b4977904e82
Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>
2021-12-02 13:46:27 +0530

    Bug 576975 - The p2.mirrorsURL property is missing for the 4.20 and 4.21 release repositories
    Change-Id: I0f13b3a7dabb2a21599cf46247792a842e3db04a
    Signed-off-by: Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>
    Reviewed-on: https://git.eclipse.org/r/c/platform/eclipse.platform.releng.aggregator/+/188447

--- cje-production/promotion/promoteSites.sh
+++ cje-production/promotion/promoteSites.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 #*******************************************************************************
-# Copyright (c) 2020 IBM Corporation and others.
+# Copyright (c) 2021 IBM Corporation and others.
 #
 # This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License 2.0
@@ -201,7 +201,7 @@ function createBaseBuilder ()
     ${WORKSPACE}/tempEclipse/eclipse/eclipse -nosplash \
         -debug -consolelog -data ${WORKSPACE}/workspace-toolsinstall \
         -application org.eclipse.equinox.p2.director \
-        -repository "https://download.eclipse.org/eclipse/updates/latest/","https://download.eclipse.org/eclipse/updates/buildtools/",${WEBTOOL_REPO} \
+        -repository "https://download.eclipse.org/eclipse/updates/latest/","https://download.eclipse.org/eclipse/updates/buildtools/",${WEBTOOLS_REPO} \
         -installIU org.eclipse.platform.ide,org.eclipse.pde.api.tools,org.eclipse.releng.build.tools.feature.feature.group,org.eclipse.wtp.releng.tools.feature.feature.group \
         -destination ${BASEBUILDER_DIR} \
         -profile SDKProfile
