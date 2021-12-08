b995b072ef6b206ef0990d7564f4af28b6fa0e59
Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>
2021-12-08 10:05:03 +0530

    Bug 577633 - Publish Eclipse 4.22 to maven central
    Change-Id: I576f2cf2dd626ac4e0681ab478e180b1e3128f8e
    Signed-off-by: Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>
    Reviewed-on: https://git.eclipse.org/r/c/platform/eclipse.platform.releng/+/188628

--- publish-to-maven-central/src/org/eclipse/platform/releng/maven/pom/Developer.java
+++ publish-to-maven-central/src/org/eclipse/platform/releng/maven/pom/Developer.java
@@ -73,11 +73,16 @@ public abstract class Developer {
 			String token = tokens[5]; // https://git.eclipse.org/c/equinox/rt.equinox.framework.git => start with rt.equinox.framework
 			int end = token.endsWith(".git") ? token.length()-".git".length() : token.length();
 			String project = token.substring(0, end);
-			while (!projects.contains(project)) {
-				end = project.lastIndexOf('.');
-				if (end == -1)
-					return null;
-				project = project.substring(0, end); // cut off non-matching tail segment
+			// Special case for e4 projects
+			if (project.contains("org.eclipse.e4")) {
+				project  = "eclipse.platform";
+			} else {
+				while (!projects.contains(project)) {
+					end = project.lastIndexOf('.');
+					if (end == -1)
+						return null;
+					project = project.substring(0, end); // cut off non-matching tail segment
+				}
 			}
 			return "https://projects.eclipse.org/projects/"+project+"/who";
 		}
