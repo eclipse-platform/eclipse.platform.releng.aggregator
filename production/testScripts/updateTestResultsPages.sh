430bfa458a46cc413dd2195789f7b3be1a40f924
Samantha Dawley <sdawley@redhat.com>
2022-06-03 11:41:14 -0700

    Move previous version to 4.24RC2

--- production/testScripts/updateTestResultsPages.sh
+++ production/testScripts/updateTestResultsPages.sh
@@ -295,7 +295,7 @@ then
 
   # TODO: avoid this hard coding of baseline value
   # NOTE: value must start with a letter match baselinePerfVersion in testScripts/configuration/streamSpecific.properties
-  baselineCode="R-4.23-202203080310"
+  baselineCode="S-4.24RC2-202206011800"
   # to get time stamp, first remove initial IMN:
   baselineForBuildSuffix=${buildId/[IMN]/}
   #Then remove final '-' in build id
