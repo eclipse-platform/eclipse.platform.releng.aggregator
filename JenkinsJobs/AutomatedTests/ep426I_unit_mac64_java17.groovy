5c13405d4122e048105a27fd71cf07c0cdbd40ba
Александър Куртаков <akurtakov@gmail.com>
2022-10-19 10:59:23 +0300

    Use https links in JenkinsJobs

--- JenkinsJobs/AutomatedTests/ep426I_unit_mac64_java17.groovy
+++ JenkinsJobs/AutomatedTests/ep426I_unit_mac64_java17.groovy
@@ -116,7 +116,7 @@ ant -diagnostics  1>antDiagnostics.txt 2>&1
 java -XshowSettings -version  1>javaSettings.txt 2>&1
 
 export eclipseArch=x86_64
-ant -f getEBuilder.xml -Djava.io.tmpdir=${WORKSPACE}/tmp -DbuildId=$buildId  -DeclipseStream=$STREAM -DEBUILDER_HASH=${EBUILDER_HASH}  -DdownloadURL=http://download.eclipse.org/eclipse/downloads/drops4/${buildId}  -Dosgi.os=macosx -Dosgi.ws=cocoa -Dosgi.arch=x86_64 -DtestSuite=${testSuite}
+ant -f getEBuilder.xml -Djava.io.tmpdir=${WORKSPACE}/tmp -DbuildId=$buildId  -DeclipseStream=$STREAM -DEBUILDER_HASH=${EBUILDER_HASH}  -DdownloadURL=https://download.eclipse.org/eclipse/downloads/drops4/${buildId}  -Dosgi.os=macosx -Dosgi.ws=cocoa -Dosgi.arch=x86_64 -DtestSuite=${testSuite}
 
 RAW_DATE_END="$(date +%s )"
 
