0626253fffd337cc5962320bebdff58c8e3ba84c
sdawley <sdawley@redhat.com>
2022-12-02 16:19:28 -0800

    Use 4.26 Release URL

--- publish-to-maven-central/properties.sh
+++ publish-to-maven-central/properties.sh
@@ -14,8 +14,8 @@
 
 # ECLIPSE:
 DROPS4=/home/data/httpd/download.eclipse.org/eclipse/downloads/drops4
-SDK_BUILD_DIR=R-4.25-202208311800
-SDK_VERSION=4.25
+SDK_BUILD_DIR=R-4.26-202211231800
+SDK_VERSION=4.26
 FILE_ECLIPSE=${DROPS4}/${SDK_BUILD_DIR}/eclipse-SDK-${SDK_VERSION}-linux-gtk-x86_64.tar.gz
 
 # JDT / ECJ:
