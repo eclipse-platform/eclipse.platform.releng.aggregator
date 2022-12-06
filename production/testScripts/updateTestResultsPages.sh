5dbd92b9d85a2d0a90a993249c63c2e109fed220
sdawley <sdawley@redhat.com>
2022-12-06 12:52:58 -0800

    Use 4.26 GA as previous release version

--- production/testScripts/updateTestResultsPages.sh
+++ production/testScripts/updateTestResultsPages.sh
@@ -295,7 +295,7 @@ then
 
   # TODO: avoid this hard coding of baseline value
   # NOTE: value must start with a letter match baselinePerfVersion in testScripts/configuration/streamSpecific.properties
-  baselineCode="S-4.26RC2-202211231800"
+  baselineCode="R-4.26-202211231800"
   # to get time stamp, first remove initial IMN:
   baselineForBuildSuffix=${buildId/[IMN]/}
   #Then remove final '-' in build id
