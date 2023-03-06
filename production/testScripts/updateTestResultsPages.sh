aabcb1fa30c35d5ea45e92b6cd95376213c30204
Rahul Mohanan <rahul.mohanan@ibm.com>
2023-03-06 12:08:19 +0530

    Move previous version to 4.27RC2 across build scripts #931
    Fixes https://github.com/eclipse-platform/eclipse.platform.releng.aggregator/issues/931

--- production/testScripts/updateTestResultsPages.sh
+++ production/testScripts/updateTestResultsPages.sh
@@ -295,7 +295,7 @@ then
 
   # TODO: avoid this hard coding of baseline value
   # NOTE: value must start with a letter match baselinePerfVersion in testScripts/configuration/streamSpecific.properties
-  baselineCode="R-4.26-202211231800"
+  baselineCode="S-4.27RC2-202303020300"
   # to get time stamp, first remove initial IMN:
   baselineForBuildSuffix=${buildId/[IMN]/}
   #Then remove final '-' in build id
