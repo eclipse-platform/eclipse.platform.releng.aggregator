43180621060fdc91c762815a3c97aedfbf7a5cef
Frederic Gurr <frederic.gurr@eclipse-foundation.org>
2023-04-19 12:50:20 +0200

    Use eclipsecbi/jiro-agent-centos-8.latest image instead of *-jdk11 image
    The eclipsecbi/jiro-agent-centos-8-jdk11:latest image is no longer
    supported and uses an outdated remoting version. It was only used to
    test for regressions before the eclipsecbi/jiro-agent-centos-8 image was
    switched to JDK11 by default.

--- JenkinsJobs/Builds/I_build.groovy
+++ JenkinsJobs/Builds/I_build.groovy
@@ -64,7 +64,7 @@ kind: Pod
 spec:
   containers:
   - name: "jnlp"
-    image: "eclipsecbi/jiro-agent-centos-8-jdk11:latest"
+    image: "eclipsecbi/jiro-agent-centos-8:latest"
     imagePullPolicy: "Always"
     resources:
       limits:
