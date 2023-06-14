2d74ab941dc9be04149b8629f8e81e388a8fe6a5
Rahul Mohanan <rahul.mohanan@ibm.com>
2023-06-14 14:27:16 +0530

    Update previous release version to 4.28 GA across build scripts
    Tracked in
    https://github.com/eclipse-platform/eclipse.platform.releng.aggregator/issues/1136

--- production/testScripts/updateTestResultsPages.sh
+++ production/testScripts/updateTestResultsPages.sh
@@ -295,7 +295,7 @@ then
 
   # TODO: avoid this hard coding of baseline value
   # NOTE: value must start with a letter match baselinePerfVersion in testScripts/configuration/streamSpecific.properties
-  baselineCode="S-4.28RC2a-202306050440"
+  baselineCode="R-4.28-202306050440"
   # to get time stamp, first remove initial IMN:
   baselineForBuildSuffix=${buildId/[IMN]/}
   #Then remove final '-' in build id
