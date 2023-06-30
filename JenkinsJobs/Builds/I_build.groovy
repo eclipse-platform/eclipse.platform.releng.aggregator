5ce4cee08459ef1186ba217c314b352e01c2a694
Rahul Mohanan <rahul.mohanan@ibm.com>
2023-06-30 11:12:44 +0530

    Update Ibuild Schedule
    Milestone/RC 4.29 schedule added

--- JenkinsJobs/Builds/I_build.groovy
+++ JenkinsJobs/Builds/I_build.groovy
@@ -19,13 +19,13 @@ for (STREAM in STREAMS){
 # - - - Integration Eclipse SDK builds - - - 
 # 2023-09 Release Schedule
 # Normal : 6 PM every day (1/6 - 2/9)
-0 18 * * *
+# 0 18 * * *
 
 
 # Milestone/RC Schedule 5/12 - 5/31
 # Post M1, no nightlies, I-builds only. (Be sure to "turn off" for tests and sign off days)
-# 0 6 12-31 5 5-7,1-3
-# 0 18 12-31 5 5-7,1-3
+0 6 30,1-7 6-7 5-7,1-3
+0 18 30,1-7 6-7 5-7,1-3
 
             """)
           }
