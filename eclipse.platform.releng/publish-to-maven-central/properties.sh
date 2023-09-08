a9de88ff3855521758654594c3bc4093f9e73940
Elsa Zacharia <elsa.zacharia@ibm.com>
2023-09-08 10:06:07 +0530

    Publish to maven central https://github.com/eclipse-platform/eclipse.platform.releng.aggregator/issues/1328 Changes to SDK4Mvn.aggr and properties.sh file for publishing 4.29 to maven central.

--- eclipse.platform.releng/publish-to-maven-central/properties.sh
+++ eclipse.platform.releng/publish-to-maven-central/properties.sh
@@ -14,8 +14,8 @@
 
 # ECLIPSE:
 DROPS4=/home/data/httpd/download.eclipse.org/eclipse/downloads/drops4
-SDK_BUILD_DIR=R-4.28-202306050440
-SDK_VERSION=4.28
+SDK_BUILD_DIR=R-4.29-202309031000
+SDK_VERSION=4.29
 FILE_ECLIPSE=${DROPS4}/${SDK_BUILD_DIR}/eclipse-SDK-${SDK_VERSION}-linux-gtk-x86_64.tar.gz
 
 # AGGREGATOR:
