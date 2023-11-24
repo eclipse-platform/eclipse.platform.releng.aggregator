32256c88171c9f062d54ce83586a4c92aa6d69f5
Rahul Mohanan <rahul.mohanan@ibm.com>
2023-11-24 18:20:48 +0530

    Move 4.30-I builds to R4_30_maintenance branch
    Fixes https://github.com/eclipse-platform/eclipse.platform.releng.aggregator/issues/1569

--- JenkinsJobs/Builds/I_build.groovy
+++ JenkinsJobs/Builds/I_build.groovy
@@ -24,8 +24,8 @@ for (STREAM in STREAMS){
 
 # Milestone/RC Schedule 
 # Post M1, no nightlies, I-builds only. (Be sure to "turn off" for tests and sign off days)
-0 6 6-22 11 5-7,1-3
-0 18 5-22 11 5-7,1-3
+#0 6 6-22 11 5-7,1-3
+#0 18 5-22 11 5-7,1-3
 
 
 
