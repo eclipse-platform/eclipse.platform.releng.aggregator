e13de67fe7514919e8406a399336b1a9eecf3a77
Александър Куртаков <akurtakov@gmail.com>
2023-11-28 14:19:39 +0200

    Do not run smoke tests with Java 20
    Only Java 17 and 21 are LTS versions thus only they are tested.

--- JenkinsJobs/SmokeTests/StartSmokeTests.groovy
+++ JenkinsJobs/SmokeTests/StartSmokeTests.groovy
@@ -34,31 +34,6 @@ pipeline {
                         build job: 'SmokeTests/ep-smoke-test-ppcle', parameters: [string(name: 'buildId', value: "${params.buildId}"), string(name: 'javaDownload', value: "${params.java17ppcle}")]
                   }
               }
-              stage('Ubuntu 22.04 Java20'){
-                  steps {
-                        build job: 'SmokeTests/ep-smoke-test-ubuntu22', parameters: [string(name: 'buildId', value: "${params.buildId}"), string(name: 'javaDownload', value: "${params.java20x64}")]
-                  }
-              }
-              stage('Opensuse Leap Java20'){
-                  steps {
-                        build job: 'SmokeTests/ep-smoke-test-opensuse-leap', parameters: [string(name: 'buildId', value: "${params.buildId}"), string(name: 'javaDownload', value: "${params.java20x64}")]
-                  }
-              }
-              stage('Centos 9.x Java20'){
-                  steps {
-                        build job: 'SmokeTests/ep-smoke-test-centos9', parameters: [string(name: 'buildId', value: "${params.buildId}"), string(name: 'javaDownload', value: "${params.java20x64}")]
-                  }
-              }
-              stage('Centos 8 arm64 Java20'){
-                  steps {
-                        build job: 'SmokeTests/ep-smoke-test-arm64', parameters: [string(name: 'buildId', value: "${params.buildId}"), string(name: 'javaDownload', value: "${params.java20arm64}")]
-                  }
-              }
-              stage('Centos 8.x ppc64le Java20'){
-				  steps {
-						build job: 'SmokeTests/ep-smoke-test-ppcle', parameters: [string(name: 'buildId', value: "${params.buildId}"), string(name: 'javaDownload', value: "${params.java20ppcle}")]
-				  }
-			  }
           	  stage('Ubuntu 22.04 Java21'){
 				  steps {
 						build job: 'SmokeTests/ep-smoke-test-ubuntu22', parameters: [string(name: 'buildId', value: "${params.buildId}"), string(name: 'javaDownload', value: "${params.java21x64}")]
