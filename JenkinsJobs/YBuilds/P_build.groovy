8616f857ae80883aedce7c0ac099c9d5692cbc4f
Rahul Mohanan <rahul.mohanan@ibm.com>
2024-03-01 16:14:49 +0530

    Move 4.31-I builds to R4_31_maintenance branch
    Tracked in
    https://github.com/eclipse-platform/eclipse.platform.releng.aggregator/issues/1845

--- JenkinsJobs/YBuilds/P_build.groovy
+++ JenkinsJobs/YBuilds/P_build.groovy
@@ -145,7 +145,7 @@ spec:
                   sshagent(['github-bot-ssh']) {
                       dir ('eclipse.platform.releng.aggregator') {
                         sh \'\'\'
-                            git clone -b master git@github.com:eclipse-platform/eclipse.platform.releng.aggregator.git
+                            git clone -b R4_31_maintenance git@github.com:eclipse-platform/eclipse.platform.releng.aggregator.git
                         \'\'\'
                       }
                     }
