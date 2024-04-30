eee587c3a9a414688034cd2c5d6226faeb5e1992
Hannes Wellmann <wellmann.hannes1@gmx.net>
2024-04-30 22:59:55 +0200

    [I-Build-Tests] Merge multiple definitions into one per OS

--- JenkinsJobs/AutomatedTests/I_unit_cen64_gtk3.groovy
+++ JenkinsJobs/AutomatedTests/I_unit_cen64_gtk3.groovy
@@ -1,11 +1,12 @@
 def config = new groovy.json.JsonSlurper().parseText(readFileFromWorkspace('JenkinsJobs/JobDSL.json'))
 def STREAMS = config.Streams
+def JAVA_VERSIONS = ['17', '21', '23']
 
 for (STREAM in STREAMS){
-  def MAJOR = STREAM.split('\\.')[0]
-  def MINOR = STREAM.split('\\.')[1]
+for (JAVA_VERSION in JAVA_VERSIONS){
+  def (MAJOR, MINOR) = STREAM.split('\\.')
 
-  pipelineJob('AutomatedTests/ep' + MAJOR + MINOR + 'I-unit-cen64-gtk3-java23'){
+  pipelineJob('AutomatedTests/ep' + MAJOR + MINOR + 'I-unit-cen64-gtk3-java' + JAVA_VERSION){
     description('Run Eclipse SDK Tests for the platform implied by this job\'s name')
     parameters { // Define parameters in job configuration to make them available from the very first build onwards
       stringParam('buildId', null, 'Build Id to test (such as I20240611-1800, N20120716-0800).')
@@ -23,7 +24,7 @@ pipeline {
   }
   agent {
     kubernetes {
-      label 'centos-unitpod23'
+      label 'centos-unitpod&&JAVA_VERSION&&'
       defaultContainer 'custom'
       yaml """
 apiVersion: v1
@@ -83,7 +84,7 @@ spec:
       stage('Run tests'){
           environment {
               // Declaring a jdk and ant the usual way in the 'tools' section, because of unknown reasons, breaks the usage of system commands like xvnc, pkill and sh
-              JAVA_HOME = tool(type:'jdk', name:'openjdk-jdk23-latest')
+              JAVA_HOME = tool(type:'jdk', name:'openjdk-jdk&&JAVA_VERSION&&-latest')
               ANT_HOME = tool(type:'ant', name:'apache-ant-latest')
               PATH = "${JAVA_HOME}/bin:${ANT_HOME}/bin:${PATH}"
           }
@@ -153,8 +154,9 @@ spec:
       }
   }
 }
-        ''')
+        '''.replace('&&JAVA_VERSION&&', JAVA_VERSION))
       }
     }
   }
+}
 }
