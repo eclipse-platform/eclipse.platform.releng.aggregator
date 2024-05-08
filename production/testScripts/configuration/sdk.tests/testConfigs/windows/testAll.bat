3329fe93aca6ea496a15da3e09c51821cb216cac
Hannes Wellmann <wellmann.hannes1@gmx.net>
2024-05-08 21:04:54 +0200

    Clean-up and generalize testPlatforms from runTests2 ANT script
    - Remove unsupported/unused testPlatform values and configurations and
    generalize the windows configuration.
    
    - Remove obsolete java version checks and duplicated java-version
    print-outs
    - Remove commented out javascript tasks

--- production/testScripts/configuration/sdk.tests/testConfigs/windows/testAll.bat
+++ production/testScripts/configuration/sdk.tests/testConfigs/windows/testAll.bat
@@ -15,11 +15,12 @@ REM vm.properties is used by default on production machines, but will
 REM need to override on local setups and performance tests
 IF NOT DEFINED propertyFile SET propertyFile=vm.properties
 
-
 REM TODO: not sure it is good to put VM here? Is there a good default here; such as "java"?
 REM currently, in practice, we sometimes set in Hudson scripts.
 REM https://bugs.eclipse.org/bugs/show_bug.cgi?id=390286
 IF NOT DEFINED jvm SET jvm=c:\Program Files\Java\jdk1.7.0_80\jre\bin\java.exe
+
+IF NOT DEFINED eclipseArch SET eclipseArch=x86_64
 
 ECHO jvm in testAll.bat: %jvm%
 ECHO extdir in testAll.bat (if any): %extdir%
@@ -30,10 +31,10 @@ mkdir results\consolelogs
 set consolelogs=results\consolelogs\%testedPlatform%_consolelog.txt
 
 IF DEFINED extdir (
-runtests.bat -extdirprop "%extdir%" -os win32 -ws win32 -arch x86 -vm "%jvm%" -properties %propertyFile%  %* > %consolelogs%
+runtests.bat -extdirprop "%extdir%" -os win32 -ws win32 -arch %eclipseArch% -vm "%jvm%" -properties %propertyFile% %* > %consolelogs%
 GOTO END
 )
 
-runtests.bat -os win32 -ws win32 -arch x86 -vm "%jvm%" -properties %propertyFile%  %* > %consolelogs%
+runtests.bat -os win32 -ws win32 -arch %eclipseArch% -vm "%jvm%" -properties %propertyFile% %* > %consolelogs%
 
 :END
