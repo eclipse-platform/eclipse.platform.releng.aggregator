eclipse-platform/eclipse.platform.releng.aggregator

commit d1baf6fdc62b2187f4224aabf24cbc098ab83a7e

[I/Y/P-Builds] Simplify Kubernetes pod definitions
Inherit most of the configuration from the centos-8 pod-template.

Co-authored-by: Sébastien Heurtematte <sebastien.heurtematte@eclipse-foundation.org>

JenkinsJobs/Builds/I_build.groovy
@@ -26,9 +26,6 @@ for (STREAM in STREAMS){
 # Post M1, no nightlies, I-builds only. (Be sure to "turn off" for tests and sign off days)
 #0 6 16-28 8 5-7,1-3
 #0 18 16-28 8 5-7,1-3
-
-
-
             """)
           }
         }
@@ -58,15 +55,13 @@ pipeline {
 	}
   agent {
     kubernetes {
-      label 'aggrbuild-pod'
+      inheritFrom 'centos-8'
       yaml """
 apiVersion: v1
 kind: Pod
 spec:
   containers:
   - name: "jnlp"
-    image: "eclipsecbi/jiro-agent-centos-8:latest"
-    imagePullPolicy: "Always"
     resources:
       limits:
         memory: "8192Mi"
@@ -74,75 +69,6 @@ spec:
       requests:
         memory: "6144Mi"
         cpu: "2000m"
-    securityContext:
-      privileged: false
-    tty: true
-    volumeMounts:
-    - mountPath: "/home/jenkins/agent"
-      name: "workspace-volume"
-      readOnly: false
-    - mountPath: "/home/jenkins/.m2/toolchains.xml"
-      name: "toolchains-xml"
-      readOnly: true
-      subPath: "toolchains.xml"
-    - mountPath: "/opt/tools"
-      name: "volume-0"
-      readOnly: false
-    - mountPath: "/home/jenkins"
-      name: "volume-2"
-      readOnly: false
-    - mountPath: "/home/jenkins/.m2/repository"
-      name: "volume-3"
-      readOnly: false
-    - mountPath: "/home/jenkins/.m2/settings-security.xml"
-      name: "settings-security-xml"
-      readOnly: true
-      subPath: "settings-security.xml"
-    - mountPath: "/home/jenkins/.m2/settings.xml"
-      name: "settings-xml"
-      readOnly: true
-      subPath: "settings.xml"
-    - mountPath: "/home/jenkins/.ssh"
-      name: "volume-1"
-      readOnly: false
-    workingDir: "/home/jenkins/agent"
-  nodeSelector: {}
-  restartPolicy: "Never"
-  volumes:
-  - name: "settings-security-xml"
-    secret:
-      items:
-      - key: "settings-security.xml"
-        path: "settings-security.xml"
-      secretName: "m2-secret-dir"
-  - name: "volume-0"
-    persistentVolumeClaim:
-      claimName: "tools-claim-jiro-releng"
-      readOnly: true
-  - configMap:
-      items:
-      - key: "toolchains.xml"
-        path: "toolchains.xml"
-      name: "m2-dir"
-    name: "toolchains-xml"
-  - emptyDir:
-      medium: ""
-    name: "volume-2"
-  - configMap:
-      name: "known-hosts"
-    name: "volume-1"
-  - name: "settings-xml"
-    secret:
-      items:
-      - key: "settings.xml"
-        path: "settings.xml"
-      secretName: "m2-secret-dir"
-  - emptyDir:
-      medium: ""
-    name: "workspace-volume"
-  - emptyDir:
-      medium: ""
-    name: "volume-3"
 """
     }
   }

JenkinsJobs/YBuilds/P_build.groovy
@@ -32,16 +32,13 @@ pipeline {
 	}
   agent {
     kubernetes {
-      label 'aggrbuild-pod'
-      defaultContainer 'container'
+      inheritFrom 'centos-8'
       yaml """
 apiVersion: v1
 kind: Pod
 spec:
   containers:
   - name: "jnlp"
-    image: "eclipsecbi/jiro-agent-centos-8:latest"
-    imagePullPolicy: "Always"
     resources:
       limits:
         memory: "8192Mi"
@@ -49,75 +46,6 @@ spec:
       requests:
         memory: "6144Mi"
         cpu: "2000m"
-    securityContext:
-      privileged: false
-    tty: true
-    volumeMounts:
-    - mountPath: "/home/jenkins/agent"
-      name: "workspace-volume"
-      readOnly: false
-    - mountPath: "/home/jenkins/.m2/toolchains.xml"
-      name: "toolchains-xml"
-      readOnly: true
-      subPath: "toolchains.xml"
-    - mountPath: "/opt/tools"
-      name: "volume-0"
-      readOnly: false
-    - mountPath: "/home/jenkins"
-      name: "volume-2"
-      readOnly: false
-    - mountPath: "/home/jenkins/.m2/repository"
-      name: "volume-3"
-      readOnly: false
-    - mountPath: "/home/jenkins/.m2/settings-security.xml"
-      name: "settings-security-xml"
-      readOnly: true
-      subPath: "settings-security.xml"
-    - mountPath: "/home/jenkins/.m2/settings.xml"
-      name: "settings-xml"
-      readOnly: true
-      subPath: "settings.xml"
-    - mountPath: "/home/jenkins/.ssh"
-      name: "volume-1"
-      readOnly: false
-    workingDir: "/home/jenkins/agent"
-  nodeSelector: {}
-  restartPolicy: "Never"
-  volumes:
-  - name: "settings-security-xml"
-    secret:
-      items:
-      - key: "settings-security.xml"
-        path: "settings-security.xml"
-      secretName: "m2-secret-dir"
-  - name: "volume-0"
-    persistentVolumeClaim:
-      claimName: "tools-claim-jiro-releng"
-      readOnly: true
-  - configMap:
-      items:
-      - key: "toolchains.xml"
-        path: "toolchains.xml"
-      name: "m2-dir"
-    name: "toolchains-xml"
-  - emptyDir:
-      medium: ""
-    name: "volume-2"
-  - configMap:
-      name: "known-hosts"
-    name: "volume-1"
-  - name: "settings-xml"
-    secret:
-      items:
-      - key: "settings.xml"
-        path: "settings.xml"
-      secretName: "m2-secret-dir"
-  - emptyDir:
-      medium: ""
-    name: "workspace-volume"
-  - emptyDir:
-      medium: ""
-    name: "volume-3"
 """
     }
   }

JenkinsJobs/YBuilds/Y_build.groovy
@@ -52,16 +52,13 @@ pipeline {
 	}
   agent {
     kubernetes {
-      label 'aggrbuild-pod'
-      defaultContainer 'container'
+      inheritFrom 'centos-8'
       yaml """
 apiVersion: v1
 kind: Pod
 spec:
   containers:
   - name: "jnlp"
-    image: "eclipsecbi/jiro-agent-centos-8:latest"
-    imagePullPolicy: "Always"
     resources:
       limits:
         memory: "8192Mi"
@@ -69,75 +66,6 @@ spec:
       requests:
         memory: "6144Mi"
         cpu: "2000m"
-    securityContext:
-      privileged: false
-    tty: true
-    volumeMounts:
-    - mountPath: "/home/jenkins/agent"
-      name: "workspace-volume"
-      readOnly: false
-    - mountPath: "/home/jenkins/.m2/toolchains.xml"
-      name: "toolchains-xml"
-      readOnly: true
-      subPath: "toolchains.xml"
-    - mountPath: "/opt/tools"
-      name: "volume-0"
-      readOnly: false
-    - mountPath: "/home/jenkins"
-      name: "volume-2"
-      readOnly: false
-    - mountPath: "/home/jenkins/.m2/repository"
-      name: "volume-3"
-      readOnly: false
-    - mountPath: "/home/jenkins/.m2/settings-security.xml"
-      name: "settings-security-xml"
-      readOnly: true
-      subPath: "settings-security.xml"
-    - mountPath: "/home/jenkins/.m2/settings.xml"
-      name: "settings-xml"
-      readOnly: true
-      subPath: "settings.xml"
-    - mountPath: "/home/jenkins/.ssh"
-      name: "volume-1"
-      readOnly: false
-    workingDir: "/home/jenkins/agent"
-  nodeSelector: {}
-  restartPolicy: "Never"
-  volumes:
-  - name: "settings-security-xml"
-    secret:
-      items:
-      - key: "settings-security.xml"
-        path: "settings-security.xml"
-      secretName: "m2-secret-dir"
-  - name: "volume-0"
-    persistentVolumeClaim:
-      claimName: "tools-claim-jiro-releng"
-      readOnly: true
-  - configMap:
-      items:
-      - key: "toolchains.xml"
-        path: "toolchains.xml"
-      name: "m2-dir"
-    name: "toolchains-xml"
-  - emptyDir:
-      medium: ""
-    name: "volume-2"
-  - configMap:
-      name: "known-hosts"
-    name: "volume-1"
-  - name: "settings-xml"
-    secret:
-      items:
-      - key: "settings.xml"
-        path: "settings.xml"
-      secretName: "m2-secret-dir"
-  - emptyDir:
-      medium: ""
-    name: "workspace-volume"
-  - emptyDir:
-      medium: ""
-    name: "volume-3"
 """
     }
   }

commit 93decb8c3c69315bc1131bc6a2be3ad78dd7f6b2

[I/Y/P-Builds] Restore mount of toolchains.xml in K8 pod definitions
Fixes a regression from
https://github.com/eclipse-platform/eclipse.platform.releng.aggregator/pull/2149

JenkinsJobs/Builds/I_build.groovy
@@ -69,6 +69,18 @@ spec:
       requests:
         memory: "6144Mi"
         cpu: "2000m"
+    volumeMounts:
+    - mountPath: "/home/jenkins/.m2/toolchains.xml"
+      name: "toolchains-xml"
+      readOnly: true
+      subPath: "toolchains.xml"
+  volumes:
+  - configMap:
+      items:
+      - key: "toolchains.xml"
+        path: "toolchains.xml"
+      name: "m2-dir"
+    name: "toolchains-xml"
 """
     }
   }

JenkinsJobs/YBuilds/P_build.groovy
@@ -46,6 +46,18 @@ spec:
       requests:
         memory: "6144Mi"
         cpu: "2000m"
+    volumeMounts:
+    - mountPath: "/home/jenkins/.m2/toolchains.xml"
+      name: "toolchains-xml"
+      readOnly: true
+      subPath: "toolchains.xml"
+  volumes:
+  - configMap:
+      items:
+      - key: "toolchains.xml"
+        path: "toolchains.xml"
+      name: "m2-dir"
+    name: "toolchains-xml"
 """
     }
   }

JenkinsJobs/YBuilds/Y_build.groovy
@@ -66,6 +66,18 @@ spec:
       requests:
         memory: "6144Mi"
         cpu: "2000m"
+    volumeMounts:
+    - mountPath: "/home/jenkins/.m2/toolchains.xml"
+      name: "toolchains-xml"
+      readOnly: true
+      subPath: "toolchains.xml"
+  volumes:
+  - configMap:
+      items:
+      - key: "toolchains.xml"
+        path: "toolchains.xml"
+      name: "m2-dir"
+    name: "toolchains-xml"
 """
     }
   }