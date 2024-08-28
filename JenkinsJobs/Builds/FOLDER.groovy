62eb7be0cb1a8b746d4530be8a79eddf47a0de56
Hannes Wellmann <wellmann.hannes1@gmx.net>
2024-08-28 22:42:47 +0200

    [Build] Define Docker images build using Jenkin's Job-DSL
    This just preserves the existing and currently manually maintained
    'Build-Docker-images' job in a Jenkins Job-DSL script under version
    control.

--- JenkinsJobs/Builds/FOLDER.groovy
+++ JenkinsJobs/Builds/FOLDER.groovy
@@ -1,3 +1,29 @@
 folder('Builds') {
   description('Eclipse periodic build jobs.')
 }
+
+pipelineJob('Builds/Build-Docker-images'){
+	description('Build and publish custom Docker images')
+	properties {
+		pipelineTriggers {
+			triggers {
+				cron {
+					spec('''
+H	0 * * 2
+H	8 * * 4
+H 16 * * 6
+					''')
+				}
+			}
+		}
+	}
+	definition {
+		cpsScm {
+			lightweight(true)
+			scm {
+				github('eclipse-platform/eclipse.platform.releng.aggregator', 'master')
+			}
+			scriptPath('JenkinsJobs/Builds/DockerImagesBuild.jenkinsfile')
+		}
+	}
+}
