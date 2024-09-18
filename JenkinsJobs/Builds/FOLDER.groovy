4e3b63f05c1253053982babd411862b8bd1b399c
Hannes Wellmann <wellmann.hannes1@gmx.net>
2024-09-18 19:54:29 +0200

    [Build] Fix git URLs and reduce mail notification in Docker image build
    - Update to jdt.core git repository at Github
    - Don't send mails on success only on failure and only to the
    mailing-list and not specific people
    - Build only once per week (if a recent change is needed a build is
    triggered manually any ways)
    - Delete complete workspace content after the build

--- JenkinsJobs/Builds/FOLDER.groovy
+++ JenkinsJobs/Builds/FOLDER.groovy
@@ -4,19 +4,6 @@ folder('Builds') {
 
 pipelineJob('Builds/Build-Docker-images'){
 	description('Build and publish custom Docker images')
-	properties {
-		pipelineTriggers {
-			triggers {
-				cron {
-					spec('''
-H	0 * * 2
-H	8 * * 4
-H 16 * * 6
-					''')
-				}
-			}
-		}
-	}
 	definition {
 		cpsScm {
 			lightweight(true)
