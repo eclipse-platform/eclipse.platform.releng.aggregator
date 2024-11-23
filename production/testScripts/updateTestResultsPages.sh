f7bc93a9baced428c60ac91b618e2edd43ec0b80
Rahul Mohanan <rahul.mohanan@ibm.com>
2024-11-23 15:11:16 +0530

    Update product version number to 4.35 across build scripts
    https://github.com/eclipse-platform/eclipse.platform.releng.aggregator/issues/2568

--- production/testScripts/updateTestResultsPages.sh
+++ production/testScripts/updateTestResultsPages.sh
@@ -295,7 +295,7 @@ then
 
   # TODO: avoid this hard coding of baseline value
   # NOTE: value must start with a letter match baselinePerfVersion in testScripts/configuration/streamSpecific.properties
-  baselineCode="R-4.33-202409030240"
+  baselineCode="S-4.34RC2-202411201800"
   # to get time stamp, first remove initial IMN:
   baselineForBuildSuffix=${buildId/[IMN]/}
   #Then remove final '-' in build id
