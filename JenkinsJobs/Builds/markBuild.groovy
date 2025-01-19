cac0bdac15466383bcd06a0e5dcb0cf1d4508af6
Hannes Wellmann <wellmann.hannes1@gmx.net>
2025-01-19 19:43:20 +0100

    [Build] Unify marking and update-index jobs and convert to pipeline
    Furthermore derive the issue label from the 'issueURLissueNum, which
    allows to remove the separate 'issueNum' parameter.

---
+++ JenkinsJobs/Builds/markBuild.groovy
@@ -0,0 +1,113 @@
+for (marker in [ 'stable', 'unstable' ]) {
+	pipelineJob("Builds/mark${marker.capitalize()}"){
+		displayName("Mark ${marker.capitalize()}")
+		description("Mark a build as ${marker}.")
+		parameters {
+			stringParam('buildId', null, "ID of the build to be marked ${marker}.")
+			if(marker == 'unstable') {
+				stringParam('issueUrl', null, 'URL of the causing Github issue or PR.')
+			}
+		}
+		definition {
+			cps {
+				sandbox()
+				script('''\
+pipeline {
+	options {
+		timestamps()
+		buildDiscarder(logRotator(numToKeepStr:'5'))
+	}
+	agent {
+		label 'basic'
+	}
+	stages {
+		stage('Update index'){
+			steps { // workspace is not always cleaned by default. Clean before custom tools are installed into workspace.
+				sshagent(['projects-storage.eclipse.org-bot-ssh']) {
+					sh \'''#!/bin/bash -xe
+						# this function executes command passed as command line parameter and 
+						# if that command fails it exit with the same error code as the failed command
+						
+						# Strip spaces from the buildId and eclipseStream
+						buildId=$(echo $buildId|tr -d ' ')
+						issueUrl=$(echo $issueUrl|tr -d ' ')
+						
+						if [ -z "$buildId" ]; then
+							echo "BuildId is empty! Exiting."
+							exit 1
+						fi''' + (marker == 'unstable' ? '''
+						if [ -z $issueUrl ]; then
+							echo "IssueUrl is empty! Exiting."
+							exit 1
+						fi''' : '') + '''
+						
+						epDownloadDir=/home/data/httpd/download.eclipse.org/eclipse
+						dropsPath=${epDownloadDir}/downloads/drops4
+						p2RepoPath=${epDownloadDir}/updates
+						buildDir=${dropsPath}/${buildId}
+						
+						workingDir=${epDownloadDir}/workingDir
+						
+						workspace=${workingDir}/${JOB_NAME}-${BUILD_NUMBER}
+						
+						ssh genie.releng@projects-storage.eclipse.org rm -rf ${workingDir}/${JOB_NAME}*
+						
+						ssh genie.releng@projects-storage.eclipse.org mkdir -p ${workspace}
+						ssh genie.releng@projects-storage.eclipse.org cd ${workspace}
+						
+						#get latest Eclipse platform product
+						epRelDir=$(ssh genie.releng@projects-storage.eclipse.org ls -d --format=single-column ${dropsPath}/R-*|sort|tail -1)
+						ssh genie.releng@projects-storage.eclipse.org tar -C ${workspace} -xzf ${epRelDir}/eclipse-platform-*-linux-gtk-x86_64.tar.gz
+						
+						#get requisite tools
+						markingScriptName=\'''' + { switch (marker) {
+							case 'stable': return 'addToComposite'
+							case 'unstable': return 'removeFromComposite'
+							default : throw new IllegalArgumentException("Unknown marker: ${marker}")
+						} }() + ''''
+						ssh genie.releng@projects-storage.eclipse.org wget -O ${workspace}/${markingScriptName}.xml https://download.eclipse.org/eclipse/relengScripts/cje-production/scripts/${markingScriptName}.xml
+						
+						#triggering ant runner
+						baseBuilderDir=${workspace}/eclipse
+						javaCMD=/opt/public/common/java/openjdk/jdk-17_x64-latest/bin/java
+						
+						launcherJar=$(ssh genie.releng@projects-storage.eclipse.org find ${baseBuilderDir}/. -name "org.eclipse.equinox.launcher_*.jar" | sort | head -1 )
+						
+						scp genie.releng@projects-storage.eclipse.org:${buildDir}/buildproperties.shsource .
+						source ./buildproperties.shsource
+						repoDir=/home/data/httpd/download.eclipse.org/eclipse/updates/${STREAMMajor}.${STREAMMinor}-${BUILD_TYPE}-builds
+						
+						devworkspace=${workspace}/workspace-antRunner
+						devArgs=-Xmx512m
+						extraArgs="${markingScriptName} -Drepodir=${repoDir} -Dcomplocation=${buildId}"
+						ssh genie.releng@projects-storage.eclipse.org  ${javaCMD} -jar ${launcherJar} -nosplash -consolelog -debug -data $devworkspace \\
+							-application org.eclipse.ant.core.antRunner -file ${workspace}/${markingScriptName}.xml ${extraArgs} -vmargs $devArgs
+						''' + { switch (marker) {
+							case 'stable': return '''
+						#Remove hidden attribute and unstable tags
+						ssh genie.releng@projects-storage.eclipse.org rm -f ${buildDir}/buildHidden
+						ssh genie.releng@projects-storage.eclipse.org rm -f ${buildDir}/buildUnstable
+						'''
+							case 'unstable': return '''
+						# Convert URL of GH issue or PR into: 'organization/repository#number'
+						label=$(echo "${issueUrl##'https://github.com/'}" | sed 's/\\\\/issues\\\\//#/g' | sed 's/\\\\/pull\\\\//#/g')
+						#add unstable tags
+						ssh genie.releng@projects-storage.eclipse.org rm -f ${buildDir}/buildUnstable
+						echo "<p>This build is marked unstable due to <a href='${issueUrl}'>${label}</a>.</p>" > buildUnstable
+						scp buildUnstable genie.releng@projects-storage.eclipse.org:${buildDir}/buildUnstable
+						'''
+							default : throw new IllegalArgumentException("Unknown marker: ${marker}")
+						} }() + '''
+						ssh genie.releng@projects-storage.eclipse.org rm -rf ${workingDir}/${JOB_NAME}*
+					\'''
+					build job: 'Releng/updateIndex', wait: false
+				}
+			}
+		}
+	}
+}
+	''')
+			}
+		}
+	}
+}
--- JenkinsJobs/Releng/updateIndex.groovy
+++ JenkinsJobs/Releng/updateIndex.groovy
@@ -1,38 +1,37 @@
-job('Releng/updateIndex'){
-  displayName('Update Download Index')
-  description('This is a Job for recreating the download indexes.')
-
-  logRotator {
-    numToKeep(10)
-  }
-
-  jdk('openjdk-jdk11-latest')
-
-  label('basic')
-
-  wrappers { //adds pre/post actions
-    timestamps()
-    sshAgent('projects-storage.eclipse.org-bot-ssh')
-  }
-  
-  steps {
-    shell('''
-#!/bin/bash -x
-
-PHP_PAGE="createIndex4x.php"
-HTML_PAGE="index.html"
-TEMP_INDEX="tempIndex.html"
-SITE_HOST=${SITE_HOST:-download.eclipse.org}
-
-wget --no-verbose -O ${TEMP_INDEX} https://${SITE_HOST}/eclipse/downloads/${PHP_PAGE} 2>&1
-
-scp ${TEMP_INDEX} genie.releng@projects-storage.eclipse.org:/home/data/httpd/download.eclipse.org/eclipse/downloads/${HTML_PAGE}
-    ''')
-  }
-
-  publishers {
-    extendedEmail {
-      recipientList("sravankumarl@in.ibm.com")
-    }
-  }
+pipelineJob('Releng/updateIndex'){
+	displayName('Update Download Index')
+	description('This is a Job for recreating the download index.')
+	definition {
+		cps {
+			sandbox()
+			script('''
+pipeline {
+	options {
+		timestamps()
+		buildDiscarder(logRotator(numToKeepStr:'10'))
+	}
+	agent {
+		label 'basic'
+	}
+	stages {
+		stage('Update index'){
+			steps { // workspace is not always cleaned by default. Clean before custom tools are installed into workspace.
+				sshagent(['projects-storage.eclipse.org-bot-ssh']) {
+					sh \'''#!/bin/bash -xe
+						PHP_PAGE="createIndex4x.php"
+						HTML_PAGE="index.html"
+						TEMP_INDEX="tempIndex.html"
+						
+						wget --no-verbose -O ${TEMP_INDEX} https://download.eclipse.org/eclipse/downloads/${PHP_PAGE} 2>&1
+						
+						scp ${TEMP_INDEX} genie.releng@projects-storage.eclipse.org:/home/data/httpd/download.eclipse.org/eclipse/downloads/${HTML_PAGE}
+					\'''
+				}
+			}
+		}
+	}
+}
+''')
+		}
+	}
 }
