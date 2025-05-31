584412152879e6b156d6471732295920ab353fcc
Rahul Mohanan <121536011+MohananRahul@users.noreply.github.com>
2025-05-31 12:37:23 +0530

    Create new test jobs for 4.37 (#3076)
    Fixes https://github.com/eclipse-platform/eclipse.platform.releng.aggregator/issues/3075

--- JenkinsJobs/Builds/FOLDER.groovy
+++ JenkinsJobs/Builds/FOLDER.groovy
@@ -17,15 +17,15 @@ for (STREAM in config.Streams){
 # format: Minute Hour Day Month Day of the week (0-7)
 
 # - - - Integration Eclipse SDK builds - - - 
-# 2025-06 Release Schedule
-# Normal : 6 PM every day (1/6 - 2/9)
-0 18 * * *
+# 2025-09 Release Schedule
+# Normal : 6 PM every day (11/6 - 27/8)
+# 0 18 * * *
 
 
 # RC Schedule 
 # Post M3, no nightlies, I-builds only. (Be sure to "turn off" for tests and sign off days)
-# 0 6 16-28 5 5-7,1-3
-# 0 18 16-28 5 5-7,1-3
+# 0 6 15-27 8 5-7,1-3
+# 0 18 15-27 8 5-7,1-3
 ''')
 					}
 				}
