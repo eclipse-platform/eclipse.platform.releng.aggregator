8b2710586a33a8897c46bf6954db37f2d71b69b0
Hannes Wellmann <wellmann.hannes1@gmx.net>
2025-09-03 22:55:17 +0200

    [RelEng] Update build configurations with release in release promotion
    This automates the update of build configurations with the final GA
    release versions on the master and corresponding maintenance branch, by
    updating all versions or URLs that were pointing to a RC or specific
    I-build to point to the final GA release.
    On the maintenance branch also set a fixed version for ECJ.

---
+++ JenkinsJobs/shared/utilities.groovy
@@ -0,0 +1,29 @@
+
+def replaceInFile(String filePath, Map<String,String> replacements) {
+	replaceAllInFile(filePath, replacements.collectEntries{ k, v -> [java.util.regex.Pattern.quote(k), v] });
+}
+
+def replaceAllInFile(String filePath, Map<String,String> replacements) {
+	def content = readFile(filePath)
+	for (entry in replacements) {
+		def newContent = content.replaceAll(entry.key, entry.value)
+		if (newContent == content && !(content =~ entry.key)) { // pattern matches, but the replacement is equal to the current content
+			error("Pattern not found in file '${filePath}': ${entry.key}")
+		}
+		content = newContent
+	}
+	writeFile(file:filePath, text: content)
+}
+
+def commitAllChangesExcludingSubmodules(String commitMessage) {
+	withEnv(["COMMIT_MESSAGE=${commitMessage}"]) {
+		sh '''
+			#Commit all changes, except for the updated sub-modules here
+			git add --all
+			git restore --staged $(git submodule foreach --quiet 'echo $sm_path')
+			git commit --message "${COMMIT_MESSAGE}"
+		'''
+	}
+}
+
+return this
