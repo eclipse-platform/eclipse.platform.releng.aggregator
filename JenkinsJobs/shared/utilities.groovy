64413a967f1d14f56cb17646eb40051c31fa0624
Hannes Wellmann <wellmann.hannes1@gmx.net>
2025-09-06 13:21:28 +0200

    [RelEng] Unify git push and submodule iteration in RelEng pipelines

--- JenkinsJobs/shared/utilities.groovy
+++ JenkinsJobs/shared/utilities.groovy
@@ -1,4 +1,11 @@
 
+@groovy.transform.Field
+def boolean IS_DRY_RUN = true
+
+def setDryRun(boolean isDryRun) {
+	IS_DRY_RUN = isDryRun
+}
+
 def replaceInFile(String filePath, Map<String,String> replacements) {
 	replaceAllInFile(filePath, replacements.collectEntries{ k, v -> [java.util.regex.Pattern.quote(k), v] });
 }
@@ -15,6 +22,20 @@ def replaceAllInFile(String filePath, Map<String,String> replacements) {
 	writeFile(file:filePath, text: content)
 }
 
+def runHereAndForEachGitSubmodule(Closure task) {
+	task.call()
+	forEachGitSubmodule(task)
+}
+
+def forEachGitSubmodule(Closure task) {
+	def submodulePaths = sh(script: "git submodule --quiet foreach 'echo \$sm_path'", returnStdout: true).trim().split('\\s')
+	for (submodulePath in submodulePaths) {
+		dir("${submodulePath}") {
+			task.call(submodulePath)
+		}
+	}
+}
+
 def gitCommitAllExcludingSubmodules(String commitMessage) {
 	withEnv(["COMMIT_MESSAGE=${commitMessage}"]) {
 		sh '''
@@ -23,6 +44,26 @@ def gitCommitAllExcludingSubmodules(String commitMessage) {
 			git restore --staged $(git submodule foreach --quiet 'echo $sm_path')
 			git commit --message "${COMMIT_MESSAGE}"
 		'''
+	}
+}
+
+def gitPushBranch(String localBranch, String remoteTargetBranch, boolean force = false) {
+	gitPush("${localBranch}:refs/heads/${remoteTargetBranch}", force)
+}
+
+def gitPushTag(String tagName, boolean force = false) {
+	gitPush("tag ${tagName}", force)
+}
+
+private void gitPush(String refSpec, boolean force) {
+	def dryRunOption = IS_DRY_RUN ? '--dry-run' : ''
+	def forceOption = force ? '--force' : ''
+	sshagent(['github-bot-ssh']) {
+		sh """#!/bin/bash -xe
+			# Switch to SSH, if the configured URL uses HTTPS (we can only push with SSH)
+			pushURL=\$(git config remote.origin.url | sed --expression 's|https://github.com/|git@github.com:|')
+			git push ${dryRunOption} ${forceOption} "\${pushURL}" ${refSpec}
+		"""
 	}
 }
 
